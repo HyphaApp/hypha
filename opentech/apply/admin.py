3a4258fdc60619d1bcf16f27484a60d9320d4de6
Todd Dembrey <todd.dembrey@torchbox.com>
2017-12-19 09:51:26 +0000

    Add the model admin for fund

--- opentech/apply/admin.py
+++ opentech/apply/admin.py
@@ -1,3 +1,18 @@
-# from django.contrib import admin
+from wagtail.contrib.modeladmin.options import ModelAdmin, ModelAdminGroup
 
-# Register your models here.
+from .models import Fund
+
+
+class FundModelAdmin(ModelAdmin):
+    model = Fund
+    menu_icon = 'site'
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class ApplyAdminGroup(ModelAdminGroup):
+    menu_label = 'Apply'
+    menu_icon = 'folder-open-inverse'
+    menu_order = 200
+    items = (FundModelAdmin,)
