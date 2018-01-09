2b31bc460741b204d50a770a12125859750a618c
Dan Braghis <dan.braghis@torchbox.com>
2018-01-09 15:52:48 +0000

    Tweak urls

--- opentech/apply/users/urls.py
+++ opentech/apply/users/urls.py
@@ -23,19 +23,19 @@ urlpatterns = [
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
     # Password reset
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(
+    url(r'^reset/$', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset/form.html',
         email_template_name='users/password_reset/email.txt',
         success_url=reverse_lazy('users:password_reset_done'),
         form_class=PasswordResetForm
     )),
     url(
-        r'^password_reset/done/$',
+        r'^reset/done/$',
         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset/done.html'),
         name='password_reset_done'
     ),
     url(
-        r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        r'^reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
             template_name='users/password_reset/confirm.html',
             post_reset_login=True,
@@ -44,7 +44,7 @@ urlpatterns = [
         name='password_reset_confirm'
     ),
     url(
-        r'^password_reset/complete/$',
+        r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset/complete.html'),
         name='password_reset_complete'
     ),
