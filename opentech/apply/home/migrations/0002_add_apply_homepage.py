4a5f060acf0f47a382826c4735e3eb005a112501
Todd Dembrey <todd.dembrey@torchbox.com>
2018-01-09 19:01:25 +0000

    sort out the apply homepage migrations

--- opentech/apply/home/migrations/0002_add_apply_homepage.py
+++ opentech/apply/home/migrations/0002_add_apply_homepage.py
@@ -9,11 +9,11 @@ def create_homepage(apps, schema_editor):
     ContentType = apps.get_model('contenttypes.ContentType')
     Page = apps.get_model('wagtailcore.Page')
     Site = apps.get_model('wagtailcore.Site')
-    ApplyHomePage = apps.get_model('apply.ApplyHomePage')
+    ApplyHomePage = apps.get_model('apply_home.ApplyHomePage')
 
     # Create content type for homepage model
     homepage_content_type, created = ContentType.objects.get_or_create(
-        model='applyhomepage', app_label='apply')
+        model='applyhomepage', app_label='apply_home')
 
     # Create a new homepage
     applyhomepage = ApplyHomePage.objects.create(
@@ -36,7 +36,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('funds', '0001_initial'),
-        ('home', '0001_initial'),
+        ('apply_home', '0001_initial'),
     ]
 
     operations = [
