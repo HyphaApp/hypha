1e0e5f53a1024379d550a10c07949b3762f092cb
Dan Braghis <dan.braghis@torchbox.com>
2018-01-09 15:04:14 +0000

    Add password reset urls and templates

--- opentech/apply/users/urls.py
+++ opentech/apply/users/urls.py
@@ -2,15 +2,50 @@ from django.conf.urls import url
 
 from django.contrib.auth import views as auth_views
 
+from django.urls import reverse_lazy
+
+from wagtail.wagtailadmin.forms import PasswordResetForm
+
+from opentech.users.views import account
+
 urlpatterns = [
-    # TODO replace with dashboard view with a login_required decorator
-    url(r'^$', auth_views.login, {
-        'template_name': 'users/login.html'
-    }, name='user'),
-    url(r'^login$', auth_views.login, {
-        'template_name': 'users/login.html'
-    }, name='login'),
+    url(r'^$', account, name='account'),
+    url(
+        r'^login/$',
+        auth_views.LoginView.as_view(
+            template_name='users/login.html',
+            redirect_authenticated_user=True
+        ),
+        name='login'
+    ),
 
     # Log out
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    # Password reset
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset/form.html',
+        email_template_name='users/password_reset/email.txt',
+        success_url=reverse_lazy('users:password_reset_done'),
+        form_class=PasswordResetForm
+    )),
+    url(
+        r'^password_reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset/done.html'),
+        name='password_reset_done'
+    ),
+    url(
+        r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset/confirm.html',
+            post_reset_login=True,
+            success_url=reverse_lazy('users:account')
+        ),
+        name='password_reset_confirm'
+    ),
+    url(
+        r'^password_reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset/complete.html'),
+        name='password_reset_complete'
+    ),
 ]
--- opentech/apply/users/views.py
+++ opentech/apply/users/views.py
@@ -1,1 +1,8 @@
-# Views placeholder.
+from django.contrib.auth.decorators import login_required
+from django.template.response import TemplateResponse
+
+
+@login_required(login_url='/user/login')
+def account(request):
+    "Account page placeholder view"
+    return TemplateResponse(request, 'users/account.html', {})
