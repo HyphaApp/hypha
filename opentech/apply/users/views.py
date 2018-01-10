3d427781477c154a1c557ab32b30332b5a8c1ad4
Dan Braghis <dan.braghis@torchbox.com>
2018-01-10 17:29:20 +0000

    Tweak required_login url

--- opentech/apply/users/views.py
+++ opentech/apply/users/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from wagtail.wagtailadmin.views.account import password_management_enabled
 
 
-@login_required(login_url='/user/login')
+@login_required(login_url=reverse_lazy('users:login'))
 def account(request):
     "Account page placeholder view"
 
