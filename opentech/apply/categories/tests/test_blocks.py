ee8e37390b8bdeac242a0d236d686b0912bbc128
Todd Dembrey <todd.dembrey@torchbox.com>
2018-01-11 08:29:08 +0000

    Add second test for supplied values

--- opentech/apply/categories/tests/test_blocks.py
+++ opentech/apply/categories/tests/test_blocks.py
@@ -11,15 +11,26 @@ class TestCategoryQuestionBlock(TestCase):
         self.category = CategoryFactory()
         self.block = CategoryQuestionBlock()
 
-    def test_field_and_help_default(self):
-        block = self.block.to_python({
+    def get_field(self, **kwargs):
+        data = {
             'field_label': '',
             'help_text': '',
             'category': self.category.id,
             'multi': False,
-        })
+        }
+        data.update(kwargs)
 
-        field = self.block.get_field(block)
+        block = self.block.to_python(data)
 
+        return self.block.get_field(block)
+
+    def test_field_and_help_default(self):
+        field = self.get_field(field_label='', help_text='')
         self.assertEqual(self.category.name, field.label)
         self.assertEqual(self.category.help_text, field.help_text)
+
+    def test_supplied_field_and_help(self):
+        values = {'field_label': 'LABEL', 'help_text': 'HELP'}
+        field = self.get_field(**values)
+        self.assertEqual(values['field_label'], field.label)
+        self.assertEqual(values['help_text'], field.help_text)
