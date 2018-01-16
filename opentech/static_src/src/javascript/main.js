93ddc20c2cd3100e39682d07d7401691bacfd767
Chris Lawton <chris.lawton@torchbox.com>
2018-01-16 14:55:34 +0000

    refactor mobile search button when the mobile menu is open, to close the mobile menu and then open the search form

--- opentech/static_src/src/javascript/main.js
+++ opentech/static_src/src/javascript/main.js
@@ -1,6 +1,7 @@
 import $ from './globals';
 import MobileMenu from './components/mobile-menu';
 import Search from './components/search';
+import MobileSearch from './components/mobile-search';
 
 $(function () {
     $(MobileMenu.selector()).each((index, el) => {
@@ -9,5 +10,9 @@ $(function () {
 
     $(Search.selector()).each((index, el) => {
         new Search($(el), $('.header__search'));
+    });
+
+    $(MobileSearch.selector()).each((index, el) => {
+        new MobileSearch($(el), $('.header__menus--mobile'), $('.header__search'), $('.js-search-toggle'));
     });
 });
