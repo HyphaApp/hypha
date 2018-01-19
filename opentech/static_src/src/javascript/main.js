9977ec2253ab4dffd5575e4b4b9f6b1e46cf2fa4
Chris Lawton <chris.lawton@torchbox.com>
2018-01-19 12:16:54 +0000

    show list of selected files for upload to input file type labels as the actual input is being hidden along with the filenames

--- opentech/static_src/src/javascript/main.js
+++ opentech/static_src/src/javascript/main.js
@@ -18,4 +18,13 @@ $(function () {
     $(MobileSearch.selector()).each((index, el) => {
         new MobileSearch($(el), $('.header__menus--mobile'), $('.header__search'), $('.js-search-toggle'));
     });
+
+    // Show list of selected files for upload on input[type=file]
+    $('input[type=file]').change(function() {
+        for (let i = 0; i < $(this)[0].files.length; ++i) {
+            $(this).parents('.form__item').prepend(`
+                <p>${$(this)[0].files[i].name}</p>
+            `);
+        }
+    });
 });
