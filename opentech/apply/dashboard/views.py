57599f18d67755c7d94fb4d8d67808667e7fc820
Todd Dembrey <todd.dembrey@torchbox.com>
2018-01-23 10:56:13 +0000

    add django tables 2 and basic table layout

--- opentech/apply/dashboard/views.py
+++ opentech/apply/dashboard/views.py
@@ -2,7 +2,14 @@ from django.views.generic import ListView
 
 from opentech.apply.funds.models import ApplicationSubmission
 
+from .tables import DashboardTable
+
 
 class DashboardView(ListView):
     model = ApplicationSubmission
     template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = DashboardTable(context['object_list'])
+        return context
--- opentech/settings/base.py
+++ opentech/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'captcha',
     'wagtailcaptcha',
+    'django_tables2',
 
     'django.contrib.admin',
     'django.contrib.auth',
