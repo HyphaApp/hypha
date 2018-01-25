87c0578b368c7abbcc807c02ebb7a746fe74aa92
Dan Braghis <dan.braghis@torchbox.com>
2018-01-25 13:36:12 +0000

    Handle account activation

--- opentech/apply/users/utils.py
+++ opentech/apply/users/utils.py
@@ -1,4 +1,8 @@
 from django.conf import settings
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 
 def can_use_oauth_check(user):
@@ -14,3 +18,24 @@ def can_use_oauth_check(user):
         # Anonymous user or setting not defined
         pass
     return False
+
+
+def send_activation_email(user):
+    """
+    Send the activation email. The activation key is the username,
+    signed using TimestampSigner.
+    """
+    token_generator = PasswordResetTokenGenerator()
+    context = {
+        'user': user,
+        'name': user.get_full_name(),
+        'username': user.get_username(),
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': token_generator.make_token(user),
+    }
+
+    subject = render_to_string('users/activation/email_subject.txt', context)
+    # Force subject to a single line to avoid header-injection issues.
+    subject = ''.join(subject.splitlines())
+    message = render_to_string('users/activation/email.txt', context)
+    user.email_user(subject, message, settings.DEFAULT_FROM_EMAIL)
