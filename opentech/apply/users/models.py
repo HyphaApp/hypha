d66fc9e55dd1d0e1923e949ae22c0f21dfd9de23
Dan Braghis <dan.braghis@torchbox.com>
2018-01-25 09:56:17 +0000

    Add user full name field and include in admin forms

--- opentech/apply/users/models.py
+++ opentech/apply/users/models.py
@@ -1,5 +1,16 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    full_name = models.CharField(verbose_name='Full name', max_length=255, blank=True)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        if self.full_name:
+            return self.full_name.strip()
+
+        return super().get_full_name()
+
