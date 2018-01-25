066483aa934e29f3350beef94d67cdde4a6186ab
Dan Braghis <dan.braghis@torchbox.com>
2018-01-25 09:56:31 +0000

    Make email unique

--- opentech/apply/users/models.py
+++ opentech/apply/users/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     full_name = models.CharField(verbose_name='Full name', max_length=255, blank=True)
 
+    class Meta:
+        unique_together = ('email',)
+
     def get_full_name(self):
         """
         Returns the first_name plus the last_name, with a space in between.
@@ -13,4 +16,8 @@ class User(AbstractUser):
             return self.full_name.strip()
 
         return super().get_full_name()
+
+    def get_user_by_email(self, email):
+        email_field = getattr(self, 'EMAIL_FIELD', 'email')
+        return self.objects.filter(**{email_field + '__iexact': email})
 
