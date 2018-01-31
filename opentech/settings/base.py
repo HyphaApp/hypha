b419b990d5601a4dae8b5422396b66dacdda4670
Chris Lawton <chris.lawton@torchbox.com>
2018-01-31 11:05:36 +0000

    re-directing already logged in users to the dashboard

--- opentech/settings/base.py
+++ opentech/settings/base.py
@@ -193,9 +193,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'users.User'
-# TODO populate me with the dashboard URL when ready
+
 LOGIN_URL = 'users:login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'dashboard:dashboard'
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
