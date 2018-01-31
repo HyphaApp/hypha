9832bb2679a42436ff2408aeaa88ce95a7674709
Todd Dembrey <todd.dembrey@torchbox.com>
2018-01-31 16:01:58 +0000

    Update to add required fields

--- opentech/apply/funds/static/address_form.js
+++ opentech/apply/funds/static/address_form.js
@@ -1,4 +1,14 @@
 (function ($) {
+    var oldTransform = $.fn.addressfield.transform;
+    $.fn.addressfield.transform = function(data) {
+        var mappedData = oldTransform.call(this, data);
+        return mappedData;
+    };
+
+    $('.form').bind('addressfield:after', function (e, data) {
+        debugger;
+    });
+
     $(document).ready(function formReady() {
         $('.form div.address').addressfield({
             json: '/static/addressfield.min.json',
