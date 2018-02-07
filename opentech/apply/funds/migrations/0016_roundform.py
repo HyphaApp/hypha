aa08887e8490ef24cc62584a0d82f4b1e9a5d804
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-07 13:31:29 +0000

    Fix migration rebase issue

--- opentech/apply/funds/migrations/0016_roundform.py
+++ opentech/apply/funds/migrations/0016_roundform.py
@@ -10,7 +10,7 @@ import modelcluster.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('funds', '00014_add_meta_names'),
+        ('funds', '0015_link_user_to_application'),
     ]
 
     operations = [
