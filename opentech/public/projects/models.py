9f9a65f4100ad37281d064d785f1784e50afe5db
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-08 16:14:31 +0000

    Make the project indexpage a child of standard page

--- opentech/public/projects/models.py
+++ opentech/public/projects/models.py
@@ -140,7 +140,7 @@ class ProjectPage(FundingMixin, BasePage):
 
 class ProjectIndexPage(BasePage):
     subpage_types = ['ProjectPage']
-    parent_page_types = ['home.Homepage']
+    parent_page_types = ['home.Homepage', 'standardpage.IndexPage']
 
     introduction = models.TextField(blank=True)
 
