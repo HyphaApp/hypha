24c9cc998236bc40c736ad033c66be0436dc1741
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-08 07:59:10 +0000

    Add workflow field to round model

--- opentech/apply/funds/models.py
+++ opentech/apply/funds/models.py
@@ -33,6 +33,7 @@ from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormSubmissio
 from opentech.apply.stream_forms.models import AbstractStreamForm
 
 from .blocks import CustomFormFieldsBlock, MustIncludeFieldBlock, REQUIRED_BLOCK_NAMES
+from .edit_handlers import ReadOnlyPanel
 from .forms import WorkflowFormAdminForm
 from .workflow import SingleStage, DoubleStage
 
@@ -221,6 +222,7 @@ class Round(SubmittableStreamForm):
     parent_page_types = ['funds.FundType']
     subpage_types = []  # type: ignore
 
+    workflow = models.CharField(max_length=100, default='single')
     start_date = models.DateField(default=date.today)
     end_date = models.DateField(
         blank=True,
@@ -236,6 +238,7 @@ class Round(SubmittableStreamForm):
                 FieldPanel('end_date'),
             ]),
         ], heading="Dates"),
+        ReadOnlyPanel('workflow'),
     ]
 
     def save(self, *args, **kwargs):
