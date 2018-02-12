2cd51ce32224597542b1d1f0296b1c9d6f30513d
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-12 10:11:50 +0000

    split workflow related factories into their own file

--- opentech/apply/funds/tests/factories/__init__.py
+++ opentech/apply/funds/tests/factories/__init__.py
@@ -2,8 +2,11 @@ from . import models
 from .models import * # noqa
 from . import blocks
 from .blocks import *  # noqa
+from . import workflows
+from .workflows import * # noqa
 
 __all__ = []
 
 __all__.extend(blocks.__all__)
 __all__.extend(models.__all__)
+__all__.extend(workflows.__all__)
