ba1f134e0e558827903adfe61510fd3cd50ede02
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-13 13:54:42 +0000

    Add the view and add the link from the dashboard

--- opentech/apply/dashboard/urls.py
+++ opentech/apply/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import DashboardView
+from .views import DashboardView, SubmissionDetailView
 
 
 urlpatterns = [
-    url(r'^$', DashboardView.as_view(), name="dashboard")
+    url(r'^$', DashboardView.as_view(), name="dashboard"),
+    url(r'^submission/(?P<pk>\d+)/$', SubmissionDetailView.as_view(), name="submission"),
 ]
