20807398919442e8ef5ec37e1ae314299fefe2c9
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-15 12:26:40 +0000

    Format the filter display value

--- opentech/apply/dashboard/static/js/django_select2-checkboxes.js
+++ opentech/apply/dashboard/static/js/django_select2-checkboxes.js
@@ -1,5 +1,15 @@
 (function ($) {
   $(function () {
-      $('.django-select2-checkboxes').select2MultiCheckboxes();
+      $('.django-select2-checkboxes').select2MultiCheckboxes({
+          templateSelection: function(selected, total) {
+              let filterType = 'Placeholder';
+              if ( !selected.length ) {
+                  return filterType;
+              } else if ( selected.length===total ) {
+                  return 'All ' + filterType + ' selected';
+              }
+              return selected.length + ' of ' + total + ' selected';
+          }
+      });
   });
 }(this.jQuery));
