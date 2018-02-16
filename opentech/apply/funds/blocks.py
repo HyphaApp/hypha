e9d06a5d39d6d0a6c2adef46c305f7b56ae9e2cb
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-16 10:28:41 +0000

    Update the list of default styles for the richtext

--- opentech/apply/funds/blocks.py
+++ opentech/apply/funds/blocks.py
@@ -39,7 +39,19 @@ class RichTextFieldBlock(TextFieldBlock):
     widget = TinyMCE(mce_attrs={
         'elementpath': False,
         'branding': False,
-        'toolbar1': 'undo redo | styleselect | bold italic | bullist numlist | link'
+        'toolbar1': 'undo redo | styleselect | bold italic | bullist numlist | link',
+        'style_formats': [
+            {'title': 'Headers', 'items': [
+                {'title': 'Header 1', 'format': 'h1'},
+                {'title': 'Header 2', 'format': 'h2'},
+                {'title': 'Header 3', 'format': 'h3'},
+            ]},
+            {'title': 'Inline', 'items': [
+                {'title': 'Bold', 'icon': 'bold', 'format': 'bold'},
+                {'title': 'Italic', 'icon': 'italic', 'format': 'italic'},
+                {'title': 'Underline', 'icon': 'underline', 'format': 'underline'},
+            ]},
+        ],
     })
 
     class Meta:
