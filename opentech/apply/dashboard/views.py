f34dbc9282a27e89f8edc0de65ee70eacce7d4fb
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-16 15:19:41 +0000

    Add basic search using Postgres for submitted data

--- opentech/apply/dashboard/views.py
+++ opentech/apply/dashboard/views.py
@@ -1,3 +1,6 @@
+from django.contrib.postgres.search import SearchVector
+from django.db.models import TextField
+from django.db.models.functions import Cast
 from django.views.generic import DetailView
 
 from django_filters.views import FilterView
@@ -18,3 +21,21 @@ class DashboardView(SingleTableMixin, FilterView):
 
 class SubmissionDetailView(DetailView):
     model = ApplicationSubmission
+
+
+class SearchView(SingleTableMixin, FilterView):
+    template_name = 'dashboard/search.html'
+    table_class = DashboardTable
+
+    filterset_class = SubmissionFilter
+
+    def get_context_data(self, **kwargs):
+        search_term = self.request.GET.get('query')
+        if search_term:
+            # Postgres <10 doesn't support search on JSON
+            # Cast to text to make searchable
+            self.object_list = self.object_list.annotate(
+                search=SearchVector(Cast('form_data', TextField())),
+            ).filter(search=search_term)
+
+        return super().get_context_data(search_term=search_term, **kwargs)
--- opentech/settings/base.py
+++ opentech/settings/base.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.forms',
