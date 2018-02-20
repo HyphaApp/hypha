6d0a518ad9c9139ab870a0e1398db0d72cd6ebc8
Chris Lawton <chris.lawton@torchbox.com>
2018-02-20 10:42:54 +0000

    add an is-active class to select2 containers for styling

--- opentech/static_src/src/javascript/main.js
+++ opentech/static_src/src/javascript/main.js
@@ -27,4 +27,21 @@ $(function () {
             `);
         }
     });
+
+    // Add active class to filters - dropdowns are dynamically appended to the dom,
+    // so we have to listen for the event higher up
+    $('body').on('click', '.select2-dropdown', (e) => {
+        // get the id of the dropdown
+        let selectId = e.target.parentElement.parentElement.id;
+
+        // find the matching dropdown
+        let match = $(`.select2-selection[aria-owns="${selectId}"]`);
+
+        // if the dropdown contains a clear class, the filters are active
+        if($(match[0]).find('span.select2-selection__clear').length !== 0) {
+            match[0].classList.add('is-active');
+        } else {
+            match[0].classList.remove('is-active');
+        }
+    });
 });
