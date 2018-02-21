5d14562837a91fabe2fa045201c3759bb932ee73
Todd Dembrey <todd.dembrey@torchbox.com>
2018-02-21 09:26:24 +0000

    Delegate to the blocks for defining the searchable content

--- opentech/apply/funds/models.py
+++ opentech/apply/funds/models.py
@@ -508,10 +508,9 @@ class ApplicationSubmission(WorkflowHelpers, AbstractFormSubmission):
         return mark_safe(''.join(fields))
 
     def prepare_search_values(self):
-        excluded_fields = ['data', 'time', 'datetime', 'value', 'category', 'number', 'file', 'multifile']
         for data, stream in self.data_and_fields():
-            if stream.block_type not in excluded_fields and data:
-                value = stream.block.get_searchable_content(stream.value, data)
+            value = stream.block.get_searchable_content(stream.value, data)
+            if value:
                 if isinstance(value, list):
                     yield ', '.join(value)
                 else:
