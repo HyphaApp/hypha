889903c8139157252e7542afab32e6815d2f0267
Dan Braghis <dan.braghis@torchbox.com>
2018-02-28 14:47:51 +0000

    Use a common base class for the custom admin form classes

--- opentech/apply/users/forms.py
+++ opentech/apply/users/forms.py
@@ -3,8 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from wagtail.wagtailusers.forms import UserEditForm, UserCreationForm
 
-
-class CustomUserEditForm(UserEditForm):
+class CustomUserAdminFormBase():
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -12,10 +11,8 @@ class CustomUserEditForm(UserEditForm):
         self.fields['first_name'].widget = forms.HiddenInput(attrs={'value': f"Not used - see full_name"})
         self.fields['last_name'].widget = forms.HiddenInput(attrs={'value': f"Not used - see full_name"})
 
-class CustomUserCreationForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class CustomUserEditForm(CustomUserAdminFormBase, UserEditForm):
+    pass
 
-        # HACK: Wagtail admin doesn't work with custom User models that do not have first/last name.
-        self.fields['first_name'].widget = forms.HiddenInput(attrs={'value': f"Not used - see full_name"})
-        self.fields['last_name'].widget = forms.HiddenInput(attrs={'value': f"Not used - see full_name"})
+class CustomUserCreationForm(CustomUserAdminFormBase, UserCreationForm):
+    pass
