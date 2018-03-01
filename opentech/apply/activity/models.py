5d53830ab5d29b34cbc1d526da57edb2e8f3decf
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-01 09:59:00 +0000

    Add a related_name for the activity relation

--- opentech/apply/activity/models.py
+++ opentech/apply/activity/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Activity(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    submission = models.ForeignKey('funds.ApplicationSubmission')
+    submission = models.ForeignKey('funds.ApplicationSubmission', related_name='activities')
     message = models.TextField()
 
     class Meta:
