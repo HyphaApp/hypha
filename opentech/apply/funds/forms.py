ea140fa1c8c57e552603d827785bc36519a9784a
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-01 15:40:52 +0000

    Hide the form if there are no actions

--- opentech/apply/funds/forms.py
+++ opentech/apply/funds/forms.py
@@ -15,6 +15,7 @@ class ProgressSubmissionForm(forms.ModelForm):
         choices = [(action, action) for action in self.instance.phase.action_names]
         self.fields['action'].choices = choices
         self.fields['action'].label = self.instance.phase.name
+        self.should_show = bool(choices)
 
     def save(self, *args, **kwargs):
         new_phase = self.instance.workflow.process(self.instance.phase, self.cleaned_data['action'])
