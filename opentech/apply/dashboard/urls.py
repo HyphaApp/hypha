e189a4a1165f4ec42b86102c50c00dbf454cb0ac
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-05 12:32:39 +0000

    Add base applicant dashboard view and tempaltes

--- opentech/apply/dashboard/urls.py
+++ opentech/apply/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import DashboardView
+from .views import ApplicantDashboardView, DashboardView
 
 
 urlpatterns = [
     url(r'^$', DashboardView.as_view(), name="dashboard"),
+    url(r'^applicant/$', ApplicantDashboardView.as_view(), name="dashboard"),
 ]
--- opentech/apply/dashboard/views.py
+++ opentech/apply/dashboard/views.py
@@ -1,5 +1,21 @@
 from django.views.generic import TemplateView
 
+from opentech.apply.funds.models import ApplicationSubmission
+
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/dashboard.html'
+
+
+class ApplicantDashboardView(TemplateView):
+    template_name = 'dashboard/applicant_dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        my_submissions = ApplicationSubmission.objects.filter(user=self.request.user)
+        my_active_submissions = my_submissions
+
+        return super().get_context_data(
+            my_submissions=my_submissions,
+            my_active_submissions=my_active_submissions,
+            **kwargs,
+        )
