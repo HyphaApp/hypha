c2e4fdc19b10a5401e554c28813b9fd8c96a6d5d
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-05 12:43:07 +0000

    Add the table to the applicant's dashboard

--- opentech/apply/dashboard/views.py
+++ opentech/apply/dashboard/views.py
@@ -1,21 +1,27 @@
 from django.views.generic import TemplateView
 
+from django_tables2.views import SingleTableView
+
 from opentech.apply.funds.models import ApplicationSubmission
+from opentech.apply.funds.tables import SubmissionsTable
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/dashboard.html'
 
 
-class ApplicantDashboardView(TemplateView):
+class ApplicantDashboardView(SingleTableView):
     template_name = 'dashboard/applicant_dashboard.html'
+    model = ApplicationSubmission
+    table_class = SubmissionsTable
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
 
     def get_context_data(self, **kwargs):
-        my_submissions = ApplicationSubmission.objects.filter(user=self.request.user)
-        my_active_submissions = my_submissions
+        my_active_submissions = self.object_list
 
         return super().get_context_data(
-            my_submissions=my_submissions,
             my_active_submissions=my_active_submissions,
             **kwargs,
         )
