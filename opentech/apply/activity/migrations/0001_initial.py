35284c6f76f20318e7c854dfea867e84a145d9e3
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-12 10:25:14 +0000

    fixup! Make count of comments personalised for the user

--- opentech/apply/activity/migrations/0001_initial.py
+++ opentech/apply/activity/migrations/0001_initial.py
@@ -27,6 +27,7 @@ class Migration(migrations.Migration):
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'base_manager_name': 'objects',
                 'ordering': ['-timestamp'],
             },
         ),
