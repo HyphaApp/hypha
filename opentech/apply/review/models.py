420e72ec34d8ac0487ba00b179cf755e56a82c04
Dan Braghis <dan.braghis@torchbox.com>
2018-03-14 15:16:49 +0000

    Add common review fields

--- opentech/apply/review/models.py
+++ opentech/apply/review/models.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.db import models
 
 
+RECOMMENDATION_CHOICES = (
+    (0, 'No'),
+    (1, 'Yes'),
+    (2, 'Maybe')
+)
+
 class Review(models.Model):
     submission = models.ForeignKey('funds.ApplicationSubmission', on_delete=models.CASCADE)
     author = models.ForeignKey(
@@ -9,6 +15,14 @@ class Review(models.Model):
         on_delete=models.PROTECT,
     )
     review = models.TextField()
+    recommendation = models.IntegerField(verbose_name="Recommendation", choices=RECOMMENDATION_CHOICES, default=0)
+    score = models.DecimalField(max_digits=10, decimal_places=1, default=0)
 
     class Meta:
         unique_together = ('author', 'submission')
+
+    def __str__(self):
+        return f'Review for {self.submission.title} by {self.author!s}'
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}: {str(self.review)}>'
