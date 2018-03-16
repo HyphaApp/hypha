cc458f1c0dc474652552e3edbfbaf7d7e61233e9
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-16 11:06:47 +0000

    Dry out and simplify the view code

--- opentech/apply/review/views.py
+++ opentech/apply/review/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
 
 from opentech.apply.funds.models import ApplicationSubmission
 from .forms import ConceptReviewForm
@@ -12,11 +11,11 @@ class ReviewCreateView(CreateView):
     form_class = ConceptReviewForm
 
     def get_context_data(self, **kwargs):
-        submission = get_object_or_404(ApplicationSubmission, id=self.kwargs['submission_pk'])
-        has_submitted_review = Review.objects.filter(submission=submission, author=self.request.user).count() > 0
+        self.submission = get_object_or_404(ApplicationSubmission, id=self.kwargs['submission_pk'])
+        has_submitted_review = Review.objects.filter(submission=self.submission, author=self.request.user).exists()
 
         return super().get_context_data(
-            submission=submission,
+            submission=self.submission,
             has_submitted_review=has_submitted_review,
             **kwargs
         )
@@ -24,8 +23,8 @@ class ReviewCreateView(CreateView):
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['request'] = self.request
-        kwargs['submission'] = get_object_or_404(ApplicationSubmission, id=self.kwargs['submission_pk'])
+        kwargs['submission'] = self.submission
         return kwargs
 
     def get_success_url(self):
-        return reverse_lazy('apply:submission', args=(self.kwargs['submission_pk'],))
+        return self.submission.get_absolute_url()
