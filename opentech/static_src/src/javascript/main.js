de79af872ae950b25afce9d8f3c955990e8dda1a
Chris Lawton <chris.lawton@torchbox.com>
2018-03-20 12:19:15 +0000

    method to allow click and drag scrolling of a given element, in this case the reviews table

--- opentech/static_src/src/javascript/main.js
+++ opentech/static_src/src/javascript/main.js
@@ -95,6 +95,8 @@ import '@fancyapps/fancybox';
             });
         });
 
+        // Allow click and drag scrolling within reviews table wrapper
+        $('.js-reviews-table').attachDragger();
     });
 
     // Add active class to filters - dropdowns are dynamically appended to the dom,
@@ -177,6 +179,23 @@ import '@fancyapps/fancybox';
             positionalMatch.css('top', positionalMatch.position().top - dropdownMargin);
         }
     }
+
+    // Enable click and drag scrolling within a div
+    $.fn.attachDragger = function(){
+        let attachment = false, lastPosition, position, difference;
+        $($(this).selector ).on('mousedown mouseup mousemove', (e) => {
+            if(e.type == 'mousedown') attachment = true, lastPosition = [e.clientX, e.clientY];
+            if(e.type == 'mouseup') attachment = false;
+            if(e.type == 'mousemove' && attachment == true ){
+                position = [e.clientX, e.clientY];
+                difference = [ (position[0]-lastPosition[0]), (position[1]-lastPosition[1])];
+                $(this).scrollLeft( $(this).scrollLeft() - difference[0]);
+                $(this).scrollTop( $(this).scrollTop() - difference[1]);
+                lastPosition = [e.clientX, e.clientY];
+            }
+        });
+        $(window).on('mouseup', () => attachment = false);
+    };
 
     // reset mobile filters if they're open past the tablet breakpoint
     $(window).resize(function resize(){
