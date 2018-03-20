dcd80adfb5615613a62e4566da94fd05dc2676bc
Todd Dembrey <todd.dembrey@torchbox.com>
2018-03-20 11:53:30 +0000

    Add extra permission checks for reviews

--- opentech/apply/funds/templatetags/workflow_tags.py
+++ opentech/apply/funds/templatetags/workflow_tags.py
@@ -3,6 +3,15 @@ from django import template
 register = template.Library()
 
 
-@register.filter()
+def check_permission(user, perm, submission):
+    return submission.phase.has_perm(user, perm, submission)
+
+
+@register.filter
 def can_edit(user, submission):
-    return submission.phase.has_perm(user, 'edit')
+    return check_permission(user, 'edit', submission)
+
+
+@register.filter
+def can_review(user, submission):
+    return check_permission(user, 'staff_review', submission) or check_permission(user, 'reviewer_review', submission)
