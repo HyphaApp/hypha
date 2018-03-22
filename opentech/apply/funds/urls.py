5b0b207b02ed4ec4d28e2a882a99ba9950be2561
Dan Braghis <dan.braghis@torchbox.com>
2018-03-22 09:37:39 +0000

    Add review detail and draft update view

--- opentech/apply/funds/urls.py
+++ opentech/apply/funds/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('submissions/', SubmissionListView.as_view(), name="submissions"),
     path('submissions/<int:pk>/', SubmissionDetailView.as_view(), name="submission"),
     path('submissions/<int:pk>/edit', SubmissionEditView.as_view(), name="edit_submission"),
-    path('submissions/<int:submission_pk>/review/', include('opentech.apply.review.urls', namespace="reviews")),
+    path('submissions/<int:submission_pk>/reviews/', include('opentech.apply.review.urls', namespace="reviews")),
     path('search', SubmissionSearchView.as_view(), name="search"),
 ]
