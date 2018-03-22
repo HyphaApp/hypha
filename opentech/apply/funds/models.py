5c95c66dbf9ddad53f2714dcf8e3e4df8470c8ee
Dan Braghis <dan.braghis@torchbox.com>
2018-03-22 13:35:43 +0000

    Fix missing @property post-merge

--- opentech/apply/funds/models.py
+++ opentech/apply/funds/models.py
@@ -670,6 +670,7 @@ class ApplicationSubmission(WorkflowHelpers, BaseStreamForm, AbstractFormSubmiss
     def missing_reviewers(self):
         return self.reviewers.exclude(id__in=self.reviews.values('author'))
 
+    @property
     def staff_not_reviewed(self):
         return self.missing_reviewers.staff()
 
