27daac738be266679087d2f214031024920ef8fe
Dan Braghis <dan.braghis@torchbox.com>
2018-03-22 10:37:19 +0000

    Lint

--- opentech/apply/review/templatetags/review_tags.py
+++ opentech/apply/review/templatetags/review_tags.py
@@ -37,9 +37,11 @@ def traffic_light(value):
 def can_review(user, submission):
     return submission.can_review(user)
 
+
 @register.filter
 def has_draft(user, submission):
     return submission.can_review(user) and submission.reviews.filter(author=user, is_draft=True).exists()
+
 
 @register.filter
 def draft_url(user, submission):
