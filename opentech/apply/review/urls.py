43c55909c215329da0103137463a2d0e29bd9719
Dan Braghis <dan.braghis@torchbox.com>
2018-03-22 09:37:39 +0000

    Add review detail and draft update view

--- opentech/apply/review/urls.py
+++ opentech/apply/review/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ReviewCreateView, ReviewListView
-
+from .views import ReviewCreateView, ReviewDetailView, ReviewListView, ReviewDraftEditView
 
 app_name = 'reviews'
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='list'),
     path('create', ReviewCreateView.as_view(), name='create'),
+    path('<int:pk>', ReviewDetailView.as_view(), name="review"),
+    path('<int:pk>/draft', ReviewDraftEditView.as_view(), name='draft'),
 ]
