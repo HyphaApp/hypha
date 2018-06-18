ee2d1a2bb0139a149e69dbb9ea4a378cbebc300d
Dan Braghis <dan.braghis@torchbox.com>
2018-06-18 12:27:05 +0100

    Update urls post #347 rebase

--- opentech/apply/determinations/urls.py
+++ opentech/apply/determinations/urls.py
@@ -6,5 +6,5 @@ app_name = 'determinations'
 
 urlpatterns = [
     path('determination/', DeterminationCreateOrUpdateView.as_view(), name="form"),
-    path('determination/<int:pk>', DeterminationDetailView.as_view(), name="determination"),
+    path('determination/<int:pk>', DeterminationDetailView.as_view(), name="detail"),
 ]
