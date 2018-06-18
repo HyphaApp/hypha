9121a7c6bb6ff2c8b247e537431989b53f516e24
Dan Braghis <dan.braghis@torchbox.com>
2018-06-18 12:29:48 +0100

    fixup! Re-add permission check method

--- opentech/apply/determinations/templatetags/determination_tags.py
+++ opentech/apply/determinations/templatetags/determination_tags.py
@@ -10,12 +10,12 @@ def can_add_determination(user, submission):
         has_determination = submission.determination
     except ObjectDoesNotExist:
         has_determination = False
-    return submission.has_permission_to_add_determination(user) and not has_determination
+    return submission.user_lead_or_admin(user) and not has_determination
 
 
 @register.filter
 def has_draft(user, submission):
     try:
-        return submission.has_permission_to_add_determination(user) and submission.determination.is_draft
+        return submission.user_lead_or_admin(user) and submission.determination.is_draft
     except ObjectDoesNotExist:
         return False
