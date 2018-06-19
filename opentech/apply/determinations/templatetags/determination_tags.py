8bb1da213ccb919ebea8cf1064a4f83085086b1b
Dan Braghis <dan.braghis@torchbox.com>
2018-06-19 17:36:04 +0100

    Simplify can_add_determination logic

--- opentech/apply/determinations/templatetags/determination_tags.py
+++ opentech/apply/determinations/templatetags/determination_tags.py
@@ -14,20 +14,16 @@ def is_lead_or_admin(user, submission):
 
 @register.filter
 def can_add_determination(user, submission):
-    """
-    A determination can be added when
-    - the user is admin or the submission lead
-    - the submission is in a discussion phase
-    - there was not determination response (i.e. accepted / rejected)
-    """
-    try:
-        has_determination_response = submission.determination.submitted
-    except ObjectDoesNotExist:
-        has_determination_response = False
+    if submission.status not in DETERMINATION_PHASES:
+        return False
 
-    return submission.has_permission_to_add_determination(user) \
-        and submission.status in DETERMINATION_PHASES \
-        and not has_determination_response
+    if not submission.has_permission_to_add_determination(user):
+        return False
+
+    try:
+        return not submission.determination.submitted
+    except ObjectDoesNotExist:
+        return True
 
 
 @register.filter
