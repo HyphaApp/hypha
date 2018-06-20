dafa5300c45a9e1296e2d03be151f95832029bc9
Dan Braghis <dan.braghis@torchbox.com>
2018-06-20 10:02:29 +0100

    Change determination url to /add

--- opentech/apply/determinations/urls.py
+++ opentech/apply/determinations/urls.py
@@ -6,5 +6,5 @@ app_name = 'determinations'
 
 urlpatterns = [
     path('determination/', DeterminationDetailView.as_view(), name="detail"),
-    path('determination/create', DeterminationCreateOrUpdateView.as_view(), name="form"),
+    path('determination/add', DeterminationCreateOrUpdateView.as_view(), name="form"),
 ]
