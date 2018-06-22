cb31926701fdbce0c48eb7532c82202f8dd1e938
Dan Braghis <dan.braghis@torchbox.com>
2018-06-22 10:03:25 +0100

    Clean up progress_stage

--- opentech/apply/determinations/views.py
+++ opentech/apply/determinations/views.py
@@ -83,17 +83,12 @@ class DeterminationCreateOrUpdateView(CreateOrUpdateView):
         return self.progress_stage(self.submission) or response
 
     def progress_stage(self, instance):
-        proposal_transition = instance.get_transition('draft_proposal')
-        if proposal_transition:
-            if can_proceed(proposal_transition):
-                proposal_transition(by=self.request.user)
-                instance.save()
+        try:
+            instance.perform_transition('draft_proposal', self.request.user)
+        except PermissionDenied:
+            pass
+        else:
             return HttpResponseRedirect(instance.get_absolute_url())
-
-    def form_invalid(self, form):
-        from pprint import pprint
-        pprint(form.errors)
-        return super().form_invalid(form)
 
     def get_action_name_from_determination(self, determination):
         suffix = DETERMINATION_TRANSITION_SUFFIX[determination]
