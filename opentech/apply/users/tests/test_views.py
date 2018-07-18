adccf0613c9623fcc528ede8f58ef97c032138fa
Todd Dembrey <todd.dembrey@torchbox.com>
2018-07-18 18:27:17 +0100

    Refactor the tests to use use the form not the view

--- opentech/apply/users/tests/test_views.py
+++ opentech/apply/users/tests/test_views.py
@@ -37,29 +37,9 @@ class TestProfileView(BaseTestProfielView):
         response = self.client.get(self.url)
         self.assertNotContains(response, reverse('users:password_change'))
 
-    def test_email_unique(self):
-        other_user = UserFactory()
-        data = self.form_data(self.user, email=other_user.email)
-        self.client.post(self.url, data=data)
-        self.user.refresh_from_db()
-        self.assertNotEqual(self.user.email, other_user.email)
-
-    def test_can_change_email(self):
-        new_email = 'me@another.com'
-        data = self.form_data(self.user, email=new_email)
-        self.client.post(self.url, data=data)
-        self.user.refresh_from_db()
-        self.assertEqual(self.user.email, new_email)
-
     def test_cant_set_slack_name(self):
         response = self.client.get(self.url)
-        self.assertNotContains(response, 'Slack Name')
-
-        slack_name = '@foobar'
-        data = self.form_data(self.user, slack=slack_name)
-        self.client.post(self.url, data=data)
-        self.user.refresh_from_db()
-        self.assertNotEqual(self.user.slack, slack_name)
+        self.assertNotContains(response, 'Slack name')
 
 
 class TestStaffProfileView(BaseTestProfielView):
@@ -68,41 +48,5 @@ class TestStaffProfileView(BaseTestProfielView):
         self.client.force_login(self.staff)
 
     def test_can_set_slack_name(self):
-        slack_name = '@foobar'
-        data = self.form_data(self.staff, slack=slack_name)
-
-        self.client.post(self.url, data=data)
-        self.staff.refresh_from_db()
-        self.assertEqual(self.staff.slack, slack_name)
-
-    def test_can_set_slack_name_with_trailing_space(self):
-        slack_name = '@foobar'
-        data = self.form_data(self.staff, slack=slack_name + ' ')
-
-        self.client.post(self.url, data=data)
-        self.staff.refresh_from_db()
-        self.assertEqual(self.staff.slack, slack_name)
-
-    def test_cant_set_slack_name_with_space(self):
-        slack_name = '@ foobar'
-        data = self.form_data(self.staff, slack=slack_name)
-
-        self.client.post(self.url, data=data)
-        self.staff.refresh_from_db()
-        self.assertNotEqual(self.staff.slack, slack_name)
-
-    def test_auto_prepend_at(self):
-        slack_name = 'foobar'
-        data = self.form_data(self.staff, slack=slack_name)
-
-        self.client.post(self.url, data=data)
-        self.staff.refresh_from_db()
-        self.assertEqual(self.staff.slack, '@' + slack_name)
-
-    def test_can_clear_slack_name(self):
-        slack_name = ''
-        data = self.form_data(self.staff, slack=slack_name)
-
-        self.client.post(self.url, data=data)
-        self.staff.refresh_from_db()
-        self.assertEqual(self.staff.slack, slack_name)
+        response = self.client.get(self.url)
+        self.assertContains(response, 'Slack name')
