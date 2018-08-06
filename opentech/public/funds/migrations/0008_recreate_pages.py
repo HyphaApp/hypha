500b18c76a3b9addd73ee773abc7714dc5331c41
Todd Dembrey <todd.dembrey@torchbox.com>
2018-08-06 12:29:25 +0100

    Add depth into the kwargs for recreating

--- opentech/public/funds/migrations/0008_recreate_pages.py
+++ opentech/public/funds/migrations/0008_recreate_pages.py
@@ -27,6 +27,7 @@ def recreate_objects(apps, schema_editor):
                 f'{new_model_name.lower()}_ptr': obj,
                 'draft_title': obj.draft_title,
                 'content_type': content_type,
+                'depth': obj.depth,
             }
             field_values.update(**kwargs)
             new_obj = model(**field_values)
