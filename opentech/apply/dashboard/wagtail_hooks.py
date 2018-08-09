023a5b8603c5fecb94f4990b371fc3ee4eac364d
Todd Dembrey <todd.dembrey@torchbox.com>
2018-08-09 14:37:46 +0100

    Make sure we can link after the urlconf changes

--- opentech/apply/dashboard/wagtail_hooks.py
+++ opentech/apply/dashboard/wagtail_hooks.py
@@ -1,14 +1,19 @@
+from urllib.parse import urljoin
+
 from django.urls import reverse
 
 from wagtail.core import hooks
 from wagtail.admin.menu import MenuItem
 
+from opentech.apply.home.models import ApplyHomePage
+
 
 @hooks.register('register_admin_menu_item')
 def register_dashboard_menu_item():
+    apply_home = ApplyHomePage.objects.first()
     return MenuItem(
         'Apply Dashboard',
-        reverse('dashboard:dashboard'),
+        urljoin(apply_home.url, reverse('dashboard:dashboard', 'opentech.apply.urls')),
         classnames='icon icon-arrow-left',
         order=100000,
     )
