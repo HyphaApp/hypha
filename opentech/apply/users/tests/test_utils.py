f3377381cde1787178bc68b212b17b151bac426a
Todd Dembrey <todd.dembrey@torchbox.com>
2018-08-09 17:40:27 +0100

    Add the missing conf

--- opentech/apply/users/tests/test_utils.py
+++ opentech/apply/users/tests/test_utils.py
@@ -1,11 +1,12 @@
 from django.core import mail
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from opentech.apply.users.tests.factories import UserFactory
 
 from ..utils import send_activation_email
 
 
+@override_settings(ROOT_URLCONF='opentech.apply.urls')
 class TestActivationEmail(TestCase):
     def test_activation_email_includes_link(self):
         send_activation_email(UserFactory())
