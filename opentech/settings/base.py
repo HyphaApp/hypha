2f47a41df4e315ce9efa49cbbd80ffd9ed38d4b1
Fredrik Jonsson <frjo@xdeb.org>
2018-08-12 21:57:00 +0200

    Correct AWS vars naming.

--- opentech/settings/base.py
+++ opentech/settings/base.py
@@ -381,6 +381,8 @@ else:
 if 'AWS_STORAGE_BUCKET_NAME' in env:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_STORAGE_BUCKET_NAME = env['AWS_STORAGE_BUCKET_NAME']
+    AWS_SECRET_ACCESS_KEY = env['AWS_SECRET_ACCESS_KEY'],
+    AWS_ACCESS_KEY_ID = env['AWS_ACCESS_KEY_ID'],
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
 
@@ -401,8 +403,6 @@ if 'AWS_STORAGE_BUCKET_NAME' in env:
         # Uses defaults from above if not provided
         APPLY_STORAGE_CONFIG = {
             'AWS_STORAGE_BUCKET_NAME': env['APPLY_AWS_BUCKET_NAME'],
-            'AWS_S3_SECRET_ACCESS_KEY': env['AWS_S3_SECRET_ACCESS_KEY'],
-            'AWS_S3_ACCESS_KEY_ID': env['AWS_S3_ACCESS_KEY_ID'],
             'AWS_QUERYSTRING_AUTH': True,
             'AWS_DEFAULT_ACL': 'private',
             'AWS_BUCKET_ACL': 'private',
