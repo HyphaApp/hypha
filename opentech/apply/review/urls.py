fde1ec19db3a71353ff9dbd3d04afc7a7dedbe2e
Todd Dembrey <todd.dembrey@torchbox.com>
2018-08-13 09:00:38 +0100

    Fix the review urls not resolving

--- opentech/apply/review/urls.py
+++ opentech/apply/review/urls.py
@@ -5,7 +5,7 @@ from .views import ReviewDetailView, ReviewListView, ReviewCreateOrUpdateView
 app_name = 'reviews'
 
 urlpatterns = [
-    path('reviews', ReviewListView.as_view(), name='list'),
+    path('reviews/', ReviewListView.as_view(), name='list'),
     path('reviews/<int:pk>', ReviewDetailView.as_view(), name="review"),
-    path('review', ReviewCreateOrUpdateView.as_view(), name='form'),
+    path('review/', ReviewCreateOrUpdateView.as_view(), name='form'),
 ]
