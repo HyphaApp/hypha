3ab0a2e1d9d5c05790483336af7b250a733ccdee
Fredrik Jonsson <frjo@xdeb.org>
2018-08-20 14:24:25 +0200

    Added test settings.

---
+++ opentech/settings/test.py
@@ -0,0 +1,34 @@
+import os
+
+from .base import *  # noqa
+
+# Do not set SECRET_KEY, Postgres or LDAP password or any other sensitive data here.
+# Instead, use environment variables or create a local.py file on the server.
+
+# Disable debug mode
+DEBUG = False
+
+
+# Cache everything for 10 minutes
+# This only applies to pages that do not have a more specific cache-control
+# setting. See urls.py
+CACHE_CONTROL_MAX_AGE = 600
+
+
+# Configuration from environment variables
+# Alternatively, you can set these in a local.py file on the server
+
+env = os.environ.copy()
+
+# Basic configuration
+
+APP_NAME = env.get('APP_NAME', 'opentech')
+
+SECURE_SSL_REDIRECT = False
+
+if 'SECRET_KEY' in env:
+    SECRET_KEY = env['SECRET_KEY']
+
+# Email config
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
