3015fd2fdb8561531e7b82ee60c86ee2a1052921
Todd Dembrey <todd.dembrey@torchbox.com>
2018-08-22 12:20:19 +0100

    Add tests to make sure we can add the funds

--- opentech/apply/home/factories.py
+++ opentech/apply/home/factories.py
@@ -9,6 +9,12 @@ class ApplyHomePageFactory(wagtail_factories.PageFactory):
         model = ApplyHomePage
 
     @factory.post_generation
+    def parent(self, create, extracted_parent, **parent_kwargs):
+        if create:
+            root = ApplyHomePage.get_first_root_node()
+            root.add_child(instance=self)
+
+    @factory.post_generation
     def site(self, create, extracted_site, **site_kwargs):
         if create:
             wagtail_factories.SiteFactory(root_page=self, is_default_site=True)
