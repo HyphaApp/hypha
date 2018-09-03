c5a587a3bae342cef19996388091597ed6bc4a7b
Todd Dembrey <todd.dembrey@torchbox.com>
2018-09-03 12:00:34 +0100

    Perform specific on the QS not the object

--- opentech/apply/home/models.py
+++ opentech/apply/home/models.py
@@ -24,6 +24,6 @@ class ApplyHomePage(Page):
 
     def get_context(self, *args, **kwargs):
         context = super().get_context(*args, **kwargs)
-        context['open_funds'] = ApplicationBase.objects.order_by_end_date()
-        context['open_labs'] = LabBase.objects.public().live()
+        context['open_funds'] = ApplicationBase.objects.order_by_end_date().specific()
+        context['open_labs'] = LabBase.objects.public().live().specific()
         return context
