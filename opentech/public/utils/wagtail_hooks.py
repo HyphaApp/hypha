dc2d7749ad640ae259991b2d889f2654e0b8584a
Erin Mullaney <erin.mullaney@torchbox.com>
2019-01-09 09:35:10 -0500

    #805 screening status taxonomy, screening status added to submission model

--- opentech/public/utils/wagtail_hooks.py
+++ opentech/public/utils/wagtail_hooks.py
@@ -1,5 +1,6 @@
 from wagtail.contrib.modeladmin.options import ModelAdminGroup, ModelAdmin, modeladmin_register
 
+from opentech.apply.funds.models import ScreeningStatus
 from opentech.public.news.models import NewsType
 from opentech.public.people.models import PersonType
 
@@ -13,10 +14,13 @@ class PersonTypeModelAdmin(ModelAdmin):
     model = PersonType
     menu_icon = 'tag'
 
+class ScreeningStatusModelAdmin(ModelAdmin):
+	model = ScreeningStatus
+	menu_icon = 'tag'
 
 class TaxonomiesModelAdminGroup(ModelAdminGroup):
     menu_label = "Taxonomies"
-    items = (NewsTypeModelAdmin, PersonTypeModelAdmin)
+    items = (NewsTypeModelAdmin, PersonTypeModelAdmin, ScreeningStatusModelAdmin)
     menu_icon = 'tag'
 
 
