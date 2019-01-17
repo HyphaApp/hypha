38e92bbfff6fb44807c494f1ad067e54f72e004e
Todd Dembrey <todd.dembrey@torchbox.com>
2019-01-17 18:18:42 +0000

    Fix labs not being returned

--- opentech/apply/funds/api_views.py
+++ opentech/apply/funds/api_views.py
@@ -1,11 +1,31 @@
+from django.db.models import Q
 from rest_framework import generics
 from rest_framework import permissions
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
+
+from wagtail.core.models import Page
 
 from opentech.api.pagination import StandardResultsSetPagination
 from .models import ApplicationSubmission
 from .serializers import SubmissionListSerializer, SubmissionDetailSerializer
 from .permissions import IsApplyStaffUser
+
+
+class RoundLabFilter(filters.ModelChoiceFilter):
+    def filter(self, qs, value):
+        if not value:
+            return qs
+
+        return qs.filter(Q(round=value) | Q(page=value))
+
+
+class SubmissionsFilter(filters.FilterSet):
+    # TODO replace with better call to Round and Lab base class
+    round = RoundLabFilter(queryset=Page.objects.all())
+
+    class Meta:
+        model = ApplicationSubmission
+        fields = ('status', 'round')
 
 
 class SubmissionList(generics.ListAPIView):
@@ -14,8 +34,8 @@ class SubmissionList(generics.ListAPIView):
     permission_classes = (
         permissions.IsAuthenticated, IsApplyStaffUser,
     )
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('round', 'status')
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = SubmissionsFilter
     pagination_class = StandardResultsSetPagination
 
 
