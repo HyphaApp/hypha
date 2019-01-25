49bf0a29e4a660d8c0e8192e1a7cc3826f5c5bf6
Chris Lawton <chris.lawton@torchbox.com>
2019-01-25 14:16:05 +0000

    start styling the no notes and error loading notes

--- opentech/static_src/src/app/src/components/Listing/index.js
+++ opentech/static_src/src/app/src/components/Listing/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import LoadingPanel from '@components/LoadingPanel';
 
+// import NoteIcon from 'images/note.svg';
+import SadNoteIcon from 'images/sad-note.svg';
+
 import './style.scss';
 
 export default class Listing extends React.Component {
@@ -61,8 +64,12 @@ export default class Listing extends React.Component {
         const { handleRetry, error } = this.props;
         const retryButton = <a onClick={handleRetry}>Refresh</a>;
         return (
-            <div className="listing__list is-loading">
-                <p>Something went wrong. Please try again later.</p>
+            <div className="listing__list is-blank">
+                <div className="listing__blank-icon">
+                    <SadNoteIcon  />
+                </div>
+                <p>Something went wrong!</p>
+                <p>Sorry we couldn&apos;t load the notes</p>
                 {error && <p>{error}</p>}
                 {handleRetry && retryButton}
             </div>
