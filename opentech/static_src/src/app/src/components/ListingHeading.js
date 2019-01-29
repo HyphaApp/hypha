1f2f609daa17d77f9d3829a4784ce8a272a8b81f
Chris Lawton <chris.lawton@torchbox.com>
2019-01-29 14:30:26 +0000

    li shouldn't be decedents of li

--- opentech/static_src/src/app/src/components/ListingHeading.js
+++ opentech/static_src/src/app/src/components/ListingHeading.js
@@ -5,10 +5,10 @@ export default class ListingHeading extends React.Component {
     render() {
         const parsedTitle = this.props.title.split(' ').join('-').toLowerCase();
         return (
-            <li className="listing__item listing__item--heading" id={parsedTitle}>
+            <div className="listing__item listing__item--heading" id={parsedTitle}>
                 <h5 className="listing__title">{this.props.title}</h5>
                 <span className="listing__count">{this.props.count}</span>
-            </li>
+            </div>
         );
     }
 }
