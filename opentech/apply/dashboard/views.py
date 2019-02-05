7366f96fc472ef53d907ba29fa0c9c73cee525e9
Erin Mullaney <erin.mullaney@torchbox.com>
2019-02-05 07:29:24 -0500

    #938 add Previous reviews block with filters to staff reviewer dashboard

--- opentech/apply/dashboard/views.py
+++ opentech/apply/dashboard/views.py
@@ -7,8 +7,10 @@ from opentech.apply.funds.models import ApplicationSubmission, RoundsAndLabs
 from opentech.apply.funds.tables import (
     AdminSubmissionsTable,
     ReviewerSubmissionsTable,
+    SubmissionFilterAndSearch,
     SubmissionReviewerFilterAndSearch,
     SubmissionsTable,
+    SummarySubmissionsTable,
 )
 from opentech.apply.utils.views import ViewDispatcher
 
@@ -18,8 +20,6 @@ class AdminDashboardView(TemplateView):
     def get(self, request, *args, **kwargs):
         qs = ApplicationSubmission.objects.all().for_table(self.request.user)
 
-        in_review = SubmissionsTable(qs.in_review_for(request.user), prefix='in-review-')
-        RequestConfig(request, paginate={'per_page': 10}).configure(in_review)
         base_query = RoundsAndLabs.objects.with_progress().active().order_by('-end_date')
         base_query = base_query.by_lead(request.user)
         open_rounds = base_query.open()[:6]
@@ -32,16 +32,33 @@ class AdminDashboardView(TemplateView):
         my_review = SummarySubmissionsTable(my_review_qs[:5], prefix='my-review-')
         display_more = (my_review_qs.count() > 5)
 
+        # Staff reviewer's reviewed submissions for 'Previous reviews' block
+        # Replicating django_filters.views.FilterView
+        my_reviewed_qs = qs.reviewed_by(request.user).order_by('-submit_time')
+        kwargs = {
+            'data': self.request.GET or None,
+            'request': self.request,
+        }
+        kwargs.update({
+            'queryset': my_reviewed_qs,
+        })
+        self.filterset = SubmissionFilterAndSearch(**kwargs)
+        my_reviewed_qs = self.filterset.qs
+
+        my_reviewed = SummarySubmissionsTable(my_reviewed_qs[:5], prefix='my-reviewed-')
+        display_more_reviewed = (my_reviewed_qs.count() > 5)
+
         return render(request, 'dashboard/dashboard.html', {
-            'in_review': in_review,
             'open_rounds': open_rounds,
             'open_query': open_query,
             'closed_rounds': closed_rounds,
             'closed_query': closed_query,
             'rounds_title': rounds_title,
             'my_review': my_review,
-            'in_review_count': my_review_qs.count(),
             'display_more': display_more,
+            'my_reviewed': my_reviewed,
+            'display_more_reviewed': display_more_reviewed,
+            'filter': self.filterset,
         })
 
 
