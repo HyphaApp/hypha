5e736e890c963c1878c55276cbe561da157777bd
Erin Mullaney <erin.mullaney@torchbox.com>
2019-02-05 11:10:08 -0500

    #957 ReviewerRole model and taxonomy

---
+++ opentech/apply/funds/models/reviewer_role.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class ReviewerRole(models.Model):
+    name = models.CharField(max_length=128)
+    icon = models.ForeignKey(
+        'images.CustomImage',
+        null=True,
+        blank=True,
+        related_name='+',
+        on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
