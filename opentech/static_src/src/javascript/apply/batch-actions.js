dc7ab638d5b930ce89f77883d20ee18676ac8d98
Todd Dembrey <todd.dembrey@torchbox.com>
2019-02-19 09:02:33 +0000

    Limit options in the dropdown based on selection

--- opentech/static_src/src/javascript/apply/batch-actions.js
+++ opentech/static_src/src/javascript/apply/batch-actions.js
@@ -6,6 +6,7 @@
     const $checkbox = $('.js-batch-select');
     const $allCheckboxInput = $('.js-batch-select-all');
     const $batchButtons = $('.js-batch-button');
+    const $batchProgress = $('.js-batch-progress');
     const $batchTitlesList = $('.js-batch-titles');
     const $batchTitleCount = $('.js-batch-title-count');
     const $hiddenIDlist = $('#id_submission_ids');
@@ -50,31 +51,25 @@
     // append selected project titles to batch update reviewer modal
     $batchButtons.each(function () {
         $(this).click(function () {
-            $batchTitlesList.html('');
-            $batchTitleCount.html('');
-            $batchTitlesList.addClass(closedClass);
-            $toggleBatchList.html('Show');
-
-            let selectedIDs = [];
-
-            $checkbox.each(function () {
-                if ($(this).is(':checked')) {
-                    const href = $(this).parents('tr').find('.js-title').find('a').attr('href');
-                    const title = $(this).parents('tr').find('.js-title').data('tooltip');
-
-                    $batchTitlesList.append(`
-                        <a href="${href}" class="modal__list-item" target="_blank" rel="noopener noreferrer" title="${title}">
-                            ${title}
-                            <svg class="modal__open-link-icon"><use xlink:href="#open-in-new-tab"></use></svg>
-                        </a>
-                    `);
-                    selectedIDs.push($(this).parents('tr').data('record-id'));
-                }
-            });
-
-            $batchTitleCount.append(`${selectedIDs.length} submissions selected`);
-            $hiddenIDlist.val(selectedIDs.join(','));
+            prepareBatchListing();
         });
+    });
+
+    $batchProgress.click(function () {
+        const $actionOptions = $('#id_action option');
+        var actions = $actionOptions.map(function() { return this.value; }).get();
+        $checkbox.filter(':checked').each(function () {
+            let newActions = $(this).parents('tr').find('.js-actions').data('actions');
+            actions = actions.filter(action => newActions.includes(action));
+        });
+        $actionOptions.each(function() {
+            if (!actions.includes(this.value)) {
+                $(this).attr("disabled", "disabled");
+            } else {
+                $(this).removeAttr("disabled");
+            }
+        });
+        $actionOptions.filter(':enabled:first').prop('selected', true);;
     });
 
     // show/hide the list of actions
@@ -90,6 +85,31 @@
 
         $batchTitlesList.toggleClass(closedClass);
     });
+
+    function prepareBatchListing() {
+        $batchTitlesList.html('');
+        $batchTitleCount.html('');
+        $batchTitlesList.addClass(closedClass);
+        $toggleBatchList.html('Show');
+
+        let selectedIDs = [];
+
+        $checkbox.filter(':checked').each(function () {
+            const href = $(this).parents('tr').find('.js-title').find('a').attr('href');
+            const title = $(this).parents('tr').find('.js-title').data('tooltip');
+
+            $batchTitlesList.append(`
+                <a href="${href}" class="modal__list-item" target="_blank" rel="noopener noreferrer" title="${title}">
+                    ${title}
+                    <svg class="modal__open-link-icon"><use xlink:href="#open-in-new-tab"></use></svg>
+                </a>
+            `);
+            selectedIDs.push($(this).parents('tr').data('record-id'));
+        });
+
+        $batchTitleCount.append(`${selectedIDs.length} submissions selected`);
+        $hiddenIDlist.val(selectedIDs.join(','));
+    }
 
     function toggleBatchActions() {
         if ($('.js-batch-select:checked').length) {
