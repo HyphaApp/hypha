4911463708ff61c5f96c50e504a0904ab7cc988d
Chris Lawton <chris.lawton@torchbox.com>
2019-02-19 17:18:43 +0000

    flake 8

--- opentech/apply/utils/image.py
+++ opentech/apply/utils/image.py
@@ -1,9 +1,8 @@
 from django.urls import reverse
 from django.utils.html import format_html
-from django.utils.text import mark_safe
 
 from wagtail.images.views.serve import generate_signature
-from opentech.images.models import CustomImage
+
 
 def generate_image_url(image, filter_spec):
     signature = generate_signature(image.id, filter_spec)
