68b7d77315d93c6c6a56a1dd2fd2e35a499809ab
Tomasz Knapik <tomasz.knapik@torchbox.com>
2019-02-20 17:14:40 +0000

    GH-983: Flake8

--- opentech/apply/funds/api_views.py
+++ opentech/apply/funds/api_views.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import PermissionDenied as DjangoPermissionDenied
-from django.db import transaction
 from django.db.models import Q
 from rest_framework import generics, permissions
 from rest_framework.response import Response
