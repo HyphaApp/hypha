2805f4c5a35b185a6612b8022af7a70dc649f36d
Parbhat Puri <parbhatpuri17@gmail.com>
2019-05-01 07:44:54 +0000

    use feeds module

--- opentech/public/urls.py
+++ opentech/public/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 
-from .news import views as news_views
 from .search import views as search_views
 from .mailchimp import urls as newsletter_urls
-
+from .news import feeds as news_feeds
 
 urlpatterns = [
     path('search/', search_views.search, name='search'),
-    path('news/feed/', news_views.NewsFeed(), name='news_feed'),
-    path('news/<int:news_type>/feed/', news_views.NewsTypeFeed(), name='news_types_feed'),
+    path('news/feed/', news_feeds.NewsFeed(), name='news_feed'),
+    path('news/<int:news_type>/feed/', news_feeds.NewsTypeFeed(), name='news_type_feed'),
     path('newsletter/', include(newsletter_urls))
 ]
