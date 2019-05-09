ebfcccc1095a693a941f4cb346576e9047100b90
Chris Lawton <chris.lawton@torchbox.com>
2019-05-09 15:22:48 +0100

    initial work on updating comments

--- opentech/static_src/src/javascript/apply/edit-comment.js
+++ opentech/static_src/src/javascript/apply/edit-comment.js
@@ -2,11 +2,23 @@
 
     'use strict';
 
-    $('.js-edit-comment').click(function (e) {
+    const comment = '.js-comment';
+    const pageDown = '.js-pagedown';
+    const feedMeta = '.js-feed-meta';
+    const editBlock = '.js-edit-block';
+    const lastEdited = '.js-last-edited';
+    const editButton = '.js-edit-comment';
+    const feedContent = '.js-feed-content';
+    const cancelEditButton = '.js-cancel-edit';
+    const submitEditButton = '.js-submit-edit';
+
+    // handle edit
+    $(editButton).click(function (e) {
         e.preventDefault();
-        const editFormWrapper = $(this).closest('.js-feed-content').children('.js-edit-form');
-        const commentWrapper = $(this).closest('.js-feed-content').children('.js-comment');
-        const commentContents = $(this).closest('.js-feed-content').children('.js-comment').data('comment');
+
+        const editBlockWrapper = $(this).closest(feedContent).find(editBlock);
+        const commentWrapper = $(this).closest(feedContent).find(comment);
+        const commentContents = $(commentWrapper).data('comment');
 
         // hide the edit link and original comment
         $(this).hide();
@@ -17,27 +29,80 @@
                 <div id="wmd-button-bar-edit-comment" class="wmd-button-bar"></div>
                 <textarea id="wmd-input-edit-comment" class="wmd-input" rows="10">${commentContents}</textarea>
                 <div id="wmd-preview-edit-comment" class="wmd-preview"></div>
-                <div class="wrapper--top-outer-space-small">
-                    <button class="button button--primary" type="submit">Update</button>
+                <div class="wrapper--outer-space-medium">
+                    <button class="button button--primary js-submit-edit" type="submit">Update</button>
                     <button class="button button--white js-cancel-edit">Cancel</button>
                 </div>
             </div>
         `;
 
         // add the comment to the editor
-        $(editFormWrapper).append(markup);
+        $(editBlockWrapper).append(markup);
 
         // run the editor
-        const converterOne = Markdown.getSanitizingConverter();
-        const commentEditor = new Markdown.Editor(converterOne, '-edit-comment');
+        initEditor();
+    });
+
+    // handle cancel
+    $(document).on('click', cancelEditButton, function () {
+        showComment(this);
+        showEditButton(this);
+        hidePageDownEditor(this);
+    });
+
+    // handle submit
+    $(document).on('click', submitEditButton, function () {
+        const commentContainer = $(this).closest(editBlock).siblings(comment);
+        const id = $(commentContainer).data('id');
+        const editedComment = $(this).closest(pageDown).find('.wmd-preview').html();
+
+        // TODO - get correct URL
+        const url = `${window.location.origin}/apply/api/comments/${id}/edit/`;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': $.cookie('csrftoken')
+            },
+            body: JSON.stringify({
+                message: editedComment
+            })
+
+        }).then(response => response.json())
+        .then(data => {
+            updateComment(commentContainer, data.id, data.message);
+            updateLastEdited(this, data.edited);
+            showComment(this);
+            showEditButton(this);
+            hidePageDownEditor(this);
+        });
+    });
+
+    const initEditor = () => {
+        const converterOne = window.Markdown.getSanitizingConverter();
+        const commentEditor = new window.Markdown.Editor(converterOne, '-edit-comment');
         commentEditor.run();
-    });
+    };
 
-    $(document).on('click', '.js-cancel-edit', function () {
-        // show the comment, edit button and remove the editor
-        $(this).closest('.js-edit-form').prev('.js-comment').show();
-        $(this).closest('.js-edit-form').siblings('.js-feed-meta').find('.js-edit-comment').show();
-        $(this).closest('.js-pagedown').remove();
-    });
+    const showEditButton = (el) => {
+        $(el).closest(editBlock).siblings(feedMeta).find(editButton).show();
+    };
 
+    const hidePageDownEditor = (el) => {
+        $(el).closest(pageDown).remove();
+    };
+
+    const showComment = (el) => {
+        $(el).closest(editBlock).siblings(comment).show();
+    };
+
+    // TODO - parse date
+    const updateLastEdited = (el, date) => {
+        $(el).closest(feedContent).find(lastEdited).html(date);
+    };
+
+    const updateComment = (el, id, newComment) => {
+        $(el).html(newComment).data('comment', newComment).data('id', id);
+    };
 })(jQuery);
