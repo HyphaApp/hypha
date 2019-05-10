f4bebfdaa82ca90ba453509a94fabf8cb24b3fb0
Chris Lawton <chris.lawton@torchbox.com>
2019-05-10 12:12:55 +0100

    add tomd and convert comment html to markdown

--- opentech/apply/funds/templatetags/markdown_tags.py
+++ opentech/apply/funds/templatetags/markdown_tags.py
@@ -1,11 +1,16 @@
 import mistune
+import tomd
 
 from django import template
 
 register = template.Library()
 
+mistune_markdown = mistune.Markdown()
 
 @register.filter
 def markdown(value):
-    markdown = mistune.Markdown()
-    return markdown(value)
+    return mistune_markdown(value)
+
+@register.filter
+def to_markdown(value):
+    return tomd.convert(value)
