83919af40628734a5337f5ec8537a4e5bada94eb
George Hickman <ghickman@users.noreply.github.com>
2019-08-01 09:07:36 +0100

    Copy Submission fields on Project Creation (#1358)
    * Copy submission fields onto Project during creation of Project
    * Display Project fields on detail page
    * test creating Projects from a submission

---
+++ opentech/apply/projects/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from opentech.apply.funds.tests.factories import ApplicationSubmissionFactory
+
+from ..models import Project
+
+
+class TestProjectModel(TestCase):
+    def test_create_from_submission(self):
+        submission = ApplicationSubmissionFactory()
+
+        project = Project.create_from_submission(submission)
+
+        self.assertEquals(project.submission, submission)
+        self.assertEquals(project.name, submission.title)
