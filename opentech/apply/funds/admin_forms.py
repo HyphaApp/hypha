b4b0cfde3208aacec65320fa9bb3e20139de22f1
Parbhat Puri <parbhatpuri17@gmail.com>
2019-08-05 13:58:44 +0000

    GH-717: Admin changes to allow multiple forms in second stage

--- opentech/apply/funds/admin_forms.py
+++ opentech/apply/funds/admin_forms.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from wagtail.admin.forms import WagtailAdminPageForm
 
 from .workflow import WORKFLOWS
@@ -10,11 +11,41 @@ class WorkflowFormAdminForm(WagtailAdminPageForm):
         workflow = WORKFLOWS[cleaned_data['workflow_name']]
         application_forms = self.formsets['forms']
         review_forms = self.formsets['review_forms']
+        number_of_stages = len(workflow.stages)
 
-        self.validate_stages_equal_forms(workflow, application_forms)
+        self.validate_application_forms(workflow, application_forms)
+        if number_of_stages == 1:
+            self.validate_stages_equal_forms(workflow, application_forms)
         self.validate_stages_equal_forms(workflow, review_forms, form_type="Review form")
 
         return cleaned_data
+
+    def validate_application_forms(self, workflow, forms):
+        """
+        Application forms are not equal to the number of stages like review forms.
+        Now, staff can select a proposal form from multiple forms list in stage 2.
+        """
+        if forms.is_valid():
+            valid_forms = [form for form in forms if not form.cleaned_data['DELETE']]
+            forms_stages = [form.cleaned_data['stage'] for form in valid_forms]
+            stages_counter = Counter(forms_stages)
+
+            number_of_stages = len(workflow.stages)
+            error_list = []
+
+            for stage in range(1, number_of_stages + 1):
+                is_form_present = True if stages_counter.get(stage, 0) > 0 else False
+                if not is_form_present:
+                    error_list.append(f'Please provide form for Stage {stage}.')
+
+                if stage == 1 and stages_counter.get(stage, 0) > 1:
+                    error_list.append('Only 1 form can be selected for 1st Stage.')
+
+            if error_list:
+                self.add_error(
+                    None,
+                    error_list,
+                )
 
     def validate_stages_equal_forms(self, workflow, forms, form_type="form"):
         if forms.is_valid():
