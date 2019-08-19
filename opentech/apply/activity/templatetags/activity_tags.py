8bd91fc6cfcc689500f5d80f36e4c148e76f77c2
Todd Dembrey <todd.dembrey@torchbox.com>
2019-08-19 10:15:51 +0100

    Improve general performance on listing page and activities
    * Tweak pre-fetching to fetch the correct things
    * Defer checking of user in activity until we are sure we need to

--- opentech/apply/activity/templatetags/activity_tags.py
+++ opentech/apply/activity/templatetags/activity_tags.py
@@ -12,7 +12,7 @@ register = template.Library()
 
 @register.filter
 def display_author(activity, user):
-    if activity.source.user == user and isinstance(activity.related_object, Review):
+    if isinstance(activity.related_object, Review) and activity.source.user == user:
         return 'Reviewer'
     return activity.user
 
