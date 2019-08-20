0eb170d3690e692c42df29a2b770126219d54a99
George Hickman <george@ghickman.co.uk>
2019-08-20 10:32:20 +0100

    Test contract templatetags to confirm they show at the correct time

--- opentech/apply/projects/models.py
+++ opentech/apply/projects/models.py
@@ -150,12 +150,13 @@ COMMITTED = 'committed'
 CONTRACTING = 'contracting'
 IN_PROGRESS = 'in_progress'
 CLOSING = 'closing'
+COMPLETE = 'complete'
 PROJECT_STATUS_CHOICES = [
     (COMMITTED, 'Committed'),
     (CONTRACTING, 'Contracting'),
     (IN_PROGRESS, 'In Progress'),
     (CLOSING, 'Closing'),
-    ('complete', 'Complete'),
+    (COMPLETE, 'Complete'),
 ]
 
 
