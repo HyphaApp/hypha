4917254a0644cf767e7233e8639dd75b7abc44d0
Fredrik Jonsson <frjo@xdeb.org>
2019-09-03 11:11:55 +0200

    Use settings for organisation name etc. in templates.

--- opentech/apply/users/utils.py
+++ opentech/apply/users/utils.py
@@ -39,12 +39,13 @@ def send_activation_email(user, site=None):
         'username': user.get_username(),
         'activation_path': activation_path,
         'timeout_days': settings.PASSWORD_RESET_TIMEOUT_DAYS,
+        'org_long_name': settings.ORG_LONG_NAME,
     }
 
     if site:
         context.update(site=site)
 
-    subject = 'Account details for {username} at Open Technology Fund'.format(**context)
+    subject = 'Account details for {username} at {org_long_name}'.format(**context)
     # Force subject to a single line to avoid header-injection issues.
     subject = ''.join(subject.splitlines())
     message = render_to_string('users/activation/email.txt', context)
