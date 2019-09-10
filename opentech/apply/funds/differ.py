b4cc2a27bd062f033b5606fbf313f17c920922de
Fredrik Jonsson <frjo@xdeb.org>
2019-09-10 22:06:57 +0200

    Implement first stab at side by side revision diff.

--- opentech/apply/funds/differ.py
+++ opentech/apply/funds/differ.py
@@ -1,6 +1,5 @@
-from bleach.sanitizer import Cleaner
-from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
+from django_bleach.templatetags.bleach_tags import bleach_value
 
 from django.utils.html import format_html
 from django.utils.text import mark_safe
@@ -18,67 +17,27 @@ def wrap_added(text):
     return wrap_with_span(text, 'added')
 
 
-def compare(answer_a, answer_b, should_bleach=True):
-    if not answer_a and not answer_b:
-        # This catches the case where both results are None and we cant compare
-        return answer_b
-    if isinstance(answer_a, dict) or isinstance(answer_b, dict):
-        # TODO: handle file dictionaries
-        return answer_b
-
-    if should_bleach:
-        cleaner = Cleaner(tags=['h4'], attributes={}, strip=True)
-        if isinstance(answer_a, str):
-            answer_a = cleaner.clean(answer_a)
-        else:
-            answer_a = str(answer_a)
-
-        if isinstance(answer_b, str):
-            answer_b = cleaner.clean(answer_b)
-        else:
-            answer_b = str(answer_b)
-
+def compare(answer_a, answer_b):
     diff = SequenceMatcher(None, answer_a, answer_b)
-    output = []
-    added = []
-    deleted = []
+    from_diff = []
+    to_diff = []
     for opcode, a0, a1, b0, b1 in diff.get_opcodes():
         if opcode == 'equal':
-            if a1 - a0 > 2 or not (added or deleted):
-                # if there are more than two of the same characters, commit the added and removed text
-                if added:
-                    output.append(wrap_added(''.join(added)))
-                    added = []
-                if deleted:
-                    output.append(wrap_deleted(''.join(deleted)))
-                    deleted = []
-                output.append(diff.a[a0:a1])
-            else:
-                # Ignore the small gap pretend it has been both added and removed
-                added.append(diff.a[a0:a1])
-                deleted.append(diff.a[a0:a1])
+            from_diff.append(mark_safe(diff.a[a0:a1]))
+            to_diff.append(mark_safe(diff.b[b0:b1]))
         elif opcode == 'insert':
-            added.append(diff.b[b0:b1])
+            from_diff.append(mark_safe(diff.a[a0:a1]))
+            to_diff.append(wrap_with_span(diff.b[b0:b1], 'added'))
         elif opcode == 'delete':
-            deleted.append(diff.a[a0:a1])
+            from_diff.append(wrap_with_span(diff.a[a0:a1], 'deleted'))
+            to_diff.append(mark_safe(diff.b[b0:b1]))
         elif opcode == 'replace':
-            deleted.append(diff.a[a0:a1])
-            added.append(diff.b[b0:b1])
+            from_diff.append(wrap_with_span(diff.a[a0:a1], 'deleted'))
+            to_diff.append(wrap_with_span(diff.b[b0:b1], 'added'))
 
-    # Handle text not added to the output already
-    if added == deleted:
-        output.append(''.join(added))
-    else:
-        if deleted:
-            output.append(wrap_deleted(''.join(deleted)))
-        if added:
-            output.append(wrap_added(''.join(added)))
+    from_display = ''.join(from_diff)
+    to_display = ''.join(to_diff)
+    from_display = bleach_value(from_display)
+    to_display = bleach_value(to_display)
 
-    display = ''.join(output)
-
-    if not should_bleach:
-        soup = BeautifulSoup(display, "html5lib")
-        soup.body.hidden = True
-        display = soup.body.prettify()
-
-    return mark_safe(display)
+    return (from_display, to_display)
