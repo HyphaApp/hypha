3f962e856b241f3f59ea98e78d62e725d13d7368
Todd Dembrey <todd.dembrey@torchbox.com>
2019-09-10 16:17:20 +0100

    Allow passing a path to the slack notification system
    User.get_absolute_url was pointing at the wagtail admin which would expose the
    admin urls to users on the frontend. The admin view is not necessarily the
    "canonical" view for the user so should not be relied upon in the long run.
    
    By allowing passing of a url to the slack notifier we allow the emitter to point
    to the destination regardless of the more front end focused model method.

--- opentech/apply/users/wagtail_hooks.py
+++ opentech/apply/users/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import reverse
 
 from wagtail.core import hooks
 
@@ -20,6 +21,7 @@ def notify_after_create_user(request, user):
         message=f'{request.user} has crated a new account for {user}.',
         request=request,
         related=user,
+        path=reverse('wagtailusers_users:edit', args=(user.id,))
     )
 
 
@@ -32,4 +34,5 @@ def notify_after_edit_user(request, user):
             message=f'{request.user} has edited the account for {user} that now have these roles: {roles}.',
             request=request,
             related=user,
+            path=reverse('wagtailusers_users:edit', args=(user.id,))
         )
