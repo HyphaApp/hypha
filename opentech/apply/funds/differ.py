2657d638ef921df1fc084b98055dd770a8cdbe0a
Fredrik Jonsson <frjo@xdeb.org>
2019-09-16 15:49:57 +0200

    Side by side diff with plain text but addning in line breaks with regexp. A hack but it might make the text easier to read.

--- opentech/apply/funds/differ.py
+++ opentech/apply/funds/differ.py
@@ -1,5 +1,7 @@
+import re
+
+from bleach.sanitizer import Cleaner
 from difflib import SequenceMatcher
-from django_bleach.templatetags.bleach_tags import bleach_value
 
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
@@ -17,7 +19,14 @@ def wrap_added(text):
     return wrap_with_span(text, 'added')
 
 
-def compare(answer_a, answer_b):
+def compare(answer_a, answer_b, should_bleach=True):
+    if should_bleach:
+        cleaner = Cleaner(tags=['h4'], attributes={}, strip=True)
+        answer_a = re.sub('(<li[^>]*>)', r'\1● ', answer_a)
+        answer_b = re.sub('(<li[^>]*>)', r'\1● ', answer_b)
+        answer_a = cleaner.clean(answer_a)
+        answer_b = cleaner.clean(answer_b)
+
     diff = SequenceMatcher(None, answer_a, answer_b)
     from_diff = []
     to_diff = []
@@ -37,7 +46,11 @@ def compare(answer_a, answer_b):
 
     from_display = ''.join(from_diff)
     to_display = ''.join(to_diff)
-    from_display = bleach_value(from_display)
-    to_display = bleach_value(to_display)
+    from_display = re.sub('([●○]|[0-9]{1,2}[\)\.])', r'<br>\1', from_display)
+    to_display = re.sub('([●○]|[0-9]{1,2}[\)\.])', r'<br>\1', to_display)
+    from_display = re.sub('(\.\n)', r'\1<br><br>', from_display)
+    to_display = re.sub('(\.\n)', r'\1<br><br>', to_display)
+    from_display = mark_safe(from_display)
+    to_display = mark_safe(to_display)
 
     return (from_display, to_display)
