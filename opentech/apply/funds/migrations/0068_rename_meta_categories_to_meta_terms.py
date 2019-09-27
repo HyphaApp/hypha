520b29d1abddacb4c20e231c3ab30f2e094bb878
Fredrik Jonsson <frjo@xdeb.org>
2019-09-27 13:54:04 +0200

    Update migrations after rebase.

--- opentech/apply/funds/migrations/0068_rename_meta_categories_to_meta_terms.py
+++ opentech/apply/funds/migrations/0068_rename_meta_categories_to_meta_terms.py
@@ -7,7 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('categories', '0003_rename_meta_categories_to_meta_terms'),
-        ('funds', '0065_applicationsubmission_meta_categories'),
+        ('funds', '0067_data_migration_for_one_form_per_stage'),
     ]
 
     operations = [
