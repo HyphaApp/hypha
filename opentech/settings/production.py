289beef71b328634dac095df0baa65154e9616e0
Fredrik Jonsson <frjo@xdeb.org>
2019-10-07 11:00:06 +0200

    Make the production settings work on more enviroments than Heroku.

--- opentech/settings/production.py
+++ opentech/settings/production.py
@@ -1,19 +1,18 @@
 import os
 
-import django_heroku
-import sentry_sdk
-
-from sentry_sdk.integrations.django import DjangoIntegration
-
 from .base import *  # noqa
 
 # Disable debug mode
 DEBUG = False
 
 # Configuration from environment variables
-# Alternatively, you can set these in a local.py file on the server
-
 env = os.environ.copy()
+
+# Alternatively, you can set these in a local.py file on the server
+try:
+     from .local import *  # noqa
+except ImportError:
+    pass
 
 # Mailgun configuration.
 if 'MAILGUN_API_KEY' in env:
@@ -26,6 +25,8 @@ if 'MAILGUN_API_KEY' in env:
 
 # Sentry configuration.
 if 'SENTRY_DSN' in env:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
     sentry_sdk.init(
         dsn=env['SENTRY_DSN'],
         environment=env.get('SENTRY_ENVIRONMENT', None),
@@ -33,4 +34,7 @@ if 'SENTRY_DSN' in env:
     )
 
 # Heroku configuration.
-django_heroku.settings(locals())
+# Set ON_HEROKU to true in Config Vars or via cli "heroku config:set ON_HEROKU=true".
+if 'ON_HEROKU' in env:
+    import django_heroku
+    django_heroku.settings(locals())
