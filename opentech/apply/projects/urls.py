c3f18773825a6e50eeff3a4cf7c36199080b6b20
Fredrik Jonsson <frjo@xdeb.org>
2019-10-21 11:32:59 +0200

    Expose project urls even when projects are not enabled. Otherwise the dashboard can not load.

--- opentech/apply/projects/urls.py
+++ opentech/apply/projects/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.urls import include, path
 
 from .views import (
@@ -19,28 +18,25 @@ from .views import (
 
 app_name = 'projects'
 
-urlpatterns = []
-
-if settings.PROJECTS_ENABLED:
-    urlpatterns = [
-        path('', ProjectOverviewView.as_view(), name='overview'),
-        path('all/', ProjectListView.as_view(), name='all'),
+urlpatterns = [
+    path('', ProjectOverviewView.as_view(), name='overview'),
+    path('all/', ProjectListView.as_view(), name='all'),
+    path('<int:pk>/', include([
+        path('', ProjectDetailView.as_view(), name='detail'),
+        path('edit/', ProjectEditView.as_view(), name="edit"),
+        path('documents/<int:file_pk>/', ProjectPrivateMediaView.as_view(), name="document"),
+        path('contract/<int:file_pk>/', ContractPrivateMediaView.as_view(), name="contract"),
+        path('simplified/', ProjectDetailSimplifiedView.as_view(), name='simplified'),
+        path('request/', CreatePaymentRequestView.as_view(), name='request'),
+    ])),
+    path('payment-requests/', include(([
+        path('', PaymentRequestListView.as_view(), name='all'),
         path('<int:pk>/', include([
-            path('', ProjectDetailView.as_view(), name='detail'),
-            path('edit/', ProjectEditView.as_view(), name="edit"),
-            path('documents/<int:file_pk>/', ProjectPrivateMediaView.as_view(), name="document"),
-            path('contract/<int:file_pk>/', ContractPrivateMediaView.as_view(), name="contract"),
-            path('simplified/', ProjectDetailSimplifiedView.as_view(), name='simplified'),
-            path('request/', CreatePaymentRequestView.as_view(), name='request'),
+            path('', PaymentRequestView.as_view(), name='detail'),
+            path('edit/', EditPaymentRequestView.as_view(), name='edit'),
+            path('delete/', DeletePaymentRequestView.as_view(), name='delete'),
+            path('documents/invoice/', PaymentRequestPrivateMedia.as_view(), name="invoice"),
+            path('documents/receipt/<int:file_pk>/', PaymentRequestPrivateMedia.as_view(), name="receipt"),
         ])),
-        path('payment-requests/', include(([
-            path('', PaymentRequestListView.as_view(), name='all'),
-            path('<int:pk>/', include([
-                path('', PaymentRequestView.as_view(), name='detail'),
-                path('edit/', EditPaymentRequestView.as_view(), name='edit'),
-                path('delete/', DeletePaymentRequestView.as_view(), name='delete'),
-                path('documents/invoice/', PaymentRequestPrivateMedia.as_view(), name="invoice"),
-                path('documents/receipt/<int:file_pk>/', PaymentRequestPrivateMedia.as_view(), name="receipt"),
-            ])),
-        ], 'payments'))),
-    ]
+    ], 'payments'))),
+]
