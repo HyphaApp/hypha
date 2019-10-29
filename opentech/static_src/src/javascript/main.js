c8987d0db3dd031957ff4b54e59c3310cfe70164
Fredrik Jonsson <frjo@xdeb.org>
2019-10-29 20:47:58 +0100

    Add flag feature to allow staff as well as each users to flag submissions.

--- opentech/static_src/src/javascript/main.js
+++ opentech/static_src/src/javascript/main.js
@@ -1,4 +1,3 @@
-
 (function ($) {
 
     'use strict';
@@ -155,6 +154,36 @@
         const headerHeight = $('.header').outerHeight();
         const adminbarHeight = $('.admin-bar').outerHeight();
         document.documentElement.style.setProperty('--header-admin-height', headerHeight + adminbarHeight + 'px');
+    });
+
+    // Setting the CSRF token on AJAX requests.
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader('X-CSRFToken', csrftoken);
+            }
+        }
     });
 
 })(jQuery);
