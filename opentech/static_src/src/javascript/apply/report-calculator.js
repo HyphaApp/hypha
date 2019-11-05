e3c0dbb2dedad996a799dea78a3682c2b1271e8a
Chris Lawton <chris.lawton@torchbox.com>
2019-11-05 15:06:05 +0000

    add start and end reporting period dates

--- opentech/static_src/src/javascript/apply/report-calculator.js
+++ opentech/static_src/src/javascript/apply/report-calculator.js
@@ -7,19 +7,26 @@
     // Form inputs
     const frequencyNumberInput = document.getElementById('id_occurrence');
     const frequencyPeriodSelect = document.getElementById('id_frequency');
+    const startDateInput = document.getElementById('id_start');
 
-    // Form info slots
+    // Form slots
     const projectEndSlot = document.querySelector('.js-project-end-slot');
     const frequencyNumberSlot = document.querySelector('.js-frequency-number-slot');
     const frequencyPeriodSlot = document.querySelector('.js-frequency-period-slot');
+    const periodStartSlot = document.querySelector('.js-report-period-start');
+    const periodEndSlot = document.querySelector('.js-report-period-end');
 
 
     function init() {
         console.log(reportData);
+        // Set on page load
         setProjectEnd();
         setFrequency();
-        addFrequencyEvents();
+        setReportPeriodStart();
 
+        // Add event listeners
+        addFrequencyEvents();
+        addReportPeriodEvents();
     }
 
     // Sets the project end date in the form info box
@@ -27,27 +34,40 @@
         projectEndSlot.innerHTML = reportData.projectEndDate;
     }
 
-    // Set the reporting frequency on page load
+    // Set the reporting frequency
     function setFrequency() {
         frequencyNumberSlot.innerHTML = frequencyNumberInput.value;
         frequencyPeriodSlot.innerHTML = frequencyPeriodSelect.value;
+    }
+
+    // Set the reporting period start date (endDate + 1)
+    function setReportPeriodStart() {
+        const endDate = new Date(reportData.endDate);
+        endDate.setDate(endDate.getDate() + 1);
+        periodStartSlot.innerHTML = endDate.toISOString().slice(0, 10);
+    }
+
+    // Update the reporting period end date (next report date)
+    function addReportPeriodEvents() {
+        startDateInput.oninput = e => {
+            periodEndSlot.innerHTML = e.target.value;
+        };
     }
 
     // Update reporting frequency as the options are changed
     function addFrequencyEvents() {
         frequencyNumberInput.oninput = e => {
             frequencyNumberSlot.innerHTML = e.target.value;
-
-            checkIfPlural(e.target.value);
+            pluraliseTimePeriod(e.target.value);
         };
 
         frequencyPeriodSelect.onchange = e => {
             frequencyPeriodSlot.innerHTML = `${e.target.value}`;
-            checkIfPlural(frequencyNumberInput.value);
+            pluraliseTimePeriod(frequencyNumberInput.value);
         };
     }
 
-    function checkIfPlural(number) {
+    function pluraliseTimePeriod(number) {
         frequencyPeriodSlot.innerHTML = `${frequencyPeriodSelect.value}${Number(number) === 1 ? '' : 's'}`;
     }
 
