306ad3866b403ba40eb4984403e7b4c3855231ec
Chris Lawton <chris.lawton@torchbox.com>
2019-11-05 16:57:53 +0000

    calculate reporting period range and humanize

--- opentech/static_src/src/javascript/apply/report-calculator.js
+++ opentech/static_src/src/javascript/apply/report-calculator.js
@@ -15,10 +15,9 @@
     const frequencyPeriodSlot = document.querySelector('.js-frequency-period-slot');
     const periodStartSlot = document.querySelector('.js-report-period-start');
     const periodEndSlot = document.querySelector('.js-report-period-end');
-
+    const nextReportDueSlot = document.querySelector('.js-next-report-due-slot');
 
     function init() {
-        console.log(reportData);
         // Set on page load
         setProjectEnd();
         setFrequency();
@@ -47,10 +46,21 @@
         periodStartSlot.innerHTML = endDate.toISOString().slice(0, 10);
     }
 
-    // Update the reporting period end date (next report date)
     function addReportPeriodEvents() {
         startDateInput.oninput = e => {
+            // Update the reporting period end date (next report date)
             periodEndSlot.innerHTML = e.target.value;
+
+            // Update the reporting period range (next report date - today)
+            const daysDiff = dateDiffInDays(new Date(), new Date(e.target.value));
+            const weeksAndDays = getWeeks(daysDiff);
+            const {weeks, days} = weeksAndDays;
+            const pluraliseWeeks = weeks === 1 ? '' : 's';
+            const pluraliseDays = days === 1 ? '' : 's';
+
+            nextReportDueSlot.innerHTML = `
+                ${weeks > 0 ? `${weeks} week${pluraliseWeeks}` : ''} ${days} day${pluraliseDays}
+            `;
         };
     }
 
@@ -71,9 +81,23 @@
         frequencyPeriodSlot.innerHTML = `${frequencyPeriodSelect.value}${Number(number) === 1 ? '' : 's'}`;
     }
 
+    // Get the number of days between two dates
+    function dateDiffInDays(startDate, EndDate) {
+        const msPerDay = 1000 * 60 * 60 * 24;
+        const utc1 = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+        const utc2 = Date.UTC(EndDate.getFullYear(), EndDate.getMonth(), EndDate.getDate());
+
+        return Math.floor((utc2 - utc1) / msPerDay);
+    }
+
+    // Convert days into weeks and days
+    function getWeeks(days) {
+        return {
+            weeks: Math.floor(days / 7),
+            days: days % 7
+        };
+    }
+
     init();
 
 })(jQuery);
-
-
-// next report date - today = due period
