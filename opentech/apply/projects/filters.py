8d602b12e22dc39550edb82ccd8b3e2ac2a78fd9
Todd Dembrey <todd.dembrey@torchbox.com>
2019-11-06 15:30:28 +0000

    Feature/gh 1651 all reports table (#1663)
    * Add table to filter by submitted reports
    * Add report table to the project overview page

--- opentech/apply/projects/filters.py
+++ opentech/apply/projects/filters.py
@@ -12,7 +12,8 @@ from .models import (
     PROJECT_STATUS_CHOICES,
     REQUEST_STATUS_CHOICES,
     PaymentRequest,
-    Project
+    Project,
+    Report,
 )
 
 User = get_user_model()
@@ -41,3 +42,40 @@ class ProjectListFilter(filters.FilterSet):
     class Meta:
         fields = ['status', 'lead', 'fund']
         model = Project
+
+
+class DateRangeInputWidget(filters.widgets.SuffixedMultiWidget):
+    template_name = 'application_projects/filters/widgets/date_range_input_widget.html'
+    suffixes = ['after', 'before']
+
+    def __init__(self, attrs=None):
+        widgets = (forms.DateInput, forms.DateInput)
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return [value.start, value.stop]
+        return [None, None]
+
+
+class ReportListFilter(filters.FilterSet):
+    reporting_period = filters.DateFromToRangeFilter(
+        label="Reporting Period",
+        method="filter_reporting_period",
+        widget=DateRangeInputWidget,
+    )
+    submitted = filters.DateFromToRangeFilter(widget=DateRangeInputWidget)
+
+    class Meta:
+        model = Report
+        fields = ['submitted']
+
+    def filter_reporting_period(self, queryset, name, value):
+        after, before = value.start, value.stop
+        q = {}
+        if after:
+            q['start__gte'] = after
+        if before:
+            q['end_date__lte'] = before
+
+        return queryset.filter(**q)
