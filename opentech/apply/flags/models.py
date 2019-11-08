c202b00da1dd0b868800ec6cb257e79287e4fae3
Fredrik Jonsson <frjo@xdeb.org>
2019-11-08 12:48:28 +0100

    Improvments after review to flag feature.

--- opentech/apply/flags/models.py
+++ opentech/apply/flags/models.py
@@ -3,16 +3,14 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
-STAFF = 'staff'
-USER = 'user'
-
-FLAG_TYPES = {
-    STAFF: 'Staff',
-    USER: 'User',
-}
-
 
 class Flag(models.Model):
+    STAFF = 'staff'
+    USER = 'user'
+    FLAG_TYPES = {
+        STAFF: 'Staff',
+        USER: 'User',
+    }
     target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     target_object_id = models.PositiveIntegerField()
     target = GenericForeignKey('target_content_type', 'target_object_id')
