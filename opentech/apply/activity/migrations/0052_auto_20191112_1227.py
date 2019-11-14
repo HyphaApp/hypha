6356f3d6beeaa5791ef2db391b34194ba68d6261
Fredrik Jonsson <frjo@xdeb.org>
2019-11-14 12:24:11 +0100

    Migration fix for flag feature.

--- opentech/apply/activity/migrations/0052_auto_20191112_1227.py
+++ opentech/apply/activity/migrations/0052_auto_20191112_1227.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('activity', '0048_add_project_transition'),
+        ('activity', '0051_report_frequency_change'),
     ]
 
     operations = [
