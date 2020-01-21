3c4d911639f0d6d08233efaf8ce1bf57b2f0d5c6
Fredrik Jonsson <frjo@xdeb.org>
2020-01-21 21:19:22 +0100

    Styling for new streamfields apply link and box.

---
+++ opentech/static_src/src/javascript/public/page-box.js
@@ -0,0 +1,7 @@
+(function ($) {
+
+    'use strict';
+
+    $('.page-box').wrapAll('<div class="wrapper wrapper--breakout wrapper--inner-space-large wrapper--page-box" />').wrapAll('<div class="wrapper wrapper--small wrapper--flex page-boxes" />');
+
+})(jQuery);
