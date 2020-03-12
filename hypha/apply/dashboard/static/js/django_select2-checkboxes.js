351db229f44838b2d34b53dc7c5ef9b163b07ae0
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-03-12 19:41:43 +0530

    refactor django-select2-checkboxes.js

--- hypha/apply/dashboard/static/js/django_select2-checkboxes.js
+++ hypha/apply/dashboard/static/js/django_select2-checkboxes.js
@@ -1,34 +1,30 @@
 (function($) {
   $.fn.select2.amd.require(
     [
-      'select2/multi-checkboxes/dropdown',
       'select2/multi-checkboxes/selection',
       'select2/multi-checkboxes/results'
     ],
-    function(DropdownAdapter, SelectionAdapter, ResultsAdapter) {
+    function(SelectionAdapter, ResultsAdapter) {
 
       $(function () {
         $('.django-select2-checkboxes').each(function (i, element) {
           var $element = $(element);
+            $element.select2({
+              placeholder: $element.data('placeholder'),
+              templateSelection: function(data) {
+                let filterType = $element.data('placeholder');
 
-          $element.select2({
-            placeholder: $element.data('placeholder'),
-            templateSelection: function(data) {
-              let filterType = $element.data('placeholder');
-
-              if (!data.selected.length) {
-                return filterType
-              } else if (data.selected.length == data.all.length) {
-                return 'All ' + filterType + ' selected';
-              }
-              return data.selected.length + ' of ' + data.all.length + ' ' + filterType + ' selected';
-            },
-            selectionAdapter: SelectionAdapter,
-            returnesultsAdapter: ResultsAdapter
-          });
-
+                if (!data.selected.length) {
+                  return filterType
+                } else if (data.selected.length == data.all.length) {
+                  return 'All ' + filterType + ' selected';
+                }
+                return data.selected.length + ' of ' + data.all.length + ' ' + filterType + ' selected';
+              },
+              selectionAdapter: SelectionAdapter,
+              returnesultsAdapter: ResultsAdapter
+            });
         });
       });
-    }
-  );
+    });
 }(this.jQuery));
