42a889f092c1884b3ccf39f7368c87efd8429175
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-03-13 12:50:46 +0530

    add Reminder model

---
+++ hypha/apply/funds/models/reminders.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Reminder(models.Model):
+    REVIEW = 'review'
+    REMINDER_TYPES = {
+        REVIEW: 'Remind to Review',
+    }
+    submission = models.ForeignKey(
+        'funds.ApplicationSubmission',
+        on_delete=models.CASCADE,
+        related_name='reminders'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    time = models.DateTimeField(verbose_name=_(' time'))
+    action = models.CharField(
+        choices=REMINDER_TYPES.items(),
+        default='review',
+        max_length=15,
+    )
+    sent = models.BooleanField(default=False)
