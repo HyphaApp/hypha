782b1ac0ecc83504d90c594b9aac67bb98a989ad
Fredrik Jonsson <frjo@xdeb.org>
2020-03-19 22:23:05 +0100

    Move reset_network files from opentech to hypha dir after main app rename.

---
+++ hypha/reset_network/reset_network_basic_page/models.py
@@ -0,0 +1,20 @@
+from hypha.reset_network.reset_network_utils.models import ResetNetworkBasePage
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+
+class ResetNetworkBasicPage(ResetNetworkBasePage):
+    class Meta:
+        verbose_name = "Reset Network Basic Page"
+
+    parent_page_types = ['reset_network_home.ResetNetworkHomePage']
+    subpage_types = []
+
+    content_text = RichTextField(verbose_name='Text', blank=False)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('content_text'),
+        ], heading='Content'),
+    ]
---
+++ hypha/reset_network/reset_network_utils/models.py
@@ -0,0 +1,13 @@
+from hypha.public.utils.models import SocialFields
+from wagtail.core.models import Page
+
+
+class ResetNetworkBasePage(SocialFields, Page):
+
+    class Meta:
+        abstract = True
+
+    promote_panels = (
+        Page.promote_panels +
+        SocialFields.promote_panels
+    )
