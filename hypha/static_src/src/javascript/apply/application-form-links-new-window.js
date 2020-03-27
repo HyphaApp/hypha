96c7080ff0440eca9b740ef1a6d7f7507c92f360
Fredrik Jonsson <frjo@xdeb.org>
2020-03-27 10:25:33 +0100

    Make all link on application forms open in new window/tab.

---
+++ hypha/static_src/src/javascript/apply/application-form-links-new-window.js
@@ -0,0 +1,8 @@
+(function ($) {
+
+    'use strict';
+
+    // Make links on application forms open in a new window/tab.
+    $('.application-form').find('a').attr('target', '_blank');
+
+})(jQuery);
--- hypha/static_src/src/javascript/apply/external-link-new-window.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-
-    'use strict';
-
-    // Make external links on application forms open in a new window/tab.
-    $('.application-form').find("a[href^='http']").attr('target', '_blank');
-
-})(jQuery);
