df4621ff77ed300c5a6e24cfde2c687ad2782349
Todd Dembrey <todd.dembrey@torchbox.com>
2020-04-08 14:07:39 +0100

    Add documentation on the private media view and extra safety

--- hypha/apply/utils/storage.py
+++ hypha/apply/utils/storage.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.storage import get_storage_class
 from django.http import FileResponse
 from django.views.generic import View
@@ -7,11 +8,25 @@ private_file_storage = getattr(settings, 'PRIVATE_FILE_STORAGE', None)
 PrivateStorage = get_storage_class(private_file_storage)
 
 
-class PrivateMediaView(View):
+class PrivateMediaView(LoginRequiredMixin, View):
+    """
+    Base view to configure the access to private media stored in the private
+    storage location.
+
+    Classes inheriting from this should implement their own access requirements
+    based on the file being served, this class will only ensure that the file
+    is not made public to unauthenticated users.
+    """
     storage = PrivateStorage()
 
     def get_media(self, *args, **kwargs):
-        # Convert the URL request to a path which the storage can use to find the file
+        """
+        Convert the URL request to a path and then return the file object
+
+        e.g.
+        storage_location = get_my_storage_location(request, **args, **kwargs)
+        return self.storage.open(storage_location)
+        """
         raise NotImplementedError()
 
     def get(self, *args, **kwargs):
