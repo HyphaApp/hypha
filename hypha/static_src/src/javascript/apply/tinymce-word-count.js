c9b7064cb9df38d6a148c127e697ddbff9660872
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-04-15 19:14:31 +0530

    fix typo word_limt

--- hypha/static_src/src/javascript/apply/tinymce-word-count.js
+++ hypha/static_src/src/javascript/apply/tinymce-word-count.js
@@ -13,22 +13,22 @@
             const mutation_selector = '#' + mutation.target.id
             const word_count = mutation.target.innerText.match(/\d+/)[0];
             const word_count_node = document.querySelector(mutation_selector);
-            const word_limt = $(mutation_selector).parents().eq(5).attr('data-word-limit')
+            const word_limit = $(mutation_selector).parents().eq(5).attr('data-word-limit')
             const percent_to_get = 20
-            const word_limit_to_show_warning = word_limt - (percent_to_get / 100) * word_limt;
+            const word_limit_to_show_warning = word_limit - (percent_to_get / 100) * word_limit;
 
             if (word_count <= word_limit_to_show_warning) {
                 word_count_node.removeAttribute('data-before-word-count');
                 word_count_node.classList.remove('word-count-warning');
                 word_count_node.classList.remove('word-count-warning-2');
             }
-            else if (word_count > word_limit_to_show_warning && word_count <= word_limt) {
-                word_count_node.setAttribute('data-before-word-count', 'Close to the limit (' + word_limt + '): ');
+            else if (word_count > word_limit_to_show_warning && word_count <= word_limit) {
+                word_count_node.setAttribute('data-before-word-count', 'Close to the limit (' + word_limit + '): ');
                 word_count_node.classList.remove('word-count-warning-2');
                 word_count_node.classList.add('word-count-warning');
             }
-            else if (word_count > word_limt) {
-                word_count_node.setAttribute('data-before-word-count', 'Over the limit (' + word_limt + '): ');
+            else if (word_count > word_limit) {
+                word_count_node.setAttribute('data-before-word-count', 'Over the limit (' + word_limit + '): ');
                 word_count_node.classList.add('word-count-warning-2');
             }
         });
