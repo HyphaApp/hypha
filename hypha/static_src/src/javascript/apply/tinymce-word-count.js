1848e0649b286edbea4943d1cd724129a3e56cec
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-04-15 17:56:13 +0530

    show warning for word limit specified in applicatin form

--- hypha/static_src/src/javascript/apply/tinymce-word-count.js
+++ hypha/static_src/src/javascript/apply/tinymce-word-count.js
@@ -10,17 +10,25 @@
 
     const observer = new MutationObserver(function (mutations) {
         mutations.forEach((mutation) => {
+            const mutation_selector = '#' + mutation.target.id
             const word_count = mutation.target.innerText.match(/\d+/)[0];
-            const word_count_node = document.querySelector('#' + mutation.target.id);
-            if (word_count <= 800) {
+            const word_count_node = document.querySelector(mutation_selector);
+            const word_limt = $(mutation_selector).parents().eq(5).attr('data-word-limit')
+            const percent_to_get = 20
+            const word_limit_to_show_warning = word_limt - (percent_to_get / 100) * word_limt;
+
+            if (word_count <= word_limit_to_show_warning) {
+                word_count_node.removeAttribute('data-before-word-count');
                 word_count_node.classList.remove('word-count-warning');
                 word_count_node.classList.remove('word-count-warning-2');
             }
-            else if (word_count > 800 && word_count <= 1000) {
+            else if (word_count > word_limit_to_show_warning && word_count <= word_limt) {
+                word_count_node.setAttribute('data-before-word-count', 'Close to the limit (' + word_limt + '): ');
                 word_count_node.classList.remove('word-count-warning-2');
                 word_count_node.classList.add('word-count-warning');
             }
-            else if (word_count > 1000) {
+            else if (word_count > word_limt) {
+                word_count_node.setAttribute('data-before-word-count', 'Over the limit (' + word_limt + '): ');
                 word_count_node.classList.add('word-count-warning-2');
             }
         });
