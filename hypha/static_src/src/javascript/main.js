e56da9c0b787da848a787d716d76af88931e14b1
Fredrik Jonsson <frjo@xdeb.org>
2020-05-07 13:50:18 +0200

    Move main.js to bottom and create main-top.js with the bits that needs to go on top.

--- hypha/static_src/src/javascript/main.js
+++ hypha/static_src/src/javascript/main.js
@@ -2,10 +2,6 @@
 
     'use strict';
 
-    // IE 11 does not support classList.replace.
-    // document.querySelector('html').classList.replace('no-js', 'js');
-    $('html').removeClass('no-js').addClass('js');
-
     let Search = class {
         static selector() {
             return '.js-search-toggle';
