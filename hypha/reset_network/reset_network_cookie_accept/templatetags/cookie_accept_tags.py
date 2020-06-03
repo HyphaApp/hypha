bee0e951e596bab6d9dd6f3de4ad895e68c1d572
Fredrik Jonsson <frjo@xdeb.org>
2020-06-03 13:00:58 +0200

    Fixes regarding use of request.site after updating to Wagtail 2.9.x.

--- hypha/reset_network/reset_network_cookie_accept/templatetags/cookie_accept_tags.py
+++ hypha/reset_network/reset_network_cookie_accept/templatetags/cookie_accept_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from wagtail.core.models import Site
+
 from hypha.reset_network.reset_network_cookie_accept.models import (
     ResetNetworkCookieAccept,
 )
@@ -10,9 +12,9 @@ register = template.Library()
 @register.inclusion_tag('reset_network_cookie_accept/message.html', takes_context=True)
 def resetnetworkcookieaccept(context):
     request = context['request']
-
-    site = context.get('PUBLIC_SITE', request.site)
+    site = Site.find_for_request(request)
+    public_site = context.get('PUBLIC_SITE', site)
 
     return {
-        'cookie_accept': ResetNetworkCookieAccept.for_site(site)
+        'cookie_accept': ResetNetworkCookieAccept.for_site(public_site)
     }
--- hypha/reset_network/reset_network_menus/templatetags/menu_tags.py
+++ hypha/reset_network/reset_network_menus/templatetags/menu_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from wagtail.core.models import Site
+
 from hypha.reset_network.reset_network_menus.models import (
     ResetNetworkMenusFooter,
     ResetNetworkMenusMain,
@@ -12,14 +14,15 @@ register = template.Library()
 def resetnetworkmainmenu(context):
 
     request = context['request']
+    site = Site.find_for_request(request)
 
     response = {
-        'APPLY_SITE': context.get('APPLY_SITE', request.site),
+        'APPLY_SITE': context.get('APPLY_SITE', site),
         'current_url': '{scheme}://{host}{path}'.format(scheme=request.scheme, host=request.get_host(), path=request.path),
         'menu_items': []
     }
 
-    site = context.get('PUBLIC_SITE', request.site)
+    site = context.get('PUBLIC_SITE', site)
 
     if site:
         menu_items = ResetNetworkMenusMain.for_site(site).items
@@ -33,13 +36,14 @@ def resetnetworkmainmenu(context):
 def resetnetworkfootermenu(context):
 
     request = context['request']
+    site = Site.find_for_request(request)
 
     response = {
         'current_url': '{scheme}://{host}{path}'.format(scheme=request.scheme, host=request.get_host(), path=request.path),
         'menu_items': []
     }
 
-    site = context.get('PUBLIC_SITE', request.site)
+    site = context.get('PUBLIC_SITE', site)
 
     if site:
         menu_items = ResetNetworkMenusFooter.for_site(site).items
