1c8c208acf35d7cb8f93457efc76cf0259ccd171
Fredrik Jonsson <frjo@xdeb.org>
2020-06-11 14:29:37 +0200

    Add rel noopener noreferrer to all none local links with target blank.

--- hypha/static_src/src/javascript/apply/application-form-links-new-window.js
+++ hypha/static_src/src/javascript/apply/application-form-links-new-window.js
@@ -3,6 +3,9 @@
     'use strict';
 
     // Make links on application forms open in a new window/tab.
-    $('.application-form').find('a').attr('target', '_blank');
+    $('.application-form').find('a').attr({
+        target: '_blank',
+        rel: 'noopener noreferrer'
+    });
 
 })(jQuery);
