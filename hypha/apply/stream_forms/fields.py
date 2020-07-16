b754a127afd29e9c86b2398b2fee9967d212d22c
Balazs Endresz <balazs.endresz@gmail.com>
2020-07-16 15:57:01 +0200

    Remove now unused code for handling file uploads

--- hypha/apply/stream_forms/fields.py
+++ hypha/apply/stream_forms/fields.py
@@ -1,67 +1,13 @@
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.core.validators import FileExtensionValidator
-from django.forms import CheckboxInput, ClearableFileInput, FileField, Media
+from django.forms import FileField, Media
 from django.utils.functional import cached_property
 from django_file_form.forms import MultipleUploadedFileField
 from django_file_form.widgets import UploadMultipleWidget
 
 
-class MultiFileInput(ClearableFileInput):
-    """
-    File Input only returns one file from its clean method.
-
-    This passes all files through the clean method and means we have a list of
-    files available for post processing
-    """
-    template_name = 'stream_forms/fields/multi_file_field.html'
-
-    input_text = ''
-
-    def __init__(self, *args, **kwargs):
-        self.multiple = kwargs.pop('multiple', True)
-        super().__init__(*args, **kwargs)
-
-    def is_initial(self, value):
-        is_initial = super().is_initial
-        if not value:
-            return False
-
-        try:
-            return all(
-                is_initial(file) for file in value
-            )
-        except TypeError:
-            return is_initial(value)
-
-    def render(self, name, value, attrs=dict(), renderer=None):
-        if self.multiple:
-            attrs['multiple'] = 'multiple'
-
-        return super().render(name, value, attrs, renderer)
-
-    def value_from_datadict(self, data, files, name):
-        if hasattr(files, 'getlist'):
-            upload = files.getlist(name)
-        else:
-            upload = files.get(name)
-            if not isinstance(upload, list):
-                upload = [upload]
-
-        checkbox_name = self.clear_checkbox_name(name) + '-'
-        checkboxes = {k for k in data if checkbox_name in k}
-        cleared = {
-            int(checkbox.replace(checkbox_name, '')) for checkbox in checkboxes
-            if CheckboxInput().value_from_datadict(data, files, checkbox)
-        }
-
-        return {
-            'files': upload,
-            'cleared': cleared,
-        }
-
-
-class CustomUploadMultipleWidget(UploadMultipleWidget):
+class MultiFileFieldWidget(UploadMultipleWidget):
 
     # This file seems to be imported even during collectstatic,
     # at which point `static()` won't be able to find these files yet
@@ -80,32 +26,18 @@ class CustomUploadMultipleWidget(UploadMultipleWidget):
 
 
 class MultiFileField(MultipleUploadedFileField):
-    widget = CustomUploadMultipleWidget
+    widget = MultiFileFieldWidget
 
     def clean(self, value, initial):
-        # TODO: Re-enable validation here, and check if we still need any overrides
-        # from MultiFileInput to be present in CustomUploadMultipleWidget too.
+        validator = FileExtensionValidator(allowed_extensions=settings.FILE_ALLOWED_EXTENSIONS)
+
+        for file in value:
+            FileField(validators=[validator]).clean(file, initial)
+
         return super().clean(value, initial)
-
-        files = value['files']
-        cleared = value['cleared']
-        if not files and not cleared:
-            return initial
-        new = [
-            FileField(validators=[
-                FileExtensionValidator(allowed_extensions=settings.FILE_ALLOWED_EXTENSIONS)
-            ]).clean(file, initial) for file in files
-        ]
-
-        if initial:
-            old = [file for i, file in enumerate(initial) if i not in cleared]
-        else:
-            old = []
-
-        return old + new
 
     def widget_attrs(self, widget):
         attrs = super().widget_attrs(widget)
-        if isinstance(widget, MultiFileInput) and 'accept' not in widget.attrs:
+        if isinstance(widget, MultiFileFieldWidget) and 'accept' not in widget.attrs:
             attrs.setdefault('accept', settings.FILE_ACCEPT_ATTR_VALUE)
         return attrs
--- hypha/static_src/src/javascript/apply/file-uploads.js
+++ hypha/static_src/src/javascript/apply/file-uploads.js
@@ -1,71 +1,36 @@
 jQuery(function ($) {
-
     'use strict';
 
-    // django-file-form
-    $('form').get().forEach(function (e) {
+    // Initialize django-file-form
+    $('form').get().forEach(function (form) {
         // Prevent initilising it multiple times and run it for forms
         // that have a `form_id` field added by django-file-form.
-        if (!e.initUploadFieldsDone && e.querySelector('[name=form_id]')) {
-            window.initUploadFields(e);
-            e.initUploadFieldsDone = true;
+        if (!form.initUploadFieldsDone && form.querySelector('[name=form_id]')) {
+            init(form);
+            form.initUploadFieldsDone = true;
         }
     });
-    $('input[type=hidden]').closest('.form__group').hide();
 
-    // TODO: check what we else need from this file
-    return; /* eslint-disable */
+    function init(form) {
+        window.initUploadFields(form);
 
-    $('.form__group--file').each(function () {
-        var $file_field = $(this);
-        var $file_input = $file_field.find('input[type="file"]');
-        var $file_list = $file_field.find('.form__file-list');
-        // var $file_drop = $file_field.find('.form__file-drop-zone');
+        // Hide wrapper elements for hidden inputs added by django-file-form
+        $('input[type=hidden]').closest('.form__group').hide();
 
-        $file_input.on('change', function (e) {
-            e.stopPropagation();
-            e.preventDefault();
+        var allowedExtensions = form.querySelector('[name=files]').accept.split(', ');
 
-            var files = $(this).prop('files');
-            var output = fileList(files);
-            $file_list.html('<ul>' + output.join('') + '</ul>');
+        // TODO: this currently raises an exception when trying to remove an invalid file.
+        // Remove files from the list that have an invalid file extension.
+        $('.dff-files').on('drop', function (e) {
+            var items = e.originalEvent.dataTransfer.items;
+            var files = e.originalEvent.dataTransfer.files;
+            for (var i = 0; i < files.length; i++) {
+                var extension = '.' + files[i].name.split('.').slice(-1)[0];
+                if (!allowedExtensions.includes(extension)) {
+                    items.remove(i);
+                }
+            }
         });
-
-        /*
-        $file_drop.on('dragover', function (e) {
-            e.stopPropagation();
-            e.preventDefault();
-            e.dataTransfer.dropEffect = 'copy';
-        });
-
-        $file_drop.on('drop', function (e) {
-            e.stopPropagation();
-            e.preventDefault();
-            var files = e.dataTransfer.file;
-            var output = fileList(files);
-            $file_list.html('<ul>' + output.join('') + '</ul>');
-        });
-        */
-    });
-
-    function fileList(files) {
-        var output = [];
-        for (var i = 0, f, len = files.length; i < len; i++) {
-            f = files[i];
-            output.push('<li><strong>', f.name, '</strong> (', f.type || 'n/a', ') - ', formatBytes(f.size), '</li>');
-        }
-        return output;
-    }
-
-    function formatBytes(bytes, decimals) {
-        if (bytes === 0) {
-            return '0 Bytes';
-        }
-        var k = 1024;
-        var dm = decimals <= 0 ? 0 : decimals || 2;
-        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        var i = Math.floor(Math.log(bytes) / Math.log(k));
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
 
 });
