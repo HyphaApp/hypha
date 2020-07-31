429f10653d2577dfb5fc35137445977222404a22
sks444 <krishnasingh.ss30@gmail.com>
2020-07-31 18:00:21 +0530

    Change api doc path to api/v1/doc

--- hypha/apply/api/urls.py
+++ hypha/apply/api/urls.py
@@ -7,22 +7,22 @@ from .v1 import urls as v1_urls
 
 app_name = 'api'
 
-schema_url_patterns = [
+schema_url_patterns_v1 = [
     path('api/v1/', include(v1_urls)),
 ]
 
-schema_view = get_schema_view(
+schema_view_v1 = get_schema_view(
     openapi.Info(
         title="Hypha API",
         default_version='v1',
         description="Hypha APIs specification",
     ),
     public=False,
-    patterns=schema_url_patterns,
+    patterns=schema_url_patterns_v1,
     permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('v1/', include(v1_urls)),
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('v1/doc/', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
