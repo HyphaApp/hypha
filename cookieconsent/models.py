e3e0b24eb4ac0a6a9a3a4b921df49c6a85393423
Fredrik Jonsson <frjo@xdeb.org>
2020-09-21 09:38:19 +0200

    Use richtext filter instead of safe for banner message.

--- cookieconsent/models.py
+++ cookieconsent/models.py
@@ -20,7 +20,7 @@ class CookieConsentSettings(BaseSetting):
     )
 
     cookieconsent_message = RichTextField(
-        default='This website deploys cookies for basic functionality and to keep it secure. These cookies are strictly necessary. Optional analysis cookies which provide us with statistical information about the use of the website may also be deployed, but only with your consent. Please review our <a href="/data-privacy-policy/">Privacy & Data Policy</a> for more information.',
+        default='<p>This website deploys cookies for basic functionality and to keep it secure. These cookies are strictly necessary. Optional analysis cookies which provide us with statistical information about the use of the website may also be deployed, but only with your consent. Please review our <a href="/data-privacy-policy/">Privacy &amp; Data Policy</a> for more information.</p>',
     )
 
     panels = [
