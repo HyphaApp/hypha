ac1306ab54e296a3651e2b67f52eea2f45f93221
Fredrik Jonsson <frjo@xdeb.org>
2020-09-21 09:55:01 +0200

    Verbose titles for settings.

--- hypha/cookieconsent/models.py
+++ hypha/cookieconsent/models.py
@@ -15,11 +15,13 @@ class CookieConsentSettings(BaseSetting):
     )
 
     cookieconsent_title = models.CharField(
+        'cookie concent title',
         max_length=255,
         default='Your cookie settings',
     )
 
     cookieconsent_message = RichTextField(
+        'cookie concent message',
         default='<p>This website deploys cookies for basic functionality and to keep it secure. These cookies are strictly necessary. Optional analysis cookies which provide us with statistical information about the use of the website may also be deployed, but only with your consent. Please review our <a href="/data-privacy-policy/">Privacy &amp; Data Policy</a> for more information.</p>',
     )
 
