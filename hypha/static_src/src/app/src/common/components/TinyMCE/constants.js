2e19bd2ef3a4bbbde6237a1bd21e996971c5375a
vimal1083 <vimal1083@gmail.com>
2020-09-28 20:43:58 +0530

    Replace Axios with Fetch

--- hypha/static_src/src/app/src/common/components/TinyMCE/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = "fufz3rqdwl1zs7gkxw5wou2g7t00i0eijtyzvy3bgklon5zv";
--- hypha/static_src/src/app/src/common/components/TinyMCE/index.js
+++ hypha/static_src/src/app/src/common/components/TinyMCE/index.js
@@ -2,21 +2,17 @@ import * as React from "react";
 
 import { Editor } from '@tinymce/tinymce-react';
 import HelperComponent from "@common/components/HelperComponent";
-import {API_KEY} from './constants';
 import PropTypes from 'prop-types';
 
 
 const TinyMCE = props => {
   return <div className="form__group ">
-    <script src="/static/tinymce/js/tinymce/tinymce.min.js"></script>
     <label htmlFor={props.name} className="form__question form__question--choice_field select" required="">
       <span>{props.label} </span>
       {props.required ? <span className="form__required"> *</span> : ""}
     </label>
     <HelperComponent {...props.helperProps} />
-    
     <Editor
-      apiKey={API_KEY}
       initialValue={props.value}
       init={{
         ...(props.mce_attrs),
