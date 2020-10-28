0fc4f8b52325bf5c2ee667f7ec096a0c6e778641
sks444 <krishnasingh.ss30@gmail.com>
2020-10-28 10:18:23 +0530

    Change text/color according to like and dislike

--- hypha/static_src/src/javascript/apply/screening-status.js
+++ hypha/static_src/src/javascript/apply/screening-status.js
@@ -14,13 +14,16 @@
             type: 'POST',
             data: {yes: yes},
             success: function (json) {
-                if (json && $('#screening-options').length === 0) {
-                    var screeningOptions = $('<p>' + json.title + '<a id="screening-options" data-fancybox="" data-src="#screen-application" class="link link--secondary-change" href="#">Screening Options</a></p>');
+                if (json) {
+                    var screeningOptions = $('<p id="screening-options-para">' + json.title + '<a id="screening-options" data-fancybox="" data-src="#screen-application" class="link link--secondary-change" href="#">Screening Options</a></p>');
+                    $('.show-screening-options').find('#screening-options-para').remove();
                     $('.show-screening-options').append(screeningOptions);
                     if (yes === true) {
+                        $('.icon--dislike').removeClass('icon--dislike-no');
                         $current.find('.icon').addClass('icon--like-yes');
                     }
                     else {
+                        $('.icon--like').removeClass('icon--like-yes');
                         $current.find('.icon').addClass('icon--dislike-no');
                     }
                 }
