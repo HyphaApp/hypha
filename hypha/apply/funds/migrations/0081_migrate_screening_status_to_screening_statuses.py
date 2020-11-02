e723f959d63c72e63e9db9057e4f08ef0cfdf434
sks444 <krishnasingh.ss30@gmail.com>
2020-11-02 11:24:55 +0530

    Add noop to data migration

--- hypha/apply/funds/migrations/0081_migrate_screening_status_to_screening_statuses.py
+++ hypha/apply/funds/migrations/0081_migrate_screening_status_to_screening_statuses.py
@@ -22,5 +22,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(make_many_screening_statuses),
+        migrations.RunPython(make_many_screening_statuses, migrations.RunPython.noop),
     ]
