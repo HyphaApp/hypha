87142d7dedaf90cd250d6bbe086cfc2635ebd1ba
vimal1083 <vimal1083@gmail.com>
2021-06-15 11:07:54 +0530

    Fix Multiple Address field error

--- addressfield/static/address_form.js
+++ addressfield/static/address_form.js
@@ -74,17 +74,19 @@
     };
 
     $(document).ready(function formReady() {
-        $('.form div.address').addressfield({
-            json: '/static/addressfield.min.json',
-            fields: {
-                country: ".country",
-                thoroughfare: ".thoroughfare",
-                premise: ".premise",
-                locality: ".locality",
-                localityname: ".localityname",
-                administrativearea: ".administrativearea",
-                postalcode: ".postalcode"
-            }
-        });
+        $('.form div.address').each(function() {
+            $('.form div#' + this.id).addressfield({
+                json: '/static/addressfield.min.json',
+                fields: {
+                    country: ".country",
+                    thoroughfare: ".thoroughfare",
+                    premise: ".premise",
+                    locality: ".locality",
+                    localityname: ".localityname",
+                    administrativearea: ".administrativearea",
+                    postalcode: ".postalcode"
+                }
+            });
+        })
     });
 })(jQuery);
