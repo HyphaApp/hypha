85a5902abdf1f4a9071952375a4ad50d36952e1b
Chris Zubak-Skees <chriszs@gmail.com>
2021-12-03 13:34:53 -0500

    Add Zulip messaging adapter

--- hypha/apply/activity/messaging.py
+++ hypha/apply/activity/messaging.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from collections import defaultdict
 
 import requests
@@ -713,6 +714,85 @@ class SlackAdapter(AdapterBase):
         return str(response.status_code) + ': ' + response.content.decode()
 
 
+class ZulipAdapter(SlackAdapter):
+    adapter_type = "Zulip"
+
+    def __init__(self):
+        super().__init__()
+        self.destination = settings.ZULIP_DESTINATION_URL
+        self.target_room = settings.ZULIP_DESTINATION_STREAM
+        self.topic = settings.ZULIP_DESTINATION_TOPIC
+        self.user = settings.ZULIP_BOT_EMAIL
+        self.api_key = settings.ZULIP_API_KEY
+
+    def zulip_links(self, links, sources):
+        return ', '.join(
+            f'[{source.title}]({links[source.id]})'
+            for source in sources
+        )
+
+    def rewrite_slack_links(self, message):
+        return re.sub("<([^\\|]+)\\|([^>]+)>", r"[\2](\1)", message)
+
+    def slack_links(self, links, sources):
+        return self.zulip_links(links, sources)
+
+    def zulip_id(self, user):
+        if user is None:
+            return ''
+
+        if not user.slack:
+            return ''
+
+        return f'@**{user.slack}**'
+
+    def slack_id(self, user):
+        return self.zulip_id(user)
+
+    def zulip_streams(self, source, **kwargs):
+        target_streams = self.slack_channels(source, **kwargs)
+
+        # Make sure each stream name doesn't start with a "#".
+        target_streams = [
+            stream.replace('#','')
+            for stream in target_streams
+            if stream
+        ]
+
+        return target_streams
+
+    def send_message(self, message, recipient, source, **kwargs):
+        target_streams = self.zulip_streams(source, **kwargs)
+
+        if not self.destination or not any(target_streams):
+            errors = list()
+            if not self.destination:
+                errors.append('Destination URL')
+            if not target_streams:
+                errors.append('Stream ID')
+            if not self.user:
+                errors.append('Bot email')
+            if not self.api_key:
+                errors.append('API key')
+            return 'Missing configuration: {}'.format(', '.join(errors))
+
+        message = ' '.join([recipient, self.rewrite_slack_links(message)]).strip()
+
+        data = {
+            "type": "stream",
+            "to": target_streams,
+            "topic": self.topic,
+            "content": message,
+        }
+        auth = (
+            self.user,
+            self.api_key
+        )
+        response = requests.post(self.destination, data=data, auth=auth)
+
+        return str(response.status_code) + ': ' + response.content.decode()
+
+
 class EmailAdapter(AdapterBase):
     adapter_type = 'Email'
     messages = {
@@ -1077,6 +1157,7 @@ class MessengerBackend:
 adapters = [
     ActivityAdapter(),
     SlackAdapter(),
+    ZulipAdapter(),
     EmailAdapter(),
     DjangoMessagesAdapter(),
 ]
