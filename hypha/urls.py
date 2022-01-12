e60b79327d2578047782a487afc6c226a657abf2
sandeepsajan0 <sandeepsajan0@gmail.com>
2022-01-12 18:37:03 +0530

    Resolve lint issues

--- hypha/urls.py
+++ hypha/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
-from django.urls import re_path
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from django_file_form import urls as django_file_form_urls
 from two_factor.views import LoginView
