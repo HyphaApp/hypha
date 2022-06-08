825f24efc9546c3e290e35aa1c58190b5fae7d9d
Fredrik Jonsson <frjo@xdeb.org>
2022-06-08 09:27:11 +0200

    Fix migrations after rebase.

--- hypha/apply/funds/migrations/0098_alter_applicationsubmission_submit_time.py
+++ hypha/apply/funds/migrations/0098_alter_applicationsubmission_submit_time.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('funds', '0096_only_active_users'),
+        ('funds', '0097_applicationbaseexternalreviewform_labbaseexternalreviewform_roundbaseexternalreviewform'),
     ]
 
     operations = [
