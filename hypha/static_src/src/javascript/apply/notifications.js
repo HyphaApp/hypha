02271a7362dc2c63610975af10b00374b63f971f
sandeepsajan0 <sandeepsajan0@gmail.com>
2022-06-23 09:53:24 +0530

    Add notification heading, make bellicon clickable, and align nav icons

---
+++ hypha/static_src/src/javascript/apply/notifications.js
@@ -0,0 +1,17 @@
+function notificationToggle() {
+  document.getElementById("notificationDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn, .dropbtn *')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
