b902c13e36829fe4f03062ba59063202f46bef0b
Saurabh Kumar <theskumar@users.noreply.github.com>
2022-07-22 11:58:01 +0530

    Use wagtail 3.0.1 and use robust testing for the activation email

--- hypha/apply/users/tests/test_utils.py
+++ hypha/apply/users/tests/test_utils.py
@@ -1,6 +1,6 @@
 from django.core import mail
+from test.support import EnvironmentVarGuard
 from django.test import TestCase, override_settings
-from django.conf import settings
 
 from hypha.apply.users.tests.factories import UserFactory
 
@@ -9,7 +9,12 @@ from ..utils import send_activation_email
 
 @override_settings(ROOT_URLCONF='hypha.apply.urls')
 class TestActivationEmail(TestCase):
+    def setUp(self) -> None:
+        self.env = EnvironmentVarGuard()
+        self.env.set('PRIMARY_HOST', 'primary-domain.org')
+
     def test_activation_email_includes_link(self):
-        send_activation_email(UserFactory())
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertContains(mail.outbox[0], settings.WAGTAILADMIN_BASE_URL)
+        with self.env:
+            send_activation_email(UserFactory())
+            assert len(mail.outbox) == 1
+            assert "password reset form at https://primary-domain.org" in mail.outbox[0]
