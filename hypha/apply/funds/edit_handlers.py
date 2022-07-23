77f8739cb585332a69d7d5df2ca178df43dd3ba4
Saurabh Kumar <theskumar@users.noreply.github.com>
2022-07-23 22:57:38 +0530

    Fix “ReadOnlyPanel” and “ReadOnlyInlinePlanel” for wagtail 3.x

--- hypha/apply/funds/edit_handlers.py
+++ hypha/apply/funds/edit_handlers.py
@@ -38,9 +38,6 @@ class DisplayField(Field):
 
 
 class ReadOnlyPanel(Panel):
-    template = 'wagtailadmin/panels/single_field_panel.html'
-    field_template = 'wagtailadmin/shared/field.html'
-
     def __init__(self, attr, **kwargs):
         self.attr = attr
         super().__init__(**kwargs)
@@ -54,48 +51,60 @@ class ReadOnlyPanel(Panel):
             help_text=self.help_text,
         )
 
-    def context(self):
-        try:
-            value = getattr(self.instance, self.attr)
-        except AttributeError:
-            self.attr = '__'.join([self.instance._meta.model_name, str(self.instance.id)])
-            value = self.instance
+    class BoundPanel(Panel.BoundPanel):
+        field_template_name = 'wagtailadmin/shared/field.html'
+        template_name = 'wagtailadmin/panels/single_field_panel.html'
 
-        if callable(value):
-            value = value()
+        def render_as_object(self):
+            return render_to_string(self.template_name, self.context())
 
-        # Add initial value only when an object is present. Display nothing when a new page is being
-        # created. As it is a read-only panel and creates confusion when default values are displayed.
-        if self.instance.id:
-            self.form.initial[self.attr] = value
-        else:
-            self.form.initial[self.attr] = '-'
-        self.bound_field = DisplayField().get_bound_field(self.form, self.attr)
-        return {
-            'self': self,
-            'field': self.bound_field,
-            'show_label': False,
-        }
+        def render_as_field(self):
+            return render_to_string(self.field_template_name, self.context())
 
-    def render_as_object(self):
-        return render_to_string(self.template, self.context())
+        def context(self):
+            try:
+                value = getattr(self.instance, self.attr)
+            except AttributeError:
+                self.attr = '__'.join(
+                    [self.instance._meta.model_name, str(self.instance.id)]
+                )
+                value = self.instance
 
-    def render_as_field(self):
-        return render_to_string(self.field_template, self.context())
+            if callable(value):
+                value = value()
+
+            # Add initial value only when an object is present. Display nothing when a new page is being
+            # created. As it is a read-only panel and creates confusion when default values are displayed.
+            if self.instance.id:
+                self.form.initial[self.attr] = value
+            else:
+                self.form.initial[self.attr] = '-'
+            self.bound_field = DisplayField().get_bound_field(self.form, self.attr)
+            return {
+                'self': self,
+                'field': self.bound_field,
+                'show_label': False,
+            }
 
 
 class ReadOnlyInlinePanel(ReadOnlyPanel):
-    template = 'wagtailadmin/panels/multi_field_panel.html'
+    template_name = 'wagtailadmin/panels/multi_field_panel.html'
 
     def get_child_edit_handler(self):
         child_edit_handler = ReadOnlyPanel(self.attr)
         model = getattr(self.instance, self.attr)
         return child_edit_handler.bind_to(model=model)
 
-    def on_instance_bound(self):
-        values = getattr(self.instance, self.attr).all()
-        child_panel = self.get_child_edit_handler()
-        self.children = [child_panel.bind_to(instance=value, form=self.form, request=self.request) for value in values]
+    class BoundPanel(ReadOnlyPanel.BoundPanel):
+        def on_instance_bound(self):
+            values = getattr(self.instance, self.attr).all()
+            child_panel = self.get_child_edit_handler()
+            self.children = [
+                child_panel.bind_to(
+                    instance=value, form=self.form, request=self.request
+                )
+                for value in values
+            ]
 
 
 class FilteredFieldPanel(FieldPanel):
