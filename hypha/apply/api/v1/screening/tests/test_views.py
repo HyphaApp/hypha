bcb977a7ced137d61617067063b9571d54bf4ea9
Saurabh Kumar <theskumar@users.noreply.github.com>
2022-07-26 12:31:33 +0530

    Fix test case for “add_screening_status”

--- hypha/apply/api/v1/screening/tests/test_views.py
+++ hypha/apply/api/v1/screening/tests/test_views.py
@@ -131,13 +131,12 @@ class SubmissionScreeningStatusViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(len(response.json()), 2)
 
-        first_submission_screening_status = self.yes_default_screening_status
-        self.assertEqual(response.json()[0]['id'], first_submission_screening_status.id)
-        self.assertEqual(response.json()[0]['title'], first_submission_screening_status.title)
-        self.assertEqual(response.json()[0]['yes'], first_submission_screening_status.yes)
-        self.assertEqual(response.json()[0]['default'], first_submission_screening_status.default)
+        self.assertEqual(response.json()[0]['id'], self.yes_screening_status.id)
+        self.assertEqual(response.json()[0]['title'], self.yes_screening_status.title)
+        self.assertEqual(response.json()[0]['yes'], self.yes_screening_status.yes)
+        self.assertEqual(response.json()[0]['default'], self.yes_screening_status.default)
 
-        self.assertEqual(response.json()[1]['id'], self.yes_screening_status.id)
+        self.assertEqual(response.json()[1]['id'], self.yes_default_screening_status.id)
 
     def test_staff_can_list_submission_screening_statuses(self):
         user = StaffFactory()
@@ -147,7 +146,7 @@ class SubmissionScreeningStatusViewSetTests(APITestCase):
             self.get_submission_screening_status_url(submission_id=self.submission.id)
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.json()), self.submission.screening_statuses.count())
+        self.assertEqual(len(response.json()), 0)
 
     def test_set_default_screening_status(self):
         user = StaffFactory()
