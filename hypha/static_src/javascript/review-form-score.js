34236881b64015c2e7826a36934f3a62c51e5252
Ghulam Murtaza <gmurtaza@opentechstrategies.com>
2022-11-03 13:48:05 -0400

    Score label added to edit review form and selectors filtering improved
    Issue #3963
    
    Co-authored-by: Jesse Bickel <jbickel@opentechstrategies.com>
        Upstream lint and directory changes.

--- hypha/static_src/javascript/review-form-score.js
+++ hypha/static_src/javascript/review-form-score.js
@@ -2,21 +2,27 @@
     "use strict";
 
     // grab all the selectors
+    let filtered_selectors;
     const selectors = Array.prototype.slice.call(
         document.querySelectorAll("select")
     );
 
     if (selectors.length > 1) {
+        document.querySelector(".form--score-box").style.display = "block";
         // remove recommendation select box from array
-        selectors.shift();
-        selectors.forEach(function (selector) {
+        filtered_selectors = selectors.filter(
+            (selector) =>
+                selector.length !== 8 && selector[0].text !== "Need more info"
+        );
+        filtered_selectors.forEach(function (selector) {
             selector.onchange = calculate_score;
         });
+        calculate_score();
     }
 
     function calculate_score() {
         let score = 0;
-        selectors.forEach(function (selector) {
+        filtered_selectors.forEach(function (selector) {
             score += parseInt(selector.value);
         });
         $(".form--score-box").text("Score: " + score);
