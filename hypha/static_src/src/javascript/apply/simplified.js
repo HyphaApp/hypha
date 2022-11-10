6f71f91424be5fd97f41bcbcaf15ce6491c45431
sandeepsajan0 <sandeepsajan0@gmail.com>
2022-11-10 21:55:10 +0530

    fix lint fe

--- hypha/static_src/src/javascript/apply/simplified.js
+++ hypha/static_src/src/javascript/apply/simplified.js
@@ -3,13 +3,13 @@
     'use strict';
 
     document.querySelector('.simplified__dropbtn').addEventListener('click', function () {
-        document.getElementById("downloadDropdown").classList.toggle("simplified__show");
+        document.getElementById('downloadDropdown').classList.toggle('simplified__show');
     });
 
     // Close the dropdown menu if the user clicks outside of it
-    window.addEventListener('click', function(event) {
+    window.addEventListener('click', function (event) {
         if (!event.target.matches('.simplified__dropbtn')) {
-            var dropdown = document.querySelector(".simplified__dropdown-content");
+            var dropdown = document.querySelector('.simplified__dropdown-content');
             if (dropdown.classList.contains('simplified__show')) {
                 dropdown.classList.remove('simplified__show');
             }
