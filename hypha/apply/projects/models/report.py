445f79b08b5b42c64585d198175f4c71de9f6eb7
sandeepsajan0 <sandeepsajan0@gmail.com>
2022-12-05 10:37:05 +0530

    remove extra spaces

--- hypha/apply/projects/models/report.py
+++ hypha/apply/projects/models/report.py
@@ -218,7 +218,7 @@ class ReportConfig(models.Model):
             if self.occurrence == 1:
                 return _('Yearly on the {day} of the {month}').format(day=day_of_month, month=month)
             return _('Every {occurrence} years on the {day} of the {month}').format(occurrence=self.occurrence,
-                                                                                     day=day_of_month, month=month)
+                                                                                    day=day_of_month, month=month)
 
         if self.frequency == self.MONTH:
             if self.schedule_start and self.schedule_start.day == 31:
