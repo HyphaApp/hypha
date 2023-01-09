4e839fc44c7fc01ef922bc4e3b03141f76f774a4
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-01-09 13:24:43 +0530

    Add activity e-mail digest for each Fund and Lab (#3035)
    Fixes #3017
    
    - [x] Add an "Digest recipient e-mails" field to each Fund and Lab.
    Similar to the existing Slack channel field.
    - [x] Create the same type of digest as the global one but filtered for
    each Fund and Lab.
    - [x]  Send out all digest with the same cron command.
    
    ---
    
    ![Screenshot 2022-11-03 at 08 38
    25@2x](https://user-images.githubusercontent.com/236356/199677732-00ffbf24-4c58-410a-8f9c-1f03b82009c2.jpg)
    
    Co-authored-by: Fredrik Jonsson <frjo@xdeb.org>

--- hypha/apply/activity/management/commands/send_staff_email_digest.py
+++ hypha/apply/activity/management/commands/send_staff_email_digest.py
@@ -1,9 +1,9 @@
 import logging
 import re
+from collections import defaultdict
 from datetime import timedelta
 
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.utils.translation import gettext as _
@@ -16,6 +16,31 @@ from hypha.core.mail import MarkdownMail
 logger = logging.getLogger(__name__)
 
 
+def groupby_fund_lab_emails(messages):
+    emails_msgs_map = defaultdict(list)
+    for msg in messages:
+        emails = get_fund_or_lab_emails(msg.event)
+        if emails:
+            emails_msgs_map[",".join(emails)].append(msg)
+    return emails_msgs_map.items()
+
+
+def get_fund_or_lab_emails(event) -> list:
+    """Retrieve the digest email for a fund or lab if present.
+
+    Args:
+        event: Event ModelClass instance
+
+    Returns:
+        An array of emails or empty list.
+    """
+    if hasattr(event.source, "get_from_parent"):
+        # it is related to a lab or fund
+        return event.source.get_from_parent('activity_digest_recipient_emails')
+
+    return []
+
+
 def slack_message_to_markdown(msg):
     """Converts messages in the slack format to markdown.
 
@@ -24,6 +49,7 @@ def slack_message_to_markdown(msg):
     Args:
         msg: slack message as text
     """
+
     def to_href(match_obj):
         if match_obj.group() is not None:
             return f'[{match_obj.group(2)}]({match_obj.group(1)})'
@@ -31,17 +57,56 @@ def slack_message_to_markdown(msg):
     return re.sub(r'<(.*)\|(.*)>', to_href, msg)
 
 
+def prepare_and_send_activity_digest_email(to, subject, slack_messages):
+    # extract some important activity types into groups
+    submissions = [m for m in slack_messages if m.event.type == MESSAGES.NEW_SUBMISSION]
+    comments = [m for m in slack_messages if m.event.type == MESSAGES.COMMENT]
+    reviews = [m for m in slack_messages if m.event.type == MESSAGES.NEW_REVIEW]
+
+    exclude_ids = [m.id for m in submissions + comments + reviews]  # type: ignore
+    messages = [m for m in slack_messages if m.id not in exclude_ids]
+    total_count = len(slack_messages)
+    ctx = {
+        'messages': messages,
+        'submissions': submissions,
+        'comments': comments,
+        'reviews': reviews,
+        'has_main_sections': bool(exclude_ids),
+        'total_count': total_count,
+        'ORG_LONG_NAME': settings.ORG_LONG_NAME,
+        'ORG_SHORT_NAME': settings.ORG_SHORT_NAME,
+        'ORG_URL': settings.ORG_URL,
+    }
+
+    if total_count:
+        email = MarkdownMail('messages/email/activity_summary.md')
+        email.send(
+            to=to,
+            subject=subject,
+            from_email=settings.SERVER_EMAIL,
+            context=ctx,
+        )
+        logger.info(f"Sent activity digest email to {to}")
+    else:
+        logger.info('No email generated/sent, as there are no new activities.')
+
+
 class Command(BaseCommand):
+    """Management command to send a summary of activities in hypha to configured
+    emails.
+
+    It sends all the activities to the emails set in "ACTIVITY_DIGEST_RECIPIENT_EMAILS",
+    if it is set.
+
+    It also checks for messages that belong a fund or lab, if a lab or fund has
+    "activity_digest_recipient_emails" property set, it also sends the digest related
+    to that fund or lab to that email.
+    """
+
     help = 'Sent email digest of all unsent activities (last 7 days) in hypha'
     IGNORE_DAYS_BEFORE = 7
-    EMAIL_SUBJECT = settings.EMAIL_SUBJECT_PREFIX + _('Activities Summary')
 
     def handle(self, *args, **options):
-        if not settings.ACTIVITY_DIGEST_RECIPIENT_EMAILS:
-            raise ImproperlyConfigured(
-                'ACTIVITY_DIGEST_RECIPIENT_EMAILS Django settings is required.'
-            )
-
         slack_messages = (
             Message.objects.filter(type=SlackAdapter.adapter_type)
             .select_related('event')
@@ -53,42 +118,33 @@ class Command(BaseCommand):
                 ).date()
             )
         )
-
-        # convert slack links to markdown links
+        # convert slack links and extract the fund/lab emails
         for msg in slack_messages:
             msg.content_markdown = slack_message_to_markdown(msg.content)
 
-        # extract some important activity types into groups
-        submissions = [
-            m for m in slack_messages if m.event.type == MESSAGES.NEW_SUBMISSION
-        ]
-        comments = [m for m in slack_messages if m.event.type == MESSAGES.COMMENT]
-        reviews = [m for m in slack_messages if m.event.type == MESSAGES.NEW_REVIEW]
-
-        exclude_ids = [m.id for m in submissions + comments + reviews]  # type: ignore
-        messages = [m for m in slack_messages if m.id not in exclude_ids]
-        total_count = len(slack_messages)
-        ctx = {
-            'messages': messages,
-            'submissions': submissions,
-            'comments': comments,
-            'reviews': reviews,
-            'has_main_sections': bool(exclude_ids),
-            'total_count': total_count,
-            'ORG_LONG_NAME': settings.ORG_LONG_NAME,
-            'ORG_SHORT_NAME': settings.ORG_SHORT_NAME,
-            'ORG_URL': settings.ORG_URL,
-        }
-
-        if total_count:
-            email = MarkdownMail('messages/email/activity_summary.md')
-            email.send(
+        # Send global activity digest if "ACTIVITY_DIGEST_RECIPIENT_EMAILS" settings is set.
+        if settings.ACTIVITY_DIGEST_RECIPIENT_EMAILS:
+            prepare_and_send_activity_digest_email(
                 to=settings.ACTIVITY_DIGEST_RECIPIENT_EMAILS,
-                subject=self.EMAIL_SUBJECT,
-                from_email=settings.SERVER_EMAIL,
-                context=ctx,
+                subject=settings.EMAIL_SUBJECT_PREFIX + _('Summary of all activities'),
+                slack_messages=slack_messages,
             )
-            slack_messages.update(sent_in_email_digest=True)
-            logger.info('Sent digest email.')
         else:
-            logger.info('No email generated/sent, as there are no new activities.')
+            logger.info(
+                "Global activity digest email not sent. Set ACTIVITY_DIGEST_RECIPIENT_EMAILS setting to enable it."
+            )
+
+        # Send digest of for funds that has "activity_digest_recipient_emails" set.
+        for to, messages in groupby_fund_lab_emails(slack_messages):
+            prepare_and_send_activity_digest_email(
+                subject=settings.EMAIL_SUBJECT_PREFIX + _('Activities Summary'),
+                to=to.split(","),
+                slack_messages=messages,
+            )
+            # mark as sent
+            Message.objects.filter(id__in=[m.id for m in messages]).update(
+                sent_in_email_digest=True
+            )
+
+        if settings.ACTIVITY_DIGEST_RECIPIENT_EMAILS:
+            slack_messages.update(sent_in_email_digest=True)  # mark as sent
