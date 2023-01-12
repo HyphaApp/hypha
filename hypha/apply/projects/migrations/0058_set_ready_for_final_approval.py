04a51424cb9b7b488e66a1ebba741b99c7bc0b8c
sandeepsajan0 <sandeepsajan0@gmail.com>
2023-01-12 10:49:40 +0530

    Remove imports from custom migration and use hard code values

--- hypha/apply/projects/migrations/0058_set_ready_for_final_approval.py
+++ hypha/apply/projects/migrations/0058_set_ready_for_final_approval.py
@@ -3,11 +3,23 @@
 from django.db import migrations
 
 
-def set_value_to_ready_for_final_approval(apps, schema_editor):
-    from hypha.apply.projects.models.project import Project, WAITING_FOR_APPROVAL
+def is_paf_approved_by_all_reviewers(project, paf_reviewers_count):
+    if paf_reviewers_count == 0:
+        return True
+    elif paf_reviewers_count == len(project.paf_reviews_meta_data):
+        for paf_review_data in project.paf_reviews_meta_data.values():
+            if paf_review_data['status'] == 'request_change':
+                return False
+        return True
+    return False
 
-    for project in Project.objects.filter(status=WAITING_FOR_APPROVAL):
-        if project.can_make_final_approval:
+
+def set_value_to_ready_for_final_approval(apps, schema_editor):
+    Project = apps.get_model('application_projects', 'Project')
+    PAFReviewersRole = apps.get_model('application_projects', 'PAFReviewersRole')
+
+    for project in Project.objects.filter(status='waiting_for_approval', ready_for_final_approval=False):
+        if is_paf_approved_by_all_reviewers(project, PAFReviewersRole.objects.all().count()):
             project.ready_for_final_approval = True
             project.save(update_fields={'ready_for_final_approval'})
 
