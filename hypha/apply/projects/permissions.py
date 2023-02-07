3c28044add5c1e7b1ab74c9e6c4fd6fbe5fbc75f
sandeepsajan0 <sandeepsajan0@gmail.com>
2023-02-07 12:15:55 +0530

    Add permission for project update status, and use templatetags to show that button

--- hypha/apply/projects/permissions.py
+++ hypha/apply/projects/permissions.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 
-from .models.project import CONTRACTING
+from .models.project import CLOSING, CONTRACTING, COMPLETE, IN_PROGRESS
 
 
 def has_permission(action, user, object=None, raise_exception=True):
@@ -39,7 +39,22 @@ def can_upload_contract(user, project):
     return False, 'Forbidden Error'
 
 
+def can_update_project_status(user, project):
+    if project.status not in [COMPLETE, CLOSING, IN_PROGRESS]:
+        return False, 'Forbidden Error'
+
+    if not user.is_authenticated:
+        return False, 'Login Required'
+
+    if user.is_apply_staff or user.is_apply_staff_admin:
+        return True, 'Staff and Staff Admin can update status'
+
+    return False, 'Forbidden Error'
+
+
+
 permissions_map = {
     'contract_approve': can_approve_contract,
     'contract_upload': can_upload_contract,
+    'project_status_update': can_update_project_status,
 }
--- hypha/apply/projects/templatetags/project_tags.py
+++ hypha/apply/projects/templatetags/project_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from hypha.apply.projects.models.project import CLOSING, COMPLETE, IN_PROGRESS
+from hypha.apply.projects.permissions import has_permission
 
 register = template.Library()
 
@@ -38,3 +39,9 @@ def show_closing_banner(project):
     if project.status in [COMPLETE, CLOSING]:
         return True
     return False
+
+
+@register.simple_tag
+def user_can_update_project_status(project, user):
+    can_update_status, _ = has_permission('project_status_update', user, object=project, raise_exception=False)
+    return can_update_status
