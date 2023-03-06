7c37d5ad06838bd3f0f7fc30a0ef5eb2beb6b473
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-03-06 18:13:25 +0530

    RemovedInWagtail50Warning: importing form wagtail.core.models
    RemovedInWagtail50Warning: Importing from wagtail.core.models is deprecated. Use wagtail.models instead. See https://docs.wagtail.org/en/stable/releases/3.0.html#changes-to-module-paths

--- hypha/apply/projects/models/project.py
+++ hypha/apply/projects/models/project.py
@@ -22,8 +22,8 @@ from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
-from wagtail.core.models import Orderable
 from wagtail.fields import StreamField
+from wagtail.models import Orderable
 
 from addressfield.fields import ADDRESS_FIELDS_ORDER
 from hypha.apply.funds.models.mixins import AccessFormData
