344e2e144a927d0cb758de323509c9ddb30874b7
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-03-12 14:50:57 +0530

    Add tailwindcss integration

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./hypha/templates/**/*.html", "./hypha/**/templates/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
