638f024f41747b19faa4508b5ef65f93ad3f7cc8
Fredrik Jonsson <frjo@xdeb.org>
2023-03-20 09:56:49 +0100

    Move most global vars from public to core.

--- hypha/core/context_processors.py
+++ hypha/core/context_processors.py
@@ -1,8 +1,19 @@
 from django.conf import settings
 
+from hypha.apply.home.models import ApplyHomePage
+
 
 def global_vars(request):
     return {
+        'APPLY_SITE': ApplyHomePage.objects.first().get_site(),
+        'ORG_LONG_NAME': settings.ORG_LONG_NAME,
+        'ORG_SHORT_NAME': settings.ORG_SHORT_NAME,
+        'ORG_EMAIL': settings.ORG_EMAIL,
+        'ORG_GUIDE_URL': settings.ORG_GUIDE_URL,
+        'ORG_URL': settings.ORG_URL,
+        'CURRENCY_SYMBOL': settings.CURRENCY_SYMBOL,
+        'GOOGLE_OAUTH2': settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY,
+        'ENABLE_REGISTRATION_WITHOUT_APPLICATION': settings.ENABLE_REGISTRATION_WITHOUT_APPLICATION,
         'SENTRY_TRACES_SAMPLE_RATE': settings.SENTRY_TRACES_SAMPLE_RATE,
         'SENTRY_ENVIRONMENT': settings.SENTRY_ENVIRONMENT,
         'SENTRY_DENY_URLS': settings.SENTRY_DENY_URLS,
