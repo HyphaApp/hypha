26f09668f94785ea726624c1db4fb9c557863176
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-03-22 23:44:09 +0530

    Add an enhanced version of querystring template tags

--- hypha/apply/funds/templatetags/submission_tags.py
+++ hypha/apply/funds/templatetags/submission_tags.py
@@ -1,5 +1,4 @@
 import re
-from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -23,27 +22,6 @@ def submission_links(value):
             value = re.sub(fr'(?<!\w){sid}(?!\w)', link, value)
 
     return mark_safe(value)
-
-
-@register.simple_tag(takes_context=True)
-def update_urlparams(context, param, value):
-    url = context.get('request').get_full_path()
-    parsed_url = urlparse(url)
-    query_params = dict(parse_qs(parsed_url.query))
-    query_params.update({str(param): str(value)})
-    url = urlunparse(parsed_url._replace(query=urlencode(query=query_params, doseq=True)))
-    return url
-
-
-@register.simple_tag(takes_context=True)
-def get_url_param(context, param):
-    url = context.get('request').get_full_path()
-    parsed_url = urlparse(url)
-    captured_value = parse_qs(parsed_url.query)
-
-    if captured_value and param in captured_value.keys():
-        return captured_value[param][0]
-    return None
 
 
 @register.simple_tag
---
+++ hypha/core/templatetags/tests/test_query_params.py
@@ -0,0 +1,129 @@
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from hypha.core.templatetags.querystrings import (
+    add_to_query,
+    construct_query_string,
+    modify_query,
+    remove_from_query,
+)
+
+
+class QueryParamsTemplateTagTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.factory = RequestFactory()
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_construct_query_string(self):
+        # Given a request without any special query parameters and a template name
+        request = self.factory.get("/en/test/")
+
+        query_params = [
+            ["a", "1"],
+            ["a", 2],
+            ["b", 3],
+            ["c", "OK"],
+            ["d", "Hello, World!"],
+        ]
+        path = construct_query_string(
+            context={"request": request}, query_params=query_params
+        )
+
+        self.assertEquals(
+            path, "/en/test/?a=1&amp;a=2&amp;b=3&amp;c=OK&amp;d=Hello%2C+World%21"
+        )
+
+    def test_construct_query_string_only_query_string(self):
+        # Given a request without any special query parameters and a template name
+        request = self.factory.get("/en/test/")
+
+        query_params = [
+            ["a", "1"],
+            ["a", 2],
+            ["b", 3],
+            ["c", "OK"],
+            ["d", "Hello, World!"],
+        ]
+        path = construct_query_string(
+            context={"request": request},
+            query_params=query_params,
+            only_query_string=True,
+        )
+
+        self.assertEquals(path, "?a=1&amp;a=2&amp;b=3&amp;c=OK&amp;d=Hello%2C+World%21")
+
+    def test_modify_query(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        params_to_remove = ["c"]
+        params_to_change = {"a": 3, "b": 4}
+
+        path = modify_query({"request": request}, *params_to_remove, **params_to_change)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(path, "/en/test/?a=3&amp;b=4&amp;d=Hello%2C+World%21")
+
+    def test_modify_query_only_query_string(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        params_to_remove = ["only_query_string", "c"]
+        params_to_change = {"a": 3, "b": 4}
+
+        path = modify_query({"request": request}, *params_to_remove, **params_to_change)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(path, "?a=3&amp;b=4&amp;d=Hello%2C+World%21")
+
+    def test_add_to_query(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        params_to_remove = ["c"]
+        params_to_add = {"a": 3, "b": 4}
+
+        path = add_to_query({"request": request}, *params_to_remove, **params_to_add)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(
+            path,
+            "/en/test/?a=1&amp;a=2&amp;a=3&amp;b=3&amp;b=4&amp;d=Hello%2C+World%21",
+        )
+
+    def test_add_to_query_only_query_string(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        params_to_remove = ["only_query_string", "c"]
+        params_to_add = {"a": 3, "b": 4}
+
+        path = add_to_query({"request": request}, *params_to_remove, **params_to_add)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(
+            path,
+            "?a=1&amp;a=2&amp;a=3&amp;b=3&amp;b=4&amp;d=Hello%2C+World%21",
+        )
+
+    def test_remove_from_query(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        args = ["c"]
+        kwargs = {"a": 2}
+
+        path = remove_from_query({"request": request}, *args, **kwargs)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(path, "/en/test/?a=1&amp;b=3&amp;d=Hello%2C+World%21")
+
+    def test_remove_from_query_only_query_string(self):
+        request = self.factory.get("/en/test/?a=1&a=2&b=3&c=OK&d=Hello%2C+World%21")
+
+        args = ["only_query_string", "c"]
+        kwargs = {"a": 2}
+
+        path = remove_from_query({"request": request}, *args, **kwargs)
+
+        # Then we should get the template names saved in context variables
+        self.assertEquals(path, "?a=1&amp;b=3&amp;d=Hello%2C+World%21")
