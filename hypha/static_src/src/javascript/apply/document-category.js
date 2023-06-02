b9865020e84a058f365e12078b136e26454e00a8
sandeepsajan0 <sandeepsajan0@gmail.com>
2023-06-02 12:31:28 +0530

    Fix function not accessible issue

--- hypha/static_src/src/javascript/apply/document-category.js
+++ hypha/static_src/src/javascript/apply/document-category.js
@@ -1,6 +1,7 @@
-(function handleCategory(category) {
+// eslint-disable-next-line no-unused-vars
+function handleCategory(category) {
     'use strict';
 
     document.getElementById('id_category').value = category;
     document.getElementsByClassName('form__group id_category')[0].hidden = 'true';
-})();
+}
