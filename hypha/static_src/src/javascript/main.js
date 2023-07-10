b06a5461bf1685ad28755d3caafa359faa99653b
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-07-10 18:01:05 +0530

    Remove unused header-admin-height
    The current implementation of header doesn't need this variable to
    be set.

--- hypha/static_src/src/javascript/main.js
+++ hypha/static_src/src/javascript/main.js
@@ -145,13 +145,6 @@
         }
     });
 
-    // Get the header and admin bar height and set custom prop with value
-    $(window).on('load', function () {
-        const headerHeight = $('.header').outerHeight();
-        const adminbarHeight = $('.admin-bar').outerHeight();
-        document.documentElement.style.setProperty('--header-admin-height', headerHeight + adminbarHeight + 'px');
-    });
-
     // Setting the CSRF token on AJAX requests.
     var csrftoken = false;
     if (typeof window.Cookies !== 'undefined') {
