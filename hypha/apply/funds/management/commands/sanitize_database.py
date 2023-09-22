c3153518afebe178e6321869ebc4157a25c6330a
Frank Duncan <frankduncan@opentechstrategies.com>
2023-09-22 12:21:41 -0500

    Run linter, format after merge from v4.0.0
    Issue #3979

--- hypha/apply/funds/management/commands/sanitize_database.py
+++ hypha/apply/funds/management/commands/sanitize_database.py
@@ -1,25 +1,20 @@
+import json
+import random
+
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from faker import Faker
 
-from hypha.apply.funds.models import (
-    ApplicationSubmission,
-    ApplicationRevision,
-)
-from hypha.apply.review.models import (
-    Review,
-)
-
-from hypha.apply.stream_forms.blocks import (
-    TextFieldBlock,
-    CharFieldBlock,
-    RadioButtonsFieldBlock,
-    MultiFileFieldBlock,
-    DropdownFieldBlock,
-)
+from hypha.apply.categories.blocks import CategoryQuestionBlock
 from hypha.apply.funds.blocks import (
-    TitleBlock,
+    DurationBlock,
     EmailBlock,
     FullNameBlock,
-    DurationBlock,
+    TitleBlock,
+)
+from hypha.apply.funds.models import (
+    ApplicationRevision,
+    ApplicationSubmission,
 )
 from hypha.apply.review.blocks import (
     RecommendationBlock,
@@ -27,22 +22,21 @@ from hypha.apply.review.blocks import (
     ScoreFieldWithoutTextBlock,
     VisibilityBlock,
 )
-
+from hypha.apply.review.models import (
+    Review,
+)
 from hypha.apply.review.options import (
     RECOMMENDATION_CHOICES,
 )
-
-from django.contrib.auth import get_user_model
-
-from hypha.apply.categories.blocks import CategoryQuestionBlock
+from hypha.apply.stream_forms.blocks import (
+    CharFieldBlock,
+    DropdownFieldBlock,
+    MultiFileFieldBlock,
+    RadioButtonsFieldBlock,
+    TextFieldBlock,
+)
 from hypha.apply.utils.blocks import RichTextFieldBlock, SingleIncludeMixin
 
-from wagtail.blocks import BoundBlock
-
-from faker import Faker
-
-import random
-import json
 
 class Command(BaseCommand):
     help = "Sanitizes the reviews, submissions, and users of identifiable inforamtion"
@@ -58,60 +52,66 @@ class Command(BaseCommand):
         for form_field in form_holder.form_fields:
             data = None
             key = None
-            if(isinstance(form_field.block, SingleIncludeMixin)):
+            if isinstance(form_field.block, SingleIncludeMixin):
                 try:
                     data = form_holder.form_data[form_field.block_type]
                     key = form_field.block_type
-                except:
+                except:  # noqa: E722
                     pass
             if not data and form_field.id in form_holder.form_data:
                 data = form_holder.form_data[form_field.id]
                 key = form_field.id
 
             def update_data(new_data):
-                if not data:
+                if not data:  # noqa: B023
                     # Don't replace non data with data!!
                     pass
                 else:
-                    form_holder.form_data[key] = new_data
+                    form_holder.form_data[key] = new_data  # noqa: B023
 
             def update_text_data():
-                if len(data) == 5:
+                if len(data) == 5:  # noqa: B023
                     # Don't replace empty strings with non empty strings
                     pass
-                elif len(data) < 5:
+                elif len(data) < 5:  # noqa: B023
                     update_data(self.f.word())
                 else:
-                    update_data(self.f.text(len(data)))
+                    update_data(self.f.text(len(data)))  # noqa: B023
 
-            if form_field.value['field_label'].lower() == "organization name":
+            if form_field.value["field_label"].lower() == "organization name":
                 update_data(self.f.company())
-            elif form_field.value['field_label'].lower() == "contact phone number":
+            elif form_field.value["field_label"].lower() == "contact phone number":
                 update_data(self.f.phone_number())
-            elif form_field.value['field_label'].lower() == "organization address":
+            elif form_field.value["field_label"].lower() == "organization address":
                 try:
                     address = json.loads(data)
-                    address['country'] = self.f.country_code()
-                    address['thoroughfare'] = self.f.street_address()
-                    address['premise'] = ''
-                    address['localityname'] = self.f.city()
-                    address['administrativearea'] = 'CA'
-                    address['postalcode'] = self.f.postcode()
+                    address["country"] = self.f.country_code()
+                    address["thoroughfare"] = self.f.street_address()
+                    address["premise"] = ""
+                    address["localityname"] = self.f.city()
+                    address["administrativearea"] = "CA"
+                    address["postalcode"] = self.f.postcode()
                     update_data(json.dumps(address))
                 except json.decoder.JSONDecodeError:
                     # Address was just a string
                     update_data(self.f.address())
-            elif form_field.value['field_label'].lower() == "requested grant amount":
+            elif form_field.value["field_label"].lower() == "requested grant amount":
                 update_data(self.f.pricetag())
-            elif form_field.value['field_label'].lower() == "project website":
+            elif form_field.value["field_label"].lower() == "project website":
                 update_data(self.f.uri())
-            elif form_field.value['field_label'].lower() == "requested grant amount currency (if not us dollars)":
+            elif (
+                form_field.value["field_label"].lower()
+                == "requested grant amount currency (if not us dollars)"
+            ):
                 update_data(self.f.currency_code())
-            elif form_field.value['field_label'].lower() == "additional contact name":
+            elif form_field.value["field_label"].lower() == "additional contact name":
                 update_data(self.f.name())
-            elif form_field.value['field_label'].lower() == "ein (for us-based organizations)":
+            elif (
+                form_field.value["field_label"].lower()
+                == "ein (for us-based organizations)"
+            ):
                 update_data(self.f.ssn())
-            elif form_field.value['field_label'].lower() == "additional contact email":
+            elif form_field.value["field_label"].lower() == "additional contact email":
                 update_data(self.f.email())
             elif type(form_field.block) == FullNameBlock:
                 update_data(self.f.name())
@@ -124,28 +124,35 @@ class Command(BaseCommand):
             elif type(form_field.block) == RecommendationBlock:
                 update_data(random.choice(RECOMMENDATION_CHOICES)[0])
             elif type(form_field.block) == ScoreFieldWithoutTextBlock:
-                update_data(random.randint(form_field.value['min'], form_field.value['max']))
+                update_data(
+                    random.randint(form_field.value["min"], form_field.value["max"])
+                )
             elif type(form_field.block) == DropdownFieldBlock:
-                update_data(random.choice(form_field.value['choices']))
+                update_data(random.choice(form_field.value["choices"]))
             elif type(form_field.block) in [
                 TextFieldBlock,
                 CharFieldBlock,
-                RichTextFieldBlock, # This may need to be updated sometime in the future 
+                RichTextFieldBlock,  # This may need to be updated sometime in the future
                 RecommendationCommentsBlock,
             ]:
                 update_text_data()
-            elif type(form_field.block) in [
-                RadioButtonsFieldBlock, # If it's a radio button, then it doesn't really need to be randomized
-                DurationBlock, # Hopefully no one is encoding some super specific PII in a duration :)
-                CategoryQuestionBlock, # Similarly, what Category should not be changed (what it if affects something else)
-                VisibilityBlock, # Visility of reviews can remain
-            ]:
+            elif (
+                type(form_field.block)
+                in [
+                    RadioButtonsFieldBlock,  # If it's a radio button, then it doesn't really need to be randomized
+                    DurationBlock,  # Hopefully no one is encoding some super specific PII in a duration :)
+                    CategoryQuestionBlock,  # Similarly, what Category should not be changed (what it if affects something else)
+                    VisibilityBlock,  # Visility of reviews can remain
+                ]
+            ):
                 pass
             else:
                 print(form_holder.id)
-                print(form_field.value['field_label'])
+                print(form_field.value["field_label"])
                 print(data)
-                raise Exception("Don't know how to handle " + str(type(form_field.block)))
+                raise Exception(
+                    "Don't know how to handle " + str(type(form_field.block))
+                )
         form_holder.save()
 
     def handle(self, *args, **options):
