49f4faf7678b2a401765fd020d119c2359248041
Frank Duncan <frankduncan@opentechstrategies.com>
2023-11-08 12:35:44 -0600

    Use new report frequency settings in notify_report_due command
    Instead of using a passed in variable, use the settings so that the
    command can be configured in wagtail.
    
    Issue #3989

--- hypha/apply/projects/tests/test_commands.py
+++ hypha/apply/projects/tests/test_commands.py
@@ -5,12 +5,30 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.utils import timezone
 
+from hypha.home.factories import ApplySiteFactory
 from hypha.home.models import ApplyHomePage
 
+from ..models.project import ProjectReminderFrequency, ProjectSettings
 from .factories import ProjectFactory, ReportConfigFactory, ReportFactory
 
 
 class TestNotifyReportDue(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        apply_site = ApplySiteFactory()
+        cls.project_settings, _ = ProjectSettings.objects.get_or_create(
+            site_id=apply_site.id,
+        )
+        cls.project_settings.reminder_frequencies = [
+            ProjectReminderFrequency.objects.create(
+                page=cls.project_settings,
+                num_days=7,
+                relation=ProjectReminderFrequency.FrequencyRelation.BEFORE,
+            ),
+        ]
+        cls.project_settings.save()
+
     def test_notify_report_due_in_7_days(self):
         in_a_week = timezone.now() + relativedelta(days=7)
         ReportConfigFactory(schedule_start=in_a_week, project__in_progress=True)
@@ -19,7 +37,7 @@ class TestNotifyReportDue(TestCase):
         with self.settings(
             ALLOWED_HOSTS=[ApplyHomePage.objects.first().get_site().hostname]
         ):
-            call_command("notify_report_due", 7, stdout=out)
+            call_command("notify_report_due", stdout=out)
         self.assertIn("Notified project", out.getvalue())
 
     def test_dont_notify_report_due_in_7_days_already_submitted(self):
@@ -36,7 +54,7 @@ class TestNotifyReportDue(TestCase):
         with self.settings(
             ALLOWED_HOSTS=[ApplyHomePage.objects.first().get_site().hostname]
         ):
-            call_command("notify_report_due", 7, stdout=out)
+            call_command("notify_report_due", stdout=out)
         self.assertNotIn("Notified project", out.getvalue())
 
     def test_dont_notify_already_notified(self):
@@ -50,17 +68,17 @@ class TestNotifyReportDue(TestCase):
             notified=timezone.now(),
         )
         out = StringIO()
-        call_command("notify_report_due", 7, stdout=out)
+        call_command("notify_report_due", stdout=out)
         self.assertNotIn("Notified project", out.getvalue())
 
     def test_dont_notify_project_not_in_progress(self):
         ProjectFactory()
         out = StringIO()
-        call_command("notify_report_due", 7, stdout=out)
+        call_command("notify_report_due", stdout=out)
         self.assertNotIn("Notified project", out.getvalue())
 
     def test_dont_notify_project_complete(self):
         ProjectFactory(is_complete=True)
         out = StringIO()
-        call_command("notify_report_due", 7, stdout=out)
+        call_command("notify_report_due", stdout=out)
         self.assertNotIn("Notified project", out.getvalue())
