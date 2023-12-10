5ec824b07ec372183f3a5b96b7375491956f3755
Saurabh Kumar <theskumar@users.noreply.github.com>
2023-12-10 11:00:00 +0530

    Remove public search module
    Part of public site removal

--- hypha/public/urls.py
+++ hypha/public/urls.py
@@ -3,10 +3,8 @@ from django.urls import include, path
 from .mailchimp import urls as newsletter_urls
 from .news import feeds as news_feeds
 from .partner import views as partner_views
-from .search import views as search_views
 
 urlpatterns = [
-    path("search/", search_views.search, name="search"),
     path("news/feed/", news_feeds.NewsFeed(), name="news_feed"),
     path(
         "news/<int:news_type>/feed/", news_feeds.NewsTypeFeed(), name="news_type_feed"
