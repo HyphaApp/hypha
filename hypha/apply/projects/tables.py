d0d53865322500bd7fb9499a5a4c30f7328a5a9f
Sandeep Chauhan <sandeepsajan0@gmail.com>
2023-12-13 15:10:46 +0530

    Add "PAFs for review" table for paf approvers (#3645)
    Fixes #3535 
    
    Right now, we have two tables on the approvers dashboard "PAF waiting
    for approval" and "PAF waiting for assignee" but as per the latest
    design we want a new table "PAFs for review" that contains data from
    both of the above tables and list it in a simple manner.
    In this PR we are just adding that new table but not removing the older
    ones, once we are done with the new table testing and are ready to have
    it then we can remove the older ones.
    
    The date requested column is kind of not clear yet. 
    We have multiple dates, one of them is the "Submitted date" which is
    when a lead/staff submits the PAF for the first time. Another one is
    "Updated date" which is when staff submits or this approval(not paf) is
    updated even for change approver/assignee(right now I have used this
    one).
    But I think we might need to maintain one more date as the "date
    requested" for every approval, which might be different from both of the
    above dates in sequential workflow(ex: the second approval
    date_requested will be set as the first approval approved_date) while it
    will be same as submit_date for parallel workflow.
    
    What do you all think, is the submit_date enough, or should we have
    date_requested as another as explained above?
    
    **Update**
    We decided to use submit_date as date_requested.
    Removing the old tables as the new table contains both old tables' data.

--- hypha/apply/projects/tables.py
+++ hypha/apply/projects/tables.py
@@ -4,7 +4,7 @@ import django_tables2 as tables
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
-from .models import Invoice, Project, Report
+from .models import Invoice, PAFApprovals, Project, Report
 
 
 class BaseInvoiceTable(tables.Table):
@@ -122,6 +122,50 @@ class ProjectsAssigneeDashboardTable(BaseProjectsTable):
         orderable = False
         exclude = ["status"]
         attrs = {"class": "projects-table"}
+
+
+class PAFForReviewDashboardTable(tables.Table):
+    date_requested = tables.DateColumn(
+        verbose_name=_("Date requested"),
+        accessor="created_at",
+        orderable=True,
+    )
+    title = tables.LinkColumn(
+        "funds:projects:detail",
+        text=lambda r: textwrap.shorten(r.project.title, width=30, placeholder="..."),
+        accessor="project__title",
+        args=[tables.utils.A("project__pk")],
+        orderable=False,
+    )
+    status = tables.Column(verbose_name=_("Status"), accessor="pk", orderable=False)
+    fund = tables.Column(
+        verbose_name=_("Fund"), accessor="project__submission__page", orderable=False
+    )
+
+    assignee = tables.Column(
+        verbose_name=_("Assignee"), accessor="user", orderable=False
+    )
+
+    class Meta:
+        fields = ["date_requested", "title", "fund", "status", "assignee"]
+        model = PAFApprovals
+        template_name = (
+            "funds/tables/table.html"  # todo: update it with Project table template
+        )
+        attrs = {"class": "paf-review-table"}
+
+    def order_date_requested(self, qs, is_descending):
+        direction = "-" if is_descending else ""
+
+        qs = qs.order_by(f"{direction}created_at")
+
+        return qs, True
+
+    def render_status(self, record):
+        if record.user:
+            return _("Waiting for approval")
+        else:
+            return _("Waiting for assignee")
 
 
 class ProjectsListTable(BaseProjectsTable):
