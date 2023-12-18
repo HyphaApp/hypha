f8fbc12cb58fef264fc1b82aec991bf239ddb09f
Wes Appler <wes@opentech.fund>
2023-12-18 13:25:18 -0500

    upload url update to attempt to fix tests

--- hypha/urls.py
+++ hypha/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("sitemap.xml", sitemap),
-    path("upload/", include(django_file_form_urls)),
+    path("", include(django_file_form_urls)),
     path("", include(apply_urls)),
     path("", include((user_urls, "users_public"))),
     path("", include("social_django.urls", namespace="social")),
