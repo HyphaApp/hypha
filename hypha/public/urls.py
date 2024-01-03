763aa4a8bade73d89e707f59f26abfb1bee1d645
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-01-03 13:33:58 +0530

    Remove public search module (#3689)
    Part of public site removal

--- hypha/public/urls.py
+++ hypha/public/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 
 from .news import feeds as news_feeds
 from .partner import views as partner_views
-from .search import views as search_views
 
 urlpatterns = [
-    path("search/", search_views.search, name="search"),
     path("news/feed/", news_feeds.NewsFeed(), name="news_feed"),
     path(
         "news/<int:news_type>/feed/", news_feeds.NewsTypeFeed(), name="news_type_feed"
