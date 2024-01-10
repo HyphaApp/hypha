9afb76a62b4e9538f5cb36209e219b15b411677d
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-01-10 12:56:34 +0530

    Remove public news module
    - Keeps the "news" installed app, so that model migrations are applied
    - update the sandbox db to remove news page data from it

--- hypha/public/urls.py
+++ hypha/public/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 
-from .news import feeds as news_feeds
 from .partner import views as partner_views
 
 urlpatterns = [
-    path("news/feed/", news_feeds.NewsFeed(), name="news_feed"),
-    path(
-        "news/<int:news_type>/feed/", news_feeds.NewsTypeFeed(), name="news_type_feed"
-    ),
     path(
         "about/portfolio/",
         partner_views.InvestmentTableView.as_view(),
