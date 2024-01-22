36342c7e3262c7a6a54b6928e51fdd8ef377f3ca
Wes Appler <wes@opentech.fund>
2024-01-22 18:38:52 -0500

    Formatting fixes, fixed some existing docstrings

--- docs/scripts/gen_ref_pages.py
+++ docs/scripts/gen_ref_pages.py
@@ -1,4 +1,8 @@
-"""Generate the code reference pages."""
+"""
+Generate reference pages for the API
+
+
+"""
 
 from pathlib import Path
 
@@ -9,7 +13,7 @@ src = Path(__file__).parent.parent.parent / "hypha"
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+    full_doc_path = Path("API-reference", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -25,8 +29,7 @@ for path in sorted(src.rglob("*.py")):
 
     if len(parts) > 0:
         with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-            heading = f"*hypha.{'.'.join(parts)}*"
             identifier = f"::: hypha.{'.'.join(parts)}"
-            fd.write(f"{heading}\n{identifier}")
+            fd.write(identifier)
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
