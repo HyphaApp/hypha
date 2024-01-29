44b78b3751307da4050065814e7b63293aebddef
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-01-29 19:46:32 +0530

    Update management command

--- hypha/core/management/commands/wagtailsiteupdate.py
+++ hypha/core/management/commands/wagtailsiteupdate.py
@@ -7,7 +7,6 @@ class Command(BaseCommand):
     help = "Wagtail site update script. Requires a public hostname, a apply hostname and a port number in that order."
 
     def add_arguments(self, parser):
-        parser.add_argument("public", type=str, help="Hostname for the public site.")
         parser.add_argument("apply", type=str, help="Hostname for the apply site.")
         parser.add_argument("port", type=int, help="Port to use for all sites.")
 
@@ -18,11 +17,6 @@ class Command(BaseCommand):
         site_apply.port = options["port"]
         site_apply.save()
 
-        site_public = Site.objects.get(id=3)
-        site_public.hostname = options["public"]
-        site_public.port = options["port"]
-        site_public.save()
-
         self.stdout.write(
-            f"Updated the public site to {options['public']}:{options['port']} and the apply site to {options['apply']}:{options['port']}."
+            f"Updated the apply site to {options['apply']}:{options['port']}."
         )
