079e0150b75c66919c62c1e464e5fb75eceaf50a
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-01-30 01:23:13 +0530

    Move cookieconsent settings to generic settings (#3722)
    Removes the cookie consent settings from the "public" menu
    to "settings" menu and make it site independent setting
    
    This PR also remove the `@register_public_site_settings` as this the
    last remain setting in the "public" menu in wagtail
    
    Depends on #3721
    
    ---------
    
    Co-authored-by: Fredrik Jonsson <frjo@xdeb.org>

--- hypha/cookieconsent/models.py
+++ hypha/cookieconsent/models.py
@@ -1,29 +1,27 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-from wagtail.contrib.settings.models import BaseSiteSetting
+from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 from wagtail.fields import RichTextField
 
-from hypha.core.wagtail.admin.registry import register_public_site_setting
 
-
-@register_public_site_setting
-class CookieConsentSettings(BaseSiteSetting):
+@register_setting
+class CookieConsentSettings(BaseGenericSetting):
     class Meta:
         verbose_name = "Cookie consent settings"
 
     cookieconsent_active = models.BooleanField(
-        "Activate cookie concent feature",
+        "Activate cookie consent feature",
         default=False,
     )
 
     cookieconsent_title = models.CharField(
-        "cookie concent title",
+        "cookie consent title",
         max_length=255,
         default="Your cookie settings",
     )
 
     cookieconsent_message = RichTextField(
-        "cookie concent message",
+        "cookie consent message",
         default='<p>This website deploys cookies for basic functionality and to keep it secure. These cookies are strictly necessary. Optional analysis cookies which provide us with statistical information about the use of the website may also be deployed, but only with your consent. Please review our <a href="/data-privacy-policy/">Privacy &amp; Data Policy</a> for more information.</p>',
     )
 
