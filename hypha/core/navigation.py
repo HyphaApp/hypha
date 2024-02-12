0c3baf9209fa08348b66782c2d42243445d92775
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-02-12 16:38:36 +0530

    Update primary nagivation dropdown UX
    - Use mouseover for quick access
    - Update over states
    - Update the urls to that any external urls can also be added via
      settings, removes dependeny of `reverse` while specifying urls
    - How active page in the dropdown as well, a dark-blue border

--- hypha/core/navigation.py
+++ hypha/core/navigation.py
@@ -1,56 +1,61 @@
 from django.conf import settings
+from django.urls import reverse
 
 nav_items = [
     {
         "title": "My Dashboard",
-        "url": "dashboard:dashboard",
+        "url": reverse("dashboard:dashboard"),
         "permission_method": "has_dashboard_access",
     },
     {
         "title": "Submissions",
-        "url": "apply:submissions:overview",  # kind of basic url to figure out active tab
+        "url": reverse(
+            "apply:submissions:overview"
+        ),  # kind of basic url to figure out active tab
         "permission_method": "is_apply_staff_or_reviewer_required",
         "sub_items": [
             {
                 "title": "All Submissions",
-                "url": "apply:submissions:list",
+                "url": reverse("apply:submissions:list"),
                 "permission_method": "is_apply_staff_or_reviewer_required",
             },
             {
                 "title": "Staff Assignments",
-                "url": "apply:submissions:staff_assignments",
+                "url": reverse("apply:submissions:staff_assignments"),
                 "permission_method": "is_apply_staff",
             },
             {
                 "title": "Reviews",
-                "url": "apply:submissions:reviewer_leaderboard",
+                "url": reverse("apply:submissions:reviewer_leaderboard"),
                 "permission_method": "is_apply_staff",
             },
             {
                 "title": "Results",
-                "url": "apply:submissions:result",
+                "url": reverse("apply:submissions:result"),
                 "permission_method": "is_apply_staff",
             },
         ],
     },
     {
         "title": "Projects",
-        "url": "apply:projects:overview",  # kind of basic url to figure out active tab
+        "url": reverse(
+            "apply:projects:overview"
+        ),  # kind of basic url to figure out active tab
         "permission_method": "is_apply_staff_or_finance_or_contracting",
         "sub_items": [
             {
                 "title": "All Projects",
-                "url": "apply:projects:all",
+                "url": reverse("apply:projects:all"),
                 "permission_method": "is_apply_staff_or_finance_or_contracting",
             },
             {
                 "title": "Invoices",
-                "url": "apply:projects:invoices",
+                "url": reverse("apply:projects:invoices"),
                 "permission_method": "is_apply_staff_or_finance",
             },
             {
                 "title": "Reports",
-                "url": "apply:projects:reports:all",
+                "url": reverse("apply:projects:reports:all"),
                 "permission_method": "is_apply_staff_or_finance",
             },
         ],
