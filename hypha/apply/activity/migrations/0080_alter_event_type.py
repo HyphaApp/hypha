c86638b12cd6981d5470e15d39d4f9f0c5575d64
sandeepsajan0 <sandeepsajan0@gmail.com>
2024-02-14 17:26:21 +0530

    resolve migrations conflicts

--- hypha/apply/activity/migrations/0080_alter_event_type.py
+++ hypha/apply/activity/migrations/0080_alter_event_type.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("activity", "0078_activityattachment"),
+        ("activity", "0079_alter_activity_visibility"),
     ]
 
     operations = [
