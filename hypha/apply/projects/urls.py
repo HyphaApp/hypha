fe63d6bc506b739958075797aa3e0031d3766f6a
Fredrik Jonsson <frjo@xdeb.org>
2024-02-20 14:35:51 +0100

    Remove obsolete vendor path.

--- hypha/apply/projects/urls.py
+++ hypha/apply/projects/urls.py
@@ -146,30 +146,4 @@ urlpatterns = [
             )
         ),
     ),
-    path(
-        "vendor/",
-        include(
-            (
-                [
-                    path("", ReportListView.as_view(), name="all"),
-                    path(
-                        "<int:pk>/",
-                        include(
-                            [
-                                path("", ReportDetailView.as_view(), name="detail"),
-                                path("skip/", ReportSkipView.as_view(), name="skip"),
-                                path("edit/", ReportUpdateView.as_view(), name="edit"),
-                                path(
-                                    "documents/<int:file_pk>/",
-                                    ReportPrivateMedia.as_view(),
-                                    name="document",
-                                ),
-                            ]
-                        ),
-                    ),
-                ],
-                "reports",
-            )
-        ),
-    ),
 ]
