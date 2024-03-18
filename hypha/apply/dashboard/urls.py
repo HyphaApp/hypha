efbdc402748b3c31181f61380c98e7e014efee42
Sandeep Chauhan <sandeepsajan0@gmail.com>
2024-03-18 16:06:50 +0530

    Update applicant dashboard with all submissions into a section with pagination (#3792)

--- hypha/apply/dashboard/urls.py
+++ hypha/apply/dashboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views import DashboardView
+from .views_partials import applicant_projects, applicant_submissions
 
 app_name = "dashboard"
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    path("applicant/submissions/", applicant_submissions, name="applicant_submissions"),
+    path("applicant/projects/", applicant_projects, name="applicant_projects"),
 ]
