ab4e5ecf12ae44076e6b489f99bf8537085541d2
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-05-01 11:43:15 +0530

    Refractor primary nav and dashboard, hide projects info if not enabled (#3898)
    Also, refractor and allow for importing permission functions from
    anywhere and not just limit to be imported from the
    `hypha.users.decorators` module
    
    This PR also doesn't display project info on the dashboard if the
    projects is not enabled
    
    For the stats block at the top of the dashboard, if the projects is not
    abled displays the count of flagged submissions and the count of
    previously reviewed submissions
    
    Simplify the nav generator logic.

--- hypha/core/navigation.py
+++ hypha/core/navigation.py
@@ -1,43 +1,48 @@
+import copy
+import functools
+import importlib
+
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
-nav_items = [
+DEFAULT_NAV_ITEMS = [
     {
         "title": _("My Dashboard"),
         "url": reverse_lazy("dashboard:dashboard"),
-        "permission_method": "has_dashboard_access",
+        "permission_method": "hypha.apply.users.decorators.has_dashboard_access",
     },
     {
         "title": _("Submissions"),
         # kind of basic url to figure out active tab
         "url": reverse_lazy("apply:submissions:overview"),
-        "permission_method": "is_apply_staff_or_reviewer_required",
+        "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_reviewer_required",
         "sub_items": [
             {
                 "title": _("All Submissions"),
                 "url": reverse_lazy("apply:submissions:list"),
-                "permission_method": "is_apply_staff_or_reviewer_required",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_reviewer_required",
             },
             {
                 "title": _("Staff Assignments"),
                 "url": reverse_lazy("apply:submissions:staff_assignments"),
-                "permission_method": "is_apply_staff",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff",
             },
             {
                 "title": _("Reviews"),
                 "url": reverse_lazy("apply:submissions:reviewer_leaderboard"),
-                "permission_method": "is_apply_staff",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff",
             },
             {
                 "title": _("Results"),
                 "url": reverse_lazy("apply:submissions:result"),
-                "permission_method": "is_apply_staff",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff",
             },
             {
                 "title": _("Staff flagged"),
                 "url": reverse_lazy("apply:submissions:staff_flagged"),
-                "permission_method": "is_apply_staff",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff",
             },
         ],
     },
@@ -45,33 +50,73 @@ nav_items = [
         "title": _("Projects"),
         # kind of basic url to figure out active tab
         "url": reverse_lazy("apply:projects:overview"),
-        "permission_method": "is_apply_staff_or_finance_or_contracting",
+        "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_finance_or_contracting",
         "sub_items": [
             {
                 "title": _("All Projects"),
                 "url": reverse_lazy("apply:projects:all"),
-                "permission_method": "is_apply_staff_or_finance_or_contracting",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_finance_or_contracting",
             },
             {
                 "title": _("Invoices"),
                 "url": reverse_lazy("apply:projects:invoices"),
-                "permission_method": "is_apply_staff_or_finance",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_finance",
             },
             {
                 "title": _("Reports"),
                 "url": reverse_lazy("apply:projects:reports:all"),
-                "permission_method": "is_apply_staff_or_finance",
+                "permission_method": "hypha.apply.users.decorators.is_apply_staff_or_finance",
             },
         ],
     },
 ]
 
 
-if settings.APPLY_NAV_MENU_ITEMS:
-    nav_items = settings.APPLY_NAV_MENU_ITEMS
+def _check_permission(user, method: str) -> bool:
+    """Resolve the method path and check if the user has permission.
 
-if settings.APPLY_NAV_SUBMISSIONS_ITEMS:
-    nav_items[1]["sub_items"] = settings.APPLY_NAV_SUBMISSIONS_ITEMS
+    Args:
+        user: user object
+        method: import path to the method to check permission
 
-if settings.APPLY_NAV_PROJECTS_ITEMS:
-    nav_items[2]["sub_items"] = settings.APPLY_NAV_PROJECTS_ITEMS
+    Returns:
+        bool: True if user has permission, False otherwise
+    """
+    module = importlib.import_module(method.rsplit(".", 1)[0])
+    method = method.rsplit(".", 1)[1]
+    try:
+        return getattr(module, method)(user)
+    except PermissionDenied:
+        return False
+
+
+@functools.cache
+def get_primary_navigation_items(user):
+    """Get the primary navigation items based on user permissions."""
+    original_nav_items = copy.deepcopy(
+        settings.APPLY_NAV_MENU_ITEMS or DEFAULT_NAV_ITEMS
+    )
+
+    nav_items = []
+
+    for item in original_nav_items:
+        nav_item = item.copy()
+
+        if item["title"] == "Projects" and not settings.PROJECTS_ENABLED:
+            continue
+
+        if item["title"] == "Submissions" and settings.APPLY_NAV_SUBMISSIONS_ITEMS:
+            nav_item["sub_items"] = settings.APPLY_NAV_SUBMISSIONS_ITEMS
+
+        if not _check_permission(user, nav_item["permission_method"]):
+            continue
+        if sub_items := nav_item.get("sub_items"):
+            nav_item["sub_items"] = list(
+                filter(
+                    lambda x: _check_permission(user, x["permission_method"]),
+                    sub_items,
+                )
+            )
+        nav_items.append(nav_item)
+
+    return nav_items
