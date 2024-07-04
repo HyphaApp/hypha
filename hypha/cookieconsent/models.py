972396acc26ed3df17649f6bee46d3e38a05dead
Wes Appler <145372368+wes-otf@users.noreply.github.com>
2024-07-04 01:34:17 -0400

    Cookie popup improvements (#3976)
    Fixes #3925.
    - Cookie consent preference is now stored in local storage rather than
    using cookies
    - Improves on the existing cookie banner by adding a `Learn More` option
    where users can read more about the essential & analytic cookies, and
    toggle analytics specifically.
    - If analytics are not used, the user is no longer given the option to
    opt out, as Hypha only uses essential cookies by default.
    - Added a link/button to the footer default that allows the user to
    re-open the cookie prompt without having to clear it from their local
    storage.

--- hypha/cookieconsent/models.py
+++ hypha/cookieconsent/models.py
@@ -7,22 +7,37 @@ from wagtail.fields import RichTextField
 @register_setting
 class CookieConsentSettings(BaseGenericSetting):
     class Meta:
-        verbose_name = "Cookie consent settings"
+        verbose_name = "Cookie banner settings"
 
     cookieconsent_active = models.BooleanField(
-        "Activate cookie consent feature",
+        "Activate cookie pop-up banner",
         default=False,
     )
 
     cookieconsent_title = models.CharField(
-        "cookie consent title",
+        "Cookie banner title",
         max_length=255,
         default="Your cookie settings",
     )
 
     cookieconsent_message = RichTextField(
-        "cookie consent message",
+        "Cookie consent message",
         default='<p>This website deploys cookies for basic functionality and to keep it secure. These cookies are strictly necessary. Optional analysis cookies which provide us with statistical information about the use of the website may also be deployed, but only with your consent. Please review our <a href="/data-privacy-policy/">Privacy &amp; Data Policy</a> for more information.</p>',
+    )
+
+    cookieconsent_essential_about = RichTextField(
+        'Essential cookies information to be displayed under "Learn More"',
+        default="<p>Strictly necessary for the operation of a website because they enable you to navigate around the site and use features.  These cookies cannot be switched off in our systems and do not store any personally identifiable information.</p>",
+    )
+
+    cookieconsent_analytics = models.BooleanField(
+        "Include consent option for analytics cookies",
+        default=False,
+    )
+
+    cookieconsent_analytics_about = RichTextField(
+        'Analytics cookies information to be displayed under "Learn More"',
+        default="<p>With these cookies we count visits and traffic sources to help improve the performance of our services through metrics.  These cookies show us which pages on our services are the most and the least popular, and how users navigate our services.  The information collected is aggregated and contains no personally identifiable information.  If you block these cookies, then we will not know when you have used our services.</p>",
     )
 
     panels = [
@@ -31,7 +46,10 @@ class CookieConsentSettings(BaseGenericSetting):
                 FieldPanel("cookieconsent_active"),
                 FieldPanel("cookieconsent_title"),
                 FieldPanel("cookieconsent_message"),
+                FieldPanel("cookieconsent_essential_about"),
+                FieldPanel("cookieconsent_analytics"),
+                FieldPanel("cookieconsent_analytics_about"),
             ],
-            "cookie banner",
+            "Cookie banner",
         ),
     ]
