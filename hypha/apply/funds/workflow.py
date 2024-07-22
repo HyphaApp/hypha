dfce31848b8c8c11f71e3138bd5ec75237d22d15
Frank Duncan <frankduncan@opentechstrategies.com>
2024-07-22 11:56:07 -0500

    Add new workflow for Request With Review
    This workflow is like Request with External Review, in that both
    internal and external reviewers can review submissions.  However, it has
    a workflow more like Request in that there is only one Review step.

--- hypha/apply/funds/workflow.py
+++ hypha/apply/funds/workflow.py
@@ -232,6 +232,8 @@ staff_edit_permissions = make_permissions(edit=[staff_can])
 
 Request = Stage("Request", False)
 
+RequestSame = Stage("RequestRev", True)
+
 RequestExt = Stage("RequestExt", True)
 
 RequestCom = Stage("RequestCom", True)
@@ -385,6 +387,134 @@ SingleStageDefinition = [
         },
     },
 ]
+
+SingleStageSameDefinition = [
+    {
+        DRAFT_STATE: {
+            "transitions": {
+                INITIAL_STATE: {
+                    "display": _("Submit"),
+                    "permissions": {UserPermissions.APPLICANT},
+                    "method": "create_revision",
+                },
+            },
+            "display": _("Draft"),
+            "stage": RequestSame,
+            "permissions": applicant_edit_permissions,
+        }
+    },
+    {
+        INITIAL_STATE: {
+            "transitions": {
+                "same_more_info": _("Request More Information"),
+                "same_internal_review": _("Open Review"),
+                "same_determination": _("Ready For Determination"),
+                "same_rejected": _("Dismiss"),
+            },
+            "display": _("Need screening"),
+            "public": _("Application Received"),
+            "stage": RequestSame,
+            "permissions": default_permissions,
+        },
+        "same_more_info": {
+            "transitions": {
+                INITIAL_STATE: {
+                    "display": _("Submit"),
+                    "permissions": {
+                        UserPermissions.APPLICANT,
+                        UserPermissions.STAFF,
+                        UserPermissions.LEAD,
+                        UserPermissions.ADMIN,
+                    },
+                    "method": "create_revision",
+                },
+            },
+            "display": _("More information required"),
+            "stage": RequestSame,
+            "permissions": applicant_edit_permissions,
+        },
+    },
+    {
+        "same_internal_review": {
+            "transitions": {
+                "same_post_review_discussion": _("Close Review"),
+                INITIAL_STATE: _("Need screening (revert)"),
+            },
+            "display": _("Review"),
+            "public": _("{org_short_name} Review").format(
+                org_short_name=settings.ORG_SHORT_NAME
+            ),
+            "stage": RequestSame,
+            "permissions": reviewer_review_permissions,
+        },
+    },
+    {
+        "same_post_review_discussion": {
+            "transitions": {
+                "same_post_review_more_info": _("Request More Information"),
+                "same_determination": _("Ready For Determination"),
+                "same_internal_review": _("Open Review (revert)"),
+                "same_rejected": _("Dismiss"),
+            },
+            "display": _("Ready For Discussion"),
+            "stage": RequestSame,
+            "permissions": hidden_from_applicant_permissions,
+        },
+        "same_post_review_more_info": {
+            "transitions": {
+                "same_post_review_discussion": {
+                    "display": _("Submit"),
+                    "permissions": {
+                        UserPermissions.APPLICANT,
+                        UserPermissions.STAFF,
+                        UserPermissions.LEAD,
+                        UserPermissions.ADMIN,
+                    },
+                    "method": "create_revision",
+                },
+            },
+            "display": _("More information required"),
+            "stage": RequestSame,
+            "permissions": applicant_edit_permissions,
+        },
+    },
+    {
+        "same_determination": {
+            "transitions": {
+                "same_post_review_discussion": _("Ready For Discussion (revert)"),
+                "same_almost": _("Accept but additional info required"),
+                "same_accepted": _("Accept"),
+                "same_rejected": _("Dismiss"),
+            },
+            "display": _("Ready for Determination"),
+            "permissions": hidden_from_applicant_permissions,
+            "stage": RequestSame,
+        },
+    },
+    {
+        "same_accepted": {
+            "display": _("Accepted"),
+            "future": _("Application Outcome"),
+            "stage": RequestSame,
+            "permissions": staff_edit_permissions,
+        },
+        "same_almost": {
+            "transitions": {
+                "same_accepted": _("Accept"),
+                "same_post_review_discussion": _("Ready For Discussion (revert)"),
+            },
+            "display": _("Accepted but additional info required"),
+            "stage": RequestSame,
+            "permissions": applicant_edit_permissions,
+        },
+        "same_rejected": {
+            "display": _("Dismissed"),
+            "stage": RequestSame,
+            "permissions": no_permissions,
+        },
+    },
+]
+
 
 SingleStageExternalDefinition = [
     {
@@ -1114,6 +1244,12 @@ def phase_data(phases):
 
 Request = Workflow("Request", "single", **phase_data(SingleStageDefinition))
 
+RequestSameTime = Workflow(
+    "Request with same time review",
+    "single_same",
+    **phase_data(SingleStageSameDefinition),
+)
+
 RequestExternal = Workflow(
     "Request with external review",
     "single_ext",
@@ -1133,6 +1269,7 @@ ConceptProposal = Workflow(
 
 WORKFLOWS = {
     Request.admin_name: Request,
+    RequestSameTime.admin_name: RequestSameTime,
     RequestExternal.admin_name: RequestExternal,
     RequestCommunity.admin_name: RequestCommunity,
     ConceptProposal.admin_name: ConceptProposal,
@@ -1255,6 +1392,7 @@ DETERMINATION_PHASES = [
 DETERMINATION_RESPONSE_PHASES = [
     "post_review_discussion",
     "concept_review_discussion",
+    "same_post_review_discussion",
     "post_external_review_discussion",
     "ext_post_external_review_discussion",
     "com_post_external_review_discussion",
