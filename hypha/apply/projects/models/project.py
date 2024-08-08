3865e71a96b20b11d7cc6e4fd09f9eddb46585ab
Jesse Bickel <jbickel@opentechstrategies.com>
2024-08-08 16:25:58 -0500

    Clean up some variables and text
    Issue #3989

--- hypha/apply/projects/models/project.py
+++ hypha/apply/projects/models/project.py
@@ -517,7 +517,7 @@ class PAFReviewersRole(Orderable, ClusterableModel):
 
 
 class ProjectReminderFrequency(Orderable, ClusterableModel):
-    num_days = models.IntegerField()
+    reminder_days = models.IntegerField()
     page = ParentalKey("ProjectSettings", related_name="reminder_frequencies")
 
     class FrequencyRelation(models.TextChoices):
@@ -531,8 +531,8 @@ class ProjectReminderFrequency(Orderable, ClusterableModel):
     )
 
     panels = [
-        FieldPanel("num_days", heading=_("Number of Days")),
-        FieldPanel("relation", heading=_("Relation to Project Due Date")),
+        FieldPanel("reminder_days", heading=_("Number of days")),
+        FieldPanel("relation", heading=_("Relation to report due date")),
     ]
 
 
@@ -569,8 +569,8 @@ class ProjectSettings(BaseSiteSetting, ClusterableModel):
         ),
         InlinePanel(
             "reminder_frequencies",
-            label=_("Report Reminder Frequency"),
-            heading=_("Report Reminder Frequency"),
+            label=_("Report reminder frequency"),
+            heading=_("Report reminder frequency"),
         ),
     ]
 
