1b678dc3a625e105edb81077d1ef6f176f2b9d62
Jesse Bickel <jbickel@opentechstrategies.com>
2024-08-12 16:12:54 -0500

    Add a tiny bit of documentation
    Issue #3989

--- hypha/apply/projects/models/project.py
+++ hypha/apply/projects/models/project.py
@@ -543,6 +543,9 @@ class ProjectSettings(BaseSiteSetting, ClusterableModel):
             "reminder_frequencies",
             label=_("Report reminder frequency"),
             heading=_("Report reminder frequency"),
+            help_text=_(
+                "Set up a cron job to run `notify_report_due.py`. The script will use these reminder settings."
+            ),
         ),
     ]
 
