c5ee81643f29b9b2144be006276538f18d617239
Wes Appler <wes@opentech.fund>
2024-08-12 17:19:26 -0400

    Swapped for full table updates & included rejected table

--- hypha/apply/projects/urls.py
+++ hypha/apply/projects/urls.py
@@ -91,6 +91,18 @@ urlpatterns = [
                 ),
                 path("invoice/", CreateInvoiceView.as_view(), name="invoice"),
                 path(
+                    "partial/invoice-status/",
+                    partial_get_invoice_status,
+                    {"rejected": False},
+                    name="partial-invoice-status",
+                ),
+                path(
+                    "partial/rejected-invoice-status/",
+                    partial_get_invoice_status,
+                    {"rejected": True},
+                    name="partial-rejected-invoice-status",
+                ),
+                path(
                     "invoices/<int:invoice_pk>/",
                     include(
                         [
@@ -117,11 +129,6 @@ urlpatterns = [
                                 "documents/supporting/<int:file_pk>/",
                                 InvoicePrivateMedia.as_view(),
                                 name="invoice-supporting-document",
-                            ),
-                            path(
-                                "partial/invoice-status/",
-                                partial_get_invoice_status,
-                                name="partial-invoice-status",
                             ),
                         ]
                     ),
