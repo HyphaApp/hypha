dc27de5e87d877d6bfc8bcc0f8f945ccbd8ce51a
Wes Appler <wes@opentech.fund>
2024-08-14 14:01:19 -0400

    Implemented HTMX for invoice detail view

--- hypha/apply/projects/urls.py
+++ hypha/apply/projects/urls.py
@@ -27,7 +27,9 @@ from .views import (
     ReportUpdateView,
     get_invoices_status_counts,
     get_project_status_counts,
+    partial_get_invoice_detail_actions,
     partial_get_invoice_status,
+    partial_get_invoice_status_table,
     partial_project_activities,
 )
 
@@ -92,14 +94,14 @@ urlpatterns = [
                 path("invoice/", CreateInvoiceView.as_view(), name="invoice"),
                 path(
                     "partial/invoice-status/",
-                    partial_get_invoice_status,
-                    name="partial-invoice-status",
+                    partial_get_invoice_status_table,
+                    name="partial-invoices-status",
                 ),
                 path(
                     "partial/rejected-invoice-status/",
-                    partial_get_invoice_status,
+                    partial_get_invoice_status_table,
                     {"rejected": True},
-                    name="partial-rejected-invoice-status",
+                    name="partial-rejected-invoices-status",
                 ),
                 path(
                     "invoices/<int:invoice_pk>/",
@@ -118,6 +120,16 @@ urlpatterns = [
                                 "delete/",
                                 DeleteInvoiceView.as_view(),
                                 name="invoice-delete",
+                            ),
+                            path(
+                                "partial/status/",
+                                partial_get_invoice_status,
+                                name="partial-invoice-status",
+                            ),
+                            path(
+                                "actions/",
+                                partial_get_invoice_detail_actions,
+                                name="partial-invoice-detail-actions",
                             ),
                             path(
                                 "documents/invoice/",
