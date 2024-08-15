6b908101f9d8166761ca596acf19aceae4f2b3b0
Sandeep Chauhan <sandeepsajan0@gmail.com>
2024-08-16 02:11:47 +0530

    Hide Project task and project settings if projects is disabled (#4076)
    Fixes #3906

--- hypha/apply/projects/wagtail_hooks.py
+++ hypha/apply/projects/wagtail_hooks.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from wagtail.contrib.modeladmin.options import modeladmin_register
+from wagtail.contrib.settings.registry import register_setting
 
 from .admin import ProjectAdminGroup
+from .models import ProjectSettings
 
 if settings.PROJECTS_ENABLED:
     modeladmin_register(ProjectAdminGroup)
+    register_setting(model=ProjectSettings)
