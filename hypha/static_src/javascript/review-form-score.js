aecf753249ab55e120d616c2ad5dfca8c9d8229b
Jesse Bickel <jbickel@opentechstrategies.com>
2024-08-26 16:05:28 -0500

    Use pure JavaScript
    Refactored by dolphin-mixtral:8x7b cfada4ba31c7 on ollama 0.3.7-rc5.
    
    Reformatted using prettier.
    
    Issue #3963

--- hypha/static_src/javascript/review-form-score.js
+++ hypha/static_src/javascript/review-form-score.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function () {
     "use strict";
 
     // grab all the selectors
@@ -9,24 +9,30 @@
 
     if (selectors.length > 1) {
         document.querySelector(".form--score-box").style.display = "block";
+
         // remove recommendation select box from array
         filtered_selectors = selectors.filter(
             (selector) => selector[0].text !== "Need More Info"
         );
-        filtered_selectors.forEach(function (selector) {
-            selector.onchange = calculate_score;
-        });
         calculate_score();
+
+        filtered_selectors.forEach((selector) => {
+            selector.addEventListener("change", calculate_score);
+        });
     }
 
     function calculate_score() {
         let score = 0;
-        filtered_selectors.forEach(function (selector) {
+        filtered_selectors.forEach((selector) => {
             const value = parseInt(selector.value);
+
             if (!isNaN(value) && value !== 99) {
                 score += value;
             }
         });
-        $(".form--score-box").text("Score: " + score);
+
+        // Update the text in .form--score-box with the current score
+        document.querySelector(".form--score-box").textContent =
+            "Score: " + score;
     }
-})(jQuery);
+})();
