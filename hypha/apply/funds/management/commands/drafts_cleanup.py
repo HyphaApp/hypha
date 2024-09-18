3f73191547c5d3b217fd8b2068a7c4e58c9cbce5
Wes Appler <wes@opentech.fund>
2024-09-18 16:00:34 -0400

    Fixed help text & added documentation for Cron Jobs

--- hypha/apply/funds/management/commands/drafts_cleanup.py
+++ hypha/apply/funds/management/commands/drafts_cleanup.py
@@ -9,10 +9,17 @@ from hypha.apply.funds.models.submissions import ApplicationSubmission
 from hypha.apply.funds.workflow import DRAFT_STATE
 
 
-def check_not_negative(value):
+def check_not_negative(value) -> int:
     """Used to validate `older_than_days` argument
 
-    Needs to be a non-negative int
+    Args:
+        value: Argument to be validated
+
+    Returns:
+        int: Valid non-negative value
+
+    Raises:
+        argparse.ArgumentTypeError: if not non-negative integer
     """
     try:
         ivalue = int(value)
@@ -27,14 +34,16 @@ def check_not_negative(value):
 
 
 class Command(BaseCommand):
-    help = "Delete all drafts that are older than 2 years"
+    help = (
+        "Delete all drafts that haven't been modified in the specified time (in days)"
+    )
 
     def add_arguments(self, parser):
         parser.add_argument(
             "older_than_days",
             action="store",
             type=check_not_negative,
-            help="Clear all applications older than the days specified here",
+            help="Time in days to delete drafts older than",
         )
         parser.add_argument(
             "--noinput",
@@ -60,13 +69,13 @@ class Command(BaseCommand):
 
         if not (draft_count := old_drafts.count()):
             self.stdout.write(
-                f"No drafts older than {older_than} day{"s" if older_than > 1 else ""} exist."
+                f"No drafts older than {older_than} day{'s' if older_than > 1 else ''} exist."
             )
             return
 
         if interactive:
             confirm = input(
-                f"This action will permanently delete {draft_count} draft{"s" if draft_count != 1 else ""}.\nAre you sure you want to do this?\n\nType 'yes' to continue, or 'no' to cancel: "
+                f"This action will permanently delete {draft_count} draft{'s' if draft_count != 1 else ''}.\nAre you sure you want to do this?\n\nType 'yes' to continue, or 'no' to cancel: "
             )
         else:
             confirm = "yes"
@@ -74,7 +83,7 @@ class Command(BaseCommand):
         if confirm == "yes":
             old_drafts.delete()
             self.stdout.write(
-                f"{draft_count} draft{"s" if draft_count != 1 else ""} deleted."
+                f"{draft_count} draft{'s' if draft_count != 1 else ''} deleted."
             )
         else:
             self.stdout.write("Deletion cancelled.")
