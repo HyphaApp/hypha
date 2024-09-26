34647057d3d4552279ea0bf21ac19d3d1aa34452
Jesse Bickel <jbickel@opentechstrategies.com>
2024-09-26 12:17:21 -0500

    Remove selector filtering
    This code did not seem to have any effect, at least vs vanilla Hypha.

--- hypha/static_src/javascript/review-form-score.js
+++ hypha/static_src/javascript/review-form-score.js
@@ -2,28 +2,21 @@
     "use strict";
 
     // grab all the selectors
-    let filtered_selectors;
     const selectors = Array.prototype.slice.call(
         document.querySelectorAll("select")
     );
 
     if (selectors.length > 1) {
         document.querySelector(".form--score-box").style.display = "block";
-
-        // remove recommendation select box from array
-        filtered_selectors = selectors.filter(
-            (selector) => selector[0].text !== "Need More Info"
-        );
         calculate_score();
-
-        filtered_selectors.forEach((selector) => {
+        selectors.forEach((selector) => {
             selector.addEventListener("change", calculate_score);
         });
     }
 
     function calculate_score() {
         let score = 0;
-        filtered_selectors.forEach((selector) => {
+        selectors.forEach((selector) => {
             const value = parseInt(selector.value);
 
             if (!isNaN(value) && value !== 99) {
