f25f350c1c1d3b18440fad807f1af1fa997a17ed
Jesse Bickel <jbickel@opentechstrategies.com>
2024-10-09 10:57:52 -0500

    Add button to toggle between total and average
    Allow any reviewer to switch view between total and average while on
    the page.
    
    Issue #3963

--- hypha/static_src/javascript/review-form-score.js
+++ hypha/static_src/javascript/review-form-score.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
 
+    let use_average = false;
+
     // grab all the selectors
     const selectors = Array.prototype.slice.call(
         document.querySelectorAll("select")
@@ -12,20 +14,40 @@
         selectors.forEach((selector) => {
             selector.addEventListener("change", calculate_score);
         });
+        // Look also for the avg/total button
+        const toggleAverageButton = document.querySelector(
+            ".form--score-box button"
+        );
+        toggleAverageButton.addEventListener("click", toggle_use_average);
     }
 
     function calculate_score() {
-        let score = 0;
+        const formatter = new Intl.NumberFormat({
+            maximumSignificantDigits: 4,
+        });
+        let total = 0;
+        let count = 0;
         selectors.forEach((selector) => {
             const value = parseInt(selector.value);
 
             if (!isNaN(value) && value !== 99) {
-                score += value;
+                count += 1;
+                total += value;
             }
         });
+        let score = total;
+
+        if (use_average) {
+            score = total / count;
+        }
 
         // Update the text in .form--score-box with the current score
         document.querySelector(".form--score-box .score-number").textContent =
-            score;
+            formatter.format(score);
+    }
+
+    function toggle_use_average() {
+        use_average = !use_average;
+        calculate_score();
     }
 })();
