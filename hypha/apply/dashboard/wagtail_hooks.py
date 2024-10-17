f46b0bceb466d142b5f332c7cb5d202e7b065344
Saurabh Kumar <theskumar@users.noreply.github.com>
2024-10-17 22:16:17 +0530

    Update homepage to not used wagtail pages (#4127)
    Use standard django views to show hompeage instead of piggy backing on
    the wagtail. This allows to remove the "Pages" menu from the wagtail
    admin and have the homepage settings be at a single "System Settings"
    
    - update the hompeage to use Settings > System Settings
    - add data migration to move the properties from ApplyHompeage model to
      SystemSettings
    - hide "Pages" menu from wagtail
    - Refractor the homepage template
    
    Fixes: https://github.com/HyphaApp/hypha/issues/4116

--- hypha/apply/dashboard/wagtail_hooks.py
+++ hypha/apply/dashboard/wagtail_hooks.py
@@ -1,18 +1,14 @@
-from urllib.parse import urljoin
-
 from django.urls import reverse
+from django.utils.translation import gettext as _
 from wagtail import hooks
 from wagtail.admin.menu import MenuItem
-
-from hypha.home.models import ApplyHomePage
 
 
 @hooks.register("register_admin_menu_item")
 def register_dashboard_menu_item():
-    apply_home = ApplyHomePage.objects.first()
     return MenuItem(
-        "Apply Dashboard",
-        urljoin(apply_home.url, reverse("dashboard:dashboard", "hypha.urls")),
+        _("Goto Dashboard"),
+        reverse("dashboard:dashboard"),
         classname="icon icon-arrow-left",
         order=100000,
     )
