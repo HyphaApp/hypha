0b4c9a9ac5f3c6b89b3c3c4697fccbc6b5ade40e
Fredrik Jonsson <frjo@xdeb.org>
2024-11-04 11:12:38 +0100

    Fix bug in submission-text-cleanup that made all tables display in the last field with a table.

--- hypha/static_src/javascript/submission-text-cleanup.js
+++ hypha/static_src/javascript/submission-text-cleanup.js
@@ -1,4 +1,5 @@
 (function () {
+    // Select all rich text answers.
     const richtextanswers = document.querySelector(".rich-text--answers");
 
     // Remove p tags with only whitespace inside.
@@ -9,12 +10,11 @@
         }
     });
 
-    let table_wrapper = document.createElement("div");
-    table_wrapper.classList.add("rich-text__table");
-
     // Wrap all tables in a div so overflow auto works.
     const richtextanswers_tables = richtextanswers.querySelectorAll("table");
     richtextanswers_tables.forEach(function (table) {
+        const table_wrapper = document.createElement("div");
+        table_wrapper.classList.add("rich-text__table");
         table.parentNode.insertBefore(table_wrapper, table);
         table_wrapper.appendChild(table);
     });
