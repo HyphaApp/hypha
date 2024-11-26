615532e6750e17e4aa439982aa8ce13009069323
Fredrik Jonsson <frjo@xdeb.org>
2024-11-26 20:04:24 +0100

    Removing some more old CSS. (#4230)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
             colors: {
                 "light-blue": "#0d7db0",
                 "dark-blue": "#0c72a0",
+                "sky-blue": "#e7f2f6",
                 tomato: "#f05e54",
                 "mid-grey": "#cfcfcf",
                 "light-grey": "#f7f7f7",
