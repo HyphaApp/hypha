4947f78f1ed4f5aeb8c4e3e6a2e483eca370d39e
Fredrik Jonsson <frjo@xdeb.org>
2024-12-06 21:16:26 +0100

    Rename PAF to project form in more places (#4254)
    Fixes #4252
    
    Hopefully this is the last of it.

--- hypha/apply/activity/options.py
+++ hypha/apply/activity/options.py
@@ -42,8 +42,8 @@ class MESSAGES(TextChoices):
     EDIT_REVIEW = "EDIT_REVIEW", _("edited review")
     SEND_FOR_APPROVAL = "SEND_FOR_APPROVAL", _("sent for approval")
     APPROVE_PROJECT = "APPROVE_PROJECT", _("approved project")
-    ASSIGN_PAF_APPROVER = "ASSIGN_PAF_APPROVER", _("assign paf approver")
-    APPROVE_PAF = "APPROVE_PAF", _("approved paf")
+    ASSIGN_PAF_APPROVER = "ASSIGN_PAF_APPROVER", _("assign project form approver")
+    APPROVE_PAF = "APPROVE_PAF", _("approved project form")
     PROJECT_TRANSITION = "PROJECT_TRANSITION", _("transitioned project")
     REQUEST_PROJECT_CHANGE = "REQUEST_PROJECT_CHANGE", _("requested project change")
     SUBMIT_CONTRACT_DOCUMENTS = (
