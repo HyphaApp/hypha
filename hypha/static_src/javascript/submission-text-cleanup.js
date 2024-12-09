47054b14fba485ee72bbeaf03378c87264e5d133
Fredrik Jonsson <frjo@xdeb.org>
2024-12-09 08:59:30 +0100

    Set dir auto via js on all p tags in applications.

--- hypha/static_src/javascript/submission-text-cleanup.js
+++ hypha/static_src/javascript/submission-text-cleanup.js
@@ -2,16 +2,20 @@
     // Select all rich text answers.
     const richtextanswers = document.querySelector(".rich-text--answers");
 
-    // Remove p tags with only whitespace inside.
+    // Select all rich text answers paragraphes.
     const richtextanswers_paras = richtextanswers.querySelectorAll("p");
     richtextanswers_paras.forEach(function (para) {
+        // Remove p tags with only whitespace inside.
         if (para.textContent.trim() === "") {
             para.remove();
         }
+        // Set dir="auto" so browsers sets the correct directionality (ltr/rtl).
+        para.setAttribute('dir', 'auto');
     });
 
-    // Wrap all tables in a div so overflow auto works.
+    // Select all rich text answers tables.
     const richtextanswers_tables = richtextanswers.querySelectorAll("table");
+    // Wrap all tables in a div so overflow auto works.
     richtextanswers_tables.forEach(function (table) {
         const table_wrapper = document.createElement("div");
         table_wrapper.classList.add("rich-text__table");
