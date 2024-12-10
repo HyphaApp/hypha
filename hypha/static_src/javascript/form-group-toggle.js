0245c2ea0f3f0b7e52a5b38faa0ded8010fe8057
Fredrik Jonsson <frjo@xdeb.org>
2024-12-10 10:59:25 +0100

    form-group-toggle.js as vanilla js.

--- hypha/static_src/javascript/form-group-toggle.js
+++ hypha/static_src/javascript/form-group-toggle.js
@@ -1,57 +1,63 @@
-/* eslint-disable no-loop-func */
-(function ($) {
-    var i;
-    for (i = 2; i < 20; i++) {
-        var $field_group = $(".field-group-" + i);
-        if ($field_group.length) {
-            var classes = "field-group-wrapper field-group-wrapper-" + i;
-            $field_group.each(function () {
-                if (
-                    $(this).data("hidden") &&
-                    classes.indexOf("js-hidden") === -1
-                ) {
+/* eslint-disable max-nested-callbacks */
+(function () {
+    for (let i = 2; i < 20; i++) {
+        const field_group = document.querySelectorAll(".field-group-" + i);
+        if (field_group.length) {
+            let classes = "field-group-wrapper field-group-wrapper-" + i;
+            field_group.forEach(function (el) {
+                if (el.dataset.hidden && classes.indexOf("js-hidden") === -1) {
                     classes += " js-hidden";
                 }
             });
-            $field_group.wrapAll('<div class="' + classes + '" />');
+            var wrapper = document.createElement("div");
+            wrapper.classList.add(classes);
+            field_group[0].parentNode.insertBefore(wrapper, field_group[0]);
+            wrapper.appendChild(field_group[0]);
         } else {
             break;
         }
     }
 
-    $('.form-fields-grouper input[type="radio"]').on("change", function () {
-        var radio_input_value = $(this).val();
-        var fields_grouper_div = this.closest(".form-fields-grouper");
-        var fields_grouper_for = $(fields_grouper_div).data("grouper-for");
-        var group_toggle_on_value = $(fields_grouper_div).data("toggle-on");
-        var group_toggle_off_value = $(fields_grouper_div).data("toggle-off");
+    document.querySelectorAll('.form-fields-grouper input[type="radio"]').forEach(function (radio) {
+        radio.addEventListener("change", function () {
+            const radio_input_value = this.value;
+            const fields_grouper_div = this.closest(".form-fields-grouper");
+            const fields_grouper_for = fields_grouper_div.dataset.grouperFor;
+            const group_toggle_on_value = fields_grouper_div.dataset.toggleOn;
+            const group_toggle_off_value = fields_grouper_div.dataset.toggleOff;
 
-        if (radio_input_value === group_toggle_on_value) {
-            $(".field-group-wrapper-" + fields_grouper_for)
-                .removeClass("js-hidden")
-                .addClass("highlighted");
-            $(".field-group-" + fields_grouper_for).each(function () {
-                if ($(this).data("required") === "True") {
-                    $(this)
-                        .children(".form__item")
-                        .children()
-                        .attr("required", true);
-                    $(this)
-                        .children("label")
-                        .append('<span class="form__required">*</span>');
-                }
-            });
-        } else if (radio_input_value === group_toggle_off_value) {
-            $(".field-group-wrapper-" + fields_grouper_for)
-                .removeClass("highlighted")
-                .addClass("js-hidden");
-            $(".field-group-" + fields_grouper_for).each(function () {
-                $(this)
-                    .children(".form__item")
-                    .children()
-                    .attr("required", false);
-                $(this).children("label").children(".form__required").remove();
-            });
-        }
+            if (radio_input_value === group_toggle_on_value) {
+                var wrapper_on = document.querySelector(".field-group-wrapper-" + fields_grouper_for);
+                wrapper_on.classList.remove("js-hidden");
+                wrapper_on.classList.add("highlighted");
+                document.querySelectorAll(".field-group-" + fields_grouper_for).forEach(function (el) {
+                    if (el.dataset.required === "True") {
+                        el.querySelectorAll(".form__item > *").forEach(function (child) {
+                            child.required = true;
+                        });
+                        const label = el.querySelector("label");
+                        if (label) {
+                            const span = document.createElement("span");
+                            span.classList.add("form__required");
+                            span.textContent = "*";
+                            label.appendChild(span);
+                        }
+                    }
+                });
+            } else if (radio_input_value === group_toggle_off_value) {
+                const wrapper_off = document.querySelector(".field-group-wrapper-" + fields_grouper_for);
+                wrapper_off.classList.remove("highlighted");
+                wrapper_off.classList.add("js-hidden");
+                document.querySelectorAll(".field-group-" + fields_grouper_for).forEach(function (el) {
+                    el.querySelectorAll(".form__item > *").forEach(function (child) {
+                        delete child.required;
+                    });
+                    const span = el.querySelector("label .form__required");
+                    if (span) {
+                        span.remove();
+                    }
+                });
+            }
+        });
     });
-})(jQuery);
+})();
