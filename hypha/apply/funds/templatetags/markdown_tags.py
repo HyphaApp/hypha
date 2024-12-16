b0d342e9b3035676d174421a96a2a634949a5f82
Fredrik Jonsson <frjo@xdeb.org>
2024-12-16 09:34:15 +0100

    Remove tomd and to_markdown() since they are not used anymore.

--- hypha/apply/funds/templatetags/markdown_tags.py
+++ hypha/apply/funds/templatetags/markdown_tags.py
@@ -1,4 +1,3 @@
-import tomd
 from django import template
 
 from hypha.core.utils import markdown_to_html
@@ -9,11 +8,3 @@ register = template.Library()
 @register.filter
 def markdown(value):
     return markdown_to_html(value)
-
-
-@register.filter
-def to_markdown(value):
-    # pass through markdown to ensure comment is a
-    # fully formed HTML block
-    value = markdown_to_html(value)
-    return tomd.convert(value)
