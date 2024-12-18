5fcf0956412feb7a668877e81977a311484a32aa
Wes Appler <wes@opentech.fund>
2024-12-18 09:35:09 -0500

    Removed Endpoints completely & made final fixes

--- hypha/apply/projects/views/payment.py
+++ hypha/apply/projects/views/payment.py
@@ -229,12 +229,6 @@ class DeleteInvoiceView(DeleteView):
 
 class InvoiceAdminView(InvoiceAccessMixin, DelegateableView, DetailView):
     form_views = []
-    # template_name_suffix = "_admin_detail"
-
-    # def get_context_data(self, **kwargs):
-    #     invoice = self.get_object()
-    #     project = invoice.project
-    #     return super().get_context_data(**kwargs)
 
 
 class InvoiceApplicantView(InvoiceAccessMixin, DelegateableView, DetailView):
