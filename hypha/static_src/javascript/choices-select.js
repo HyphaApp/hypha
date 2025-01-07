9a5e2e996dda14c4ebdeead9757802385aa4ff35
sandeepsajan0 <sandeepsajan0@gmail.com>
2025-01-07 16:12:57 +0530

    Rebased and fixed biome lint issue

--- hypha/static_src/javascript/choices-select.js
+++ hypha/static_src/javascript/choices-select.js
@@ -1,10 +1,11 @@
-const selectElements = document.querySelectorAll('.js-choices');
+const selectElements = document.querySelectorAll(".js-choices");
 
-selectElements.forEach(selectElement => {
-    // eslint-disable-next-line no-undef
-    new Choices(selectElement, {
-        shouldSort: false,
-        allowHTML: true,
-        removeItemButton: true,
-    });
+selectElements.forEach((selectElement) => {
+  // eslint-disable-next-line no-undef
+  // biome-ignore lint: undeclared
+  new Choices(selectElement, {
+    shouldSort: false,
+    allowHTML: true,
+    removeItemButton: true,
+  });
 });
