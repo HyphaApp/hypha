631796901eb13074e0c8fb5e095d1e347629a167
sandeepsajan0 <sandeepsajan0@gmail.com>
2025-02-06 10:01:40 +0530

    Add author updated feature to submission

--- hypha/apply/activity/adapters/base.py
+++ hypha/apply/activity/adapters/base.py
@@ -39,6 +39,7 @@ neat_related = {
     MESSAGES.REVIEW_REMINDER: "reminder",
     MESSAGES.BATCH_UPDATE_INVOICE_STATUS: "invoices",
     MESSAGES.REMOVE_TASK: "task",
+    MESSAGES.UPDATED_AUTHOR: "old_author",
 }
 
 
--- hypha/apply/funds/urls.py
+++ hypha/apply/funds/urls.py
@@ -58,6 +58,7 @@ from .views.submission_edit import (
     CreateProjectView,
     ProgressSubmissionView,
     SubmissionEditView,
+    UpdateAuthorView,
     UpdateLeadView,
     UpdateMetaTermsView,
     UpdateReviewersView,
@@ -248,6 +249,11 @@ submission_urls = (
                         "reminder/create/",
                         ReminderCreateView.as_view(),
                         name="create_reminder",
+                    ),
+                    path(
+                        "author/change/",
+                        UpdateAuthorView.as_view(),
+                        name="change_author",
                     ),
                     path(
                         "translate/",
