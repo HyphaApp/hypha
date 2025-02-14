6d4865b2f72d36008cd48c3e3cda310033415bc5
Fredrik Jonsson <frjo@xdeb.org>
2025-02-14 15:41:27 +0100

    Language switch form.

--- hypha/core/context_processors.py
+++ hypha/core/context_processors.py
@@ -23,5 +23,6 @@ def global_vars(request):
         "SENTRY_PUBLIC_KEY": settings.SENTRY_PUBLIC_KEY,
         "SUBMISSIONS_TABLE_EXCLUDED_FIELDS": settings.SUBMISSIONS_TABLE_EXCLUDED_FIELDS,
         "HIJACK_ENABLE": settings.HIJACK_ENABLE,
+        "LANGUAGES": settings.LANGUAGES,
         "LANGUAGE_SWITCHER": settings.LANGUAGE_SWITCHER,
     }
