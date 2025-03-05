7b514c49c052315a90421fcdca1c3df767eecfd6
Fredrik Jonsson <frjo@xdeb.org>
2025-03-05 09:21:15 +0100

    Lint fixes.

--- hypha/core/templatehook.py
+++ hypha/core/templatehook.py
@@ -1,12 +1,13 @@
 # Adds the ability to have hooks injected into templates, via the `hooks_tags.hook` template tag.
-# 
+#
 # Originally from https://github.com/nitely/django-hooks but after culling everything except
 # the template hook.  See /docs/templatehook.rst for more information
-# 
+#
 # The reason this was forked, rather than used as a module, is that the last commit from the
 # other project was made in 2015, and can no longer be imported into a modern django project.
 # The template hook continues to work correctly, but the other hooks most likely do not, and
 # so were removed.
+
 
 class TemplateHook(object):
     """
