9eb63fdf009ef3bcc65f15218b277bc99f6bce18
sandeepsajan0 <sandeepsajan0@gmail.com>
2025-03-17 16:54:22 +0530

    Fix multiple, daterange filters and show selected filter with remove option

--- hypha/apply/funds/templatetags/table_tags.py
+++ hypha/apply/funds/templatetags/table_tags.py
@@ -2,6 +2,7 @@ import copy
 import math
 
 from django import forms, template
+from django_filters.fields import DateRangeField
 
 from hypha.apply.categories.models import MetaTerm
 from hypha.apply.users.models import User
@@ -12,6 +13,36 @@ register = template.Library()
 @register.simple_tag
 def prepare_query_data(key, value):
     return "{key}={value}".format(key=key, value=value)
+
+
+@register.simple_tag
+def get_filtered_query(request, form):
+    query_list = []
+    for key, values in request.GET.lists():
+        field = form.fields.get(key)
+        if not field:
+            # Handle DateRangePicker
+            if "_after" in key or "_before" in key:
+                for value in values:
+                    query_list.append(
+                        {key: {"value": value, "label": value, "key_label": key}}
+                    )
+            continue
+
+        choices_dict = {}
+        # Handle ModelChoiceField (for ModelMultipleChoiceFilter)
+        if hasattr(field, "queryset"):
+            choices_dict = {str(obj.pk): str(obj) for obj in field.queryset}
+        # Handle ChoiceField (for MultipleChoiceFilter)
+        elif hasattr(field, "choices"):
+            choices_dict = dict(field.choices)
+
+        for value in values:
+            label = choices_dict.get(str(value), value)
+            query_list.append(
+                {key: {"value": value, "label": label, "key_label": field.label}}
+            )
+    return query_list
 
 
 @register.simple_tag
@@ -28,8 +59,15 @@ def get_field_choices(form, field_name):
 
 
 @register.filter
-def check_multiple(field):
+def is_multiple(field):
     return field.widget.allow_multiple_selected
+
+
+@register.filter
+def is_datefilter(field):
+    if isinstance(field, DateRangeField):
+        return True
+    return False
 
 
 @register.filter
