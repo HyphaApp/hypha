21944687c449604eaef2106b6715d2d7f41d63a0
sandeepsajan0 <sandeepsajan0@gmail.com>
2025-03-23 12:25:11 +0530

    Update querystring method to add/modify/remove key value pair for updated filters

--- hypha/core/templatetags/querystrings.py
+++ hypha/core/templatetags/querystrings.py
@@ -62,32 +62,6 @@ def modify_query(context, *params_to_remove, **params_to_change):
 
 
 @register.simple_tag(takes_context=True)
-def dup_modify_query(
-    context, param_to_remove, param_key_to_change, param_value_to_change
-):
-    query_params = []
-    param_key_used = False
-    for key, value_list in context["request"].GET.lists():
-        if key != param_to_remove:
-            # don't add key-value pairs for params_to_remove
-            if key == param_key_to_change:
-                # update values for keys in params_to_change
-                query_params.append((key, param_value_to_change))
-                param_key_used = True
-            else:
-                # leave existing parameters as they were
-                # if not mentioned in the params_to_change
-                for value in value_list:
-                    query_params.append((key, value))
-                    # attach new params
-    if not param_key_used:
-        query_params.append((param_key_to_change, param_value_to_change))
-    return construct_query_string(
-        context=context, query_params=query_params, only_query_string=False
-    )
-
-
-@register.simple_tag(takes_context=True)
 def add_to_query(context, *params_to_remove, **params_to_add):
     """Renders a link with modified current query parameters"""
     only_query_string = False
@@ -115,34 +89,6 @@ def add_to_query(context, *params_to_remove, **params_to_add):
 
 
 @register.simple_tag(takes_context=True)
-def dup_add_to_query(
-    context,
-    param_to_remove,
-    param_key_to_add,
-    param_value_to_add,
-    only_query_string=False,
-):
-    """Renders a link with modified current query parameters"""
-    query_params = []
-    param_key_used = False
-    # go through current query params..
-    for key, value_list in context["request"].GET.lists():
-        if key != param_to_remove:
-            # don't add key-value pairs which already
-            # exist in the query
-            if key == param_key_to_add and param_value_to_add in value_list:
-                param_key_used = True
-            for value in value_list:
-                query_params.append((key, value))
-    # add the rest key-value pairs
-    if not param_key_used:
-        query_params.append((param_key_to_add, param_value_to_add))
-    return construct_query_string(
-        context=context, query_params=query_params, only_query_string=only_query_string
-    )
-
-
-@register.simple_tag(takes_context=True)
 def remove_from_query(context, *args, **kwargs):
     """Renders a link with modified current query parameters"""
     only_query_string = False
@@ -164,21 +110,38 @@ def remove_from_query(context, *args, **kwargs):
 
 
 @register.simple_tag(takes_context=True)
-def dup_remove_from_query(
-    context, param_to_remove, param_key_to_remove, param_value_to_remove
+def update_filter_query_params(
+    context,
+    param_key,
+    param_value,
+    operation="remove",
 ):
-    query_params = []
-    # go through current query params..
-    for key, value_list in context["request"].GET.lists():
-        # skip keys mentioned in the args
-        if key != param_to_remove:
-            for value in value_list:
-                # skip key-value pairs mentioned in kwargs
-                if not (
-                    key == param_key_to_remove
-                    and str(value) == str(param_value_to_remove)
-                ):
-                    query_params.append((key, value))
+    """
+    Update query params for django-filters with provided key value pair and respective operation(add, modify or remove)
+    """
+    query_params = context["request"].GET.copy()  # Make a mutable copy
+
+    if operation == "remove":
+        values = query_params.getlist(param_key)  # Get all values for the key
+        updated_values = [v for v in values if str(v) != str(param_value)]
+        if updated_values:
+            query_params.setlist(param_key, updated_values)
+        else:
+            query_params.pop(param_key, None)  # Remove key if no values left
+    elif operation == "modify":
+        query_params[param_key] = param_value  # Replace existing value
+    elif operation == "add":
+        # Add param_value only if it's not already present
+        if param_value not in query_params.getlist(param_key):
+            query_params.appendlist(param_key, param_value)
+
+    # Convert query_params to a list of tuples [(key, value), ...] for construct_query_string
+    query_params_list = [
+        (key, value) for key in query_params for value in query_params.getlist(key)
+    ]
+
     return construct_query_string(
-        context=context, query_params=query_params, only_query_string=False
+        context=context,
+        query_params=query_params_list,
+        only_query_string=(operation == "add"),
     )
