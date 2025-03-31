0ef271376a7f6a0f488f4bbfca7af0cce6565a4d
Sandeep Chauhan <sandeepsajan0@gmail.com>
2025-03-31 13:35:31 +0530

    Use new filters UI with django filters (#4425)
    Fixes #4345 
    
    
    Co-authored-by: Fredrik Jonsson <frjo@xdeb.org>

--- hypha/apply/funds/templatetags/table_tags.py
+++ hypha/apply/funds/templatetags/table_tags.py
@@ -1,12 +1,87 @@
 import copy
 import math
 
-from django import template
+from django import forms, template
+from django_filters.fields import DateRangeField
 
 from hypha.apply.categories.models import MetaTerm
 from hypha.apply.users.models import User
 
 register = template.Library()
+
+
+@register.simple_tag
+def prepare_query_data(key, value):
+    return "{key}={value}".format(key=key, value=value)
+
+
+@register.simple_tag
+def get_filtered_query(request, form):
+    query_list = []
+    for key, values in request.GET.lists():
+        field = form.fields.get(key)
+        if not field:
+            # Handle DateRangePicker
+            if "_after" in key or "_before" in key:
+                for value in values:
+                    query_list.append(
+                        {key: {"value": value, "label": value, "key_label": key}}
+                    )
+            continue
+
+        choices_dict = {}
+        # Handle ModelChoiceField (for ModelMultipleChoiceFilter)
+        if hasattr(field, "queryset"):
+            choices_dict = {str(obj.pk): str(obj) for obj in field.queryset}
+        # Handle ChoiceField (for MultipleChoiceFilter)
+        elif hasattr(field, "choices"):
+            choices_dict = dict(field.choices)
+
+        for value in values:
+            label = choices_dict.get(str(value), value)
+            query_list.append(
+                {key: {"value": value, "label": label, "key_label": field.label}}
+            )
+    return query_list
+
+
+@register.simple_tag
+def get_item_value(form, key):
+    return form[key].value()
+
+
+@register.filter
+def get_field_choices(form, field_name):
+    """Returns the choices of a form field."""
+    if hasattr(form[field_name].field, "choices"):
+        return form[field_name].field.choices if field_name in form.fields else []
+    return []
+
+
+@register.filter
+def get_dropdown_heading(field):
+    return f"Filter by {field.label}"
+
+
+@register.filter
+def is_multiple(field):
+    return field.widget.allow_multiple_selected
+
+
+@register.filter
+def is_datefilter(field):
+    if isinstance(field, DateRangeField):
+        return True
+    return False
+
+
+@register.filter
+def get_filter_fields(form):
+    fields = []
+    for field_name, field in form.fields.items():
+        if not isinstance(field.widget, forms.HiddenInput):
+            fields.append((field_name, field))
+    return fields
 
 
 @register.filter
