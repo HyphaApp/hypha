21993c6c99388b8bb81df8af610df950c97f38e7
sandeepsajan0 <sandeepsajan0@gmail.com>
2025-04-09 18:18:28 +0530

    Fix tests by handling new submission draft

--- hypha/apply/funds/models/submissions.py
+++ hypha/apply/funds/models/submissions.py
@@ -661,9 +661,10 @@ class ApplicationSubmission(
         Returns:
             Self with the `form_data` attribute updated.
         """
-        self.form_data = self.deserialised_data(
-            self, self.draft_revision.form_data, self.form_fields
-        )
+        if self.draft_revision:
+            self.form_data = self.deserialised_data(
+                self, self.draft_revision.form_data, self.form_fields
+            )
 
         return self
 
@@ -743,8 +744,13 @@ class ApplicationSubmission(
         self.process_form_data()
         self.ensure_user_has_account()
         # pass current submission data to avoid file save on every submit(if file is not updated)
-        current_submission = ApplicationSubmission.objects.get(id=self.id)
-        self.process_file_data(self.form_data, current_submission.from_draft().raw_data)
+        if self.id:
+            current_submission = ApplicationSubmission.objects.get(id=self.id)
+            self.process_file_data(
+                self.form_data, current_submission.from_draft().raw_data
+            )
+        else:
+            self.process_file_data(self.form_data)
 
     def get_assigned_meta_terms(self):
         """Returns assigned meta terms excluding the 'root' term"""
