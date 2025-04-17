dbec8a870ca31c3f64fc2e26e2818a429b0f7076
Fredrik Jonsson <frjo@xdeb.org>
2025-04-17 21:39:33 +0200

    Do not cache application id.

--- hypha/apply/funds/models/submissions.py
+++ hypha/apply/funds/models/submissions.py
@@ -27,7 +27,6 @@ from django.db.models.functions import Cast
 from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.functional import cached_property
 from django.utils.html import strip_tags
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
@@ -605,11 +604,11 @@ class ApplicationSubmission(
             # We are a lab submission
             return getattr(self.page.specific, attribute)
 
-    @cached_property
+    @property
     def fund_name(self):
         return self.page
 
-    @cached_property
+    @property
     def application_id(self):
         return self.public_id or self.id
 
