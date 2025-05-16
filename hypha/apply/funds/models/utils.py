f4056a87f94f6d01070342055f092c4c8a976dd4
Wes Appler <145372368+wes-otf@users.noreply.github.com>
2025-05-16 11:33:24 -0400

    Async exporting of applications (#4471)
    This PR makes the generation of submission exports async as (especially
    in production) the generation of these files can take a long time and
    cause a request timeout. This allows the user to do other tasks while
    the file generates, and will notify them when the file is ready for
    download via a new task on their dashboard or a green indicator on the
    submission export button in the "All Submissions" view.
    
    **Changes TLDR**:
    - Added tasks to `hypha.apply.funds` module to handle the generation of
    the submissions CSV
    - Added a `SubmissionExportManager` model that keeps track of the status
    of the generation & the finalized data
         - Each user can only have one active model/download
    - Added a check in the nprogress event to allow for the disabling of the
    progress bar while polling
    - Preserved all sync functionality for users that don't need the
    background worker, which is the default behavior
    
    This is a WIP that should be completed early next week, but wanted to
    get eyes on it early for any critiques. Steps left:
    - [x] Add unit tests
    - [x] Make a management command to remove `SubmissionExportManager`s
    older than a day
    - [x] Error handling for a failed generation
    - [x] Triggering updating of  `My Tasks` when downloading from dashboard
    
    **Running locally**
    1. Get a Redis instance setup locally (I used docker, ie. `docker run -d
    -p 6379:6379 redis`)
    2. Install requirements
    3. In a separate terminal from the running `make serve` (but with same
    environment vars), run `celery -A hypha.celery worker -E
    --loglevel=info` to start celery
    
    ## Demo
    
    *Generating and downloading the exported CSV*
    
    
    https://github.com/user-attachments/assets/71758bf1-2a0c-461f-9a4a-6942bf77b694
    
    *Failed generation of a CSV*
    
    
    https://github.com/user-attachments/assets/bb3029b2-0eb2-471a-930e-d8c85ff4ed6f

--- hypha/apply/funds/models/utils.py
+++ hypha/apply/funds/models/utils.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from wagtail.admin.panels import (
     FieldPanel,
@@ -200,3 +201,52 @@ class EmailForm(AbstractEmailForm):
     ]
 
     email_tab = ObjectList(email_confirmation_panels, heading=_("Confirmation email"))
+
+
+# Managing async submission exports
+
+STATUS_ERROR = "error"
+STATUS_SUCCESS = "success"
+STATUS_GENERATING = "generating"
+
+STATUS_CHOICES = [
+    (STATUS_ERROR, _("Failed")),
+    (STATUS_SUCCESS, _("Success")),
+    (STATUS_GENERATING, _("In Progress")),
+]
+
+
+class SubmissionExportManager(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to=LIMIT_TO_STAFF,
+        on_delete=models.CASCADE,
+    )
+
+    export_data = models.TextField()
+
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    completed_time = models.DateTimeField(null=True)
+
+    status = models.CharField(choices=STATUS_CHOICES, default=STATUS_GENERATING)
+
+    total_export = models.IntegerField(null=True)
+
+    def set_completed_and_save(self) -> None:
+        """Sets the status to completed and saves the object"""
+        self.status = "success"
+        self.completed_time = timezone.now()
+        self.save()
+
+    def set_failed_and_save(self) -> None:
+        """Sets the status to error and saves the object"""
+        self.status = "error"
+        self.save()
+
+    def get_absolute_url(self) -> str:
+        """Returns the submissions all page where the user can download the file
+
+        Primarily used for tasks
+        """
+        return reverse("apply:submissions:list")
