f88deecbb9b6dcb9d0e19d1baa23c17bc9a4db81
Fredrik Jonsson <frjo@xdeb.org>
2025-05-26 10:43:15 +0200

    Add a json output with open calls. Same list as on front page in json format.

--- hypha/home/views.py
+++ hypha/home/views.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
+from django_ratelimit.decorators import ratelimit
 
 from hypha.apply.funds.models import ApplicationBase, LabBase
 
@@ -18,3 +21,22 @@ def home(request):
         fund for fund in all_funds if fund.open_round and fund.list_on_front_page
     ]
     return render(request, "home/home.html", ctx)
+
+
+@ratelimit(key="ip", rate=settings.DEFAULT_RATE_LIMIT)
+def open_calls_json(request):
+    """Open calls in JSON format.
+
+    List open calls in JSON format, useful when you want to list open calls on an external site.
+    """
+    rounds = ApplicationBase.objects.order_by_end_date().specific()
+    labs = LabBase.objects.public().live().specific()
+    all_funds = list(rounds) + list(labs)
+
+    # Only pass rounds/labs that are open & visible for the front page
+    data = [
+        fund.as_json
+        for fund in all_funds
+        if fund.open_round and fund.list_on_front_page
+    ]
+    return JsonResponse(data, safe=False)
