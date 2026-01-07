d662aecd7c8e0a8f4e49020f400f748450873fc7
Fredrik Jonsson <frjo@xdeb.org>
2026-01-07 13:24:09 +0100

    Add text explaing why elevate is a hypha app now.

--- hypha/elevate/__init__.py
+++ hypha/elevate/__init__.py
@@ -5,6 +5,9 @@ elevate
 :copyright: (c) 2017-present by Justin Mayer.
 :copyright: (c) 2014-2016 by Matt Robenolt.
 :license: BSD, see LICENSE for more details.
+
+2026-01-07: Moved in to hypha to avoid Setuptools complaining
+about use of obsolete pkg_resources.
 """
 
 from importlib.metadata import version
