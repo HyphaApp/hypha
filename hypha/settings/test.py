5ca94677ceeb72b65ea3af7ef65d80a4160045c6
Fredrik Jonsson <frjo@xdeb.org>
2026-01-11 13:09:52 +0100

    Fix STORAGES test setting.

--- hypha/settings/test.py
+++ hypha/settings/test.py
@@ -17,7 +17,9 @@ PROJECTS_AUTO_CREATE = True
 TRANSITION_AFTER_REVIEWS = 2
 TRANSITION_AFTER_ASSIGNED = True
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STORAGES["staticfiles"] = {
+    "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+}
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
