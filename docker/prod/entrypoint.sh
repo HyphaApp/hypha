90273dd4098d4fa62e71910d9fe9d448f32d1e2b
Fredrik Jonsson <frjo@xdeb.org>
2026-02-16 15:03:57 +0100

    Production docker improvements. (#4701)

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -6,7 +6,4 @@ python manage.py migrate --noinput
 python manage.py clear_cache --cache=default
 python manage.py sync_roles
 
-# Start gunicorn server.
-gunicorn hypha.wsgi:application --env DJANGO_SETTINGS_MODULE=hypha.settings.production --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread  --bind 0.0.0.0:8000
-
 exec "$@"
