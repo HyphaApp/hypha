b0aadb389105ef13c35e1946f5fa789a92681a5d
Fredrik Jonsson <frjo@xdeb.org>
2026-03-02 14:47:05 +0100

    Remove summary part of result view to improve performance.

--- hypha/apply/funds/views/results.py
+++ hypha/apply/funds/views/results.py
@@ -4,67 +4,16 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters.views import FilterView
 
-from hypha.apply.review.models import Review
-from hypha.apply.users.decorators import (
-    staff_required,
-)
+from hypha.apply.users.decorators import staff_required
 
-from ..models import (
-    ApplicationSubmission,
-)
-from ..tables import (
-    SubmissionFilterAndSearch,
-)
-from ..utils import (
-    format_submission_sum_value,
-    is_filter_empty,
-)
+from ..tables import SubmissionFilterAndSearch
 
 User = get_user_model()
 
 
-class SubmissionStatsMixin:
-    def get_context_data(self, **kwargs):
-        submissions = ApplicationSubmission.objects.exclude_draft()
-        filter = kwargs.get("filter")
-        user = self.request.user
-
-        # Getting values is an expensive operation. If there's no valid filters
-        # then `count_values` & `total_value` will be encapsulating all submissions
-        # and should be used rather than recaluclating these values.
-        if not filter or not is_filter_empty(filter):
-            submission_sum = kwargs.get("total_value")
-        else:
-            submission_value = submissions.current().value()
-            submission_sum = format_submission_sum_value(submission_value)
-
-        submission_undetermined_count = submissions.undetermined().count()
-        review_my_count = submissions.reviewed_by(user).count()
-
-        submission_accepted = submissions.current_accepted()
-        submission_accepted_value = submission_accepted.value()
-        submission_accepted_sum = format_submission_sum_value(submission_accepted_value)
-        submission_accepted_count = submission_accepted.count()
-
-        reviews = Review.objects.submitted()
-        review_count = reviews.count()
-        review_my_score = reviews.by_user(user).score()
-
-        return super().get_context_data(
-            submission_undetermined_count=submission_undetermined_count,
-            review_my_count=review_my_count,
-            submission_sum=submission_sum,
-            submission_accepted_count=submission_accepted_count,
-            submission_accepted_sum=submission_accepted_sum,
-            review_count=review_count,
-            review_my_score=review_my_score,
-            **kwargs,
-        )
-
-
 @method_decorator(cache_page(60), name="dispatch")
 @method_decorator(staff_required, name="dispatch")
-class SubmissionResultView(SubmissionStatsMixin, FilterView):
+class SubmissionResultView(FilterView):
     template_name = "funds/submissions_result.html"
     filterset_class = SubmissionFilterAndSearch
     filter_action = ""
@@ -91,9 +40,12 @@ class SubmissionResultView(SubmissionStatsMixin, FilterView):
             submission_count = self.object_list.count()
             submission_values = self.object_list.value()
             count_values = submission_values.get("value__count")
-            total_value = format_submission_sum_value(submission_values)
-            if value := submission_values.get("value__avg"):
-                average_value = round(value)
+            if total := submission_values.get("value__sum"):
+                total_value = total
+            else:
+                total_value = 0
+            if average := submission_values.get("value__avg"):
+                average_value = round(average)
             else:
                 average_value = 0
         else:
