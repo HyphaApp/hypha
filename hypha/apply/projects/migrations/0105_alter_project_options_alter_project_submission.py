420274d17964c9cc3564d6275b292146fbf62d14
Fredrik Jonsson <frjo@xdeb.org>
2026-07-07 08:31:42 +0200

    Migration fix after rebase.

--- hypha/apply/projects/migrations/0105_alter_project_options_alter_project_submission.py
+++ hypha/apply/projects/migrations/0105_alter_project_options_alter_project_submission.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("application_projects", "0103_alter_contract_options_and_more"),
+        ("application_projects", "0104_invoice_tags"),
         ("funds", "0135_alter_applicationsubmission_user_and_more"),
     ]
 
