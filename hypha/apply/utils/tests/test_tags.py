4d1304d1afcdfb6ac2052cfd69d57b91bf58b456
Fredrik Jonsson <frjo@xdeb.org>
2026-08-19 09:33:33 +0200

    Fix translating an_or_a.

--- hypha/apply/utils/tests/test_tags.py
+++ hypha/apply/utils/tests/test_tags.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
+from django.utils import translation
 
-from hypha.apply.utils.templatetags.apply_tags import an_or_a
+from hypha.apply.utils.templatetags.apply_tags import an_or_a, with_indefinite_article
 
 
 class TestAnOrA(SimpleTestCase):
@@ -14,3 +15,21 @@ class TestAnOrA(SimpleTestCase):
         self.assertEqual("a", an_or_a("boy"))
         self.assertEqual("a", an_or_a("slug"))
         self.assertEqual("a", an_or_a("one"))
+
+
+class TestWithIndefiniteArticle(SimpleTestCase):
+    def test_english_gets_an_article(self):
+        with translation.override("en"):
+            self.assertEqual("an invoice", with_indefinite_article("invoice"))
+            self.assertEqual("a report", with_indefinite_article("report"))
+
+    def test_english_variants_get_an_article(self):
+        with translation.override("en-gb"):
+            self.assertEqual("an invoice", with_indefinite_article("invoice"))
+
+    def test_other_languages_get_the_bare_noun(self):
+        # Articles are English-specific, so other languages must translate the
+        # surrounding phrase rather than receive an English article.
+        for language in ("sv", "fr", "zh-hans"):
+            with self.subTest(language=language), translation.override(language):
+                self.assertEqual("faktura", with_indefinite_article("faktura"))
