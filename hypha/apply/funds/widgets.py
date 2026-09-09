2769c94546646ac33712ed3fb1024c6e4b660321
Fredrik Jonsson <frjo@xdeb.org>
2026-09-09 23:07:08 +0200

    Make category question widget render select input if more than 32 options for both single and multi select fields (#4974)
    We have had this in place for multichoice fields for years but for some
    reason missed it for single choice fields.
    
    Having e.g. 50 checkboxes or radioboxes on a field is just too many. I
    think 32 is a resonable limit to switch and it has been in place for
    multiselect for years.

--- hypha/apply/funds/widgets.py
+++ hypha/apply/funds/widgets.py
@@ -1,15 +1,28 @@
 from django import forms
 
 
-class MultiCheckboxesWidget(forms.SelectMultiple):
+class ChoicesJSMixin:
     """
-    Custom widget for Choices.js. Adds the required attributes.
+    Adds the attributes required to initialise Choices.js on a select.
     """
 
-    def __init__(self, *args, **kwargs):
-        attrs = kwargs.get("attrs", {})
-        # Add the date attribute for Choices.js initialization
+    def __init__(self, attrs=None, *args, **kwargs):
+        attrs = dict(attrs) if attrs else {}
+        # Add the data attributes for Choices.js initialization
         attrs.setdefault("data-js-choices", "")
         attrs.setdefault("data-placeholder", "")
-        kwargs["attrs"] = attrs
-        super().__init__(*args, **kwargs)
+        super().__init__(attrs, *args, **kwargs)
+
+    def use_required_attribute(self, initial):
+        # Choices.js conceals the underlying <select>, and browsers refuse to
+        # submit a form with a hidden required control, without showing the user
+        # an error. Required is still enforced server side by the form field.
+        return False
+
+
+class ChoicesSelectWidget(ChoicesJSMixin, forms.Select):
+    pass
+
+
+class ChoicesSelectMultipleWidget(ChoicesJSMixin, forms.SelectMultiple):
+    pass
