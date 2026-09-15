07c80f885a106bbab9cf52064f6b358744c361e8
Wes Appler <wes@opentech.fund>
2026-09-15 08:44:54 -0400

    Handle `can_publish` seperately from the wagtail `CopyForm`

--- hypha/apply/funds/admin_views.py
+++ hypha/apply/funds/admin_views.py
@@ -22,10 +22,12 @@ def custom_admin_round_copy_view(request, page):
 
     # Check if the user has permission to publish subpages on the parent
     can_publish = parent_page.permissions_for_user(request.user).can_publish_subpage()
+    print("CAN PUBLISH")
+    print(can_publish)
+    if not can_publish:
+        raise PermissionDenied(_("You do not have permission to publish this round."))
 
-    form = CopyForm(
-        request.POST or None, user=request.user, page=page, can_publish=can_publish
-    )
+    form = CopyForm(request.POST or None, user=request.user, page=page)
 
     next_url = get_valid_next_url_from_request(request)
 
