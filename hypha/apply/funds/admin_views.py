7a9392e94c6f8545bf38cad76aeebce8cc8f66df
Wes Appler <wes@opentech.fund>
2026-09-18 10:27:18 -0400

    Removed old `can_publish` code

--- hypha/apply/funds/admin_views.py
+++ hypha/apply/funds/admin_views.py
@@ -20,11 +20,6 @@ def custom_admin_round_copy_view(request, page):
     # Parent page defaults to parent of source page
     parent_page = page.get_parent()
 
-    # Check if the user has permission to publish subpages on the parent
-    can_publish = parent_page.permissions_for_user(request.user).can_publish_subpage()
-    if not can_publish:
-        raise PermissionDenied(_("You do not have permission to publish this round."))
-
     form = CopyForm(request.POST or None, user=request.user, page=page)
 
     next_url = get_valid_next_url_from_request(request)
